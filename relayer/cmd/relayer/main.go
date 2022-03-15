7ba1196f1effeb18fc388d2547a514fba1f79fc0
Gil <gil@lavanet.xyz>
2022-03-15 15:15:09 -0400

    basic sign, recover functionality

--- relayer/cmd/relayer/main.go
+++ relayer/cmd/relayer/main.go
@@ -3,28 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/lavanet/lava/app"
 	"github.com/lavanet/lava/relayer"
+	"github.com/lavanet/lava/x/spec/types"
 	"github.com/spf13/cobra"
+	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
 )
 
+//
+// TODO: https://docs.cosmos.network/master/architecture/adr-027-deterministic-protobuf-serialization.html
+
 func main() {
+	rootCmd, _ := cosmoscmd.NewRootCmd(
+		"relayer",
+		app.AccountAddressPrefix,
+		app.DefaultNodeHome,
+		app.Name,
+		app.ModuleBasics,
+		app.New,
+	)
+
 	var cmdServer = &cobra.Command{
 		Use:   "server [listen-ip] [listen-port] [node-url]",
 		Short: "server",
 		Long:  `server`,
 		Args:  cobra.ExactArgs(3),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			log.Println(clientCtx, queryClient)
+
 			port, err := strconv.Atoi(args[1])
 			if err != nil {
-				os.Exit(1)
+				return err
 			}
 
 			listenAddr := fmt.Sprintf("%s:%d", args[0], port)
 			ctx := context.Background()
 			relayer.Server(ctx, listenAddr, args[2])
+
+			return nil
 		},
 	}
 
@@ -33,24 +62,36 @@ func main() {
 		Short: "test client",
 		Long:  `test client`,
 		Args:  cobra.ExactArgs(2),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			log.Println(clientCtx, queryClient)
+
 			port, err := strconv.Atoi(args[1])
 			if err != nil {
-				os.Exit(1)
+				return err
 			}
 
 			listenAddr := fmt.Sprintf("%s:%d", args[0], port)
 			ctx := context.Background()
-			relayer.TestClient(ctx, listenAddr)
+			relayer.TestClient(ctx, listenAddr, clientCtx)
+
+			return nil
 		},
 	}
 
-	var rootCmd = &cobra.Command{Use: "relayer"}
+	flags.AddTxFlagsToCmd(cmdServer)
+	cmdServer.MarkFlagRequired(flags.FlagFrom)
+	flags.AddTxFlagsToCmd(cmdTestClient)
+	cmdTestClient.MarkFlagRequired(flags.FlagFrom)
+
 	rootCmd.AddCommand(cmdServer)
 	rootCmd.AddCommand(cmdTestClient)
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
 }
--- relayer/test_client.go
+++ relayer/test_client.go
@@ -5,13 +5,20 @@ import (
 	"log"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto"
+
+	btcSecp256k1 "github.com/btcsuite/btcd/btcec"
+	tendermintcrypto "github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+
 	grpc "google.golang.org/grpc"
 )
 
 const JSONRPC_ETH_BLOCKNUMBER = `{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}`
 const JSONRPC_ETH_GETBALANCE = `{"jsonrpc":"2.0","method":"eth_getBalance","params":["0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8", "latest"],"id":77}`
 
-func TestClient(ctx context.Context, addr string) {
+func TestClient(ctx context.Context, addr string, clientCtx client.Context) {
 	//
 	// Set up a connection to the server.
 	log.Println("TestClient connecting to", addr)
@@ -26,9 +33,58 @@ func TestClient(ctx context.Context, addr string) {
 	c := NewRelayerClient(conn)
 
 	//
-	reply, err := c.Relay(ctx, &RelayRequest{
+	//
+	relayRequest := &RelayRequest{
 		Data: []byte(JSONRPC_ETH_BLOCKNUMBER),
-	})
+	}
+	_, name, _, err := client.GetFromFields(clientCtx.Keyring, clientCtx.From, false)
+	if err != nil {
+		log.Fatalln("client.GetFromFields", err)
+	}
+
+	//
+	// Sign
+	armor, err := clientCtx.Keyring.ExportPrivKeyArmor(name, "")
+	if err != nil {
+		log.Fatalln("clientCtx.Keyring.ExportPrivKeyArmor", err)
+	}
+	log.Println(armor)
+
+	privKey, algo, err := crypto.UnarmorDecryptPrivKey(armor, "")
+	if err != nil {
+		log.Fatalln("crypto.UnarmorDecryptPrivKey", err)
+	}
+	log.Println("TODO: check compatible algo", algo)
+	priv, _ := btcSecp256k1.PrivKeyFromBytes(btcSecp256k1.S256(), privKey.Bytes())
+
+	//
+	// Hash
+	ddd := tendermintcrypto.Sha256([]byte(relayRequest.String()))
+
+	// Sign
+	//
+	sig, err := btcSecp256k1.SignCompact(btcSecp256k1.S256(), priv, ddd, false)
+	if err != nil {
+		log.Fatalln("btcSecp256k1.SignCompact", err)
+	}
+	relayRequest.Sig = sig
+
+	key, _ := clientCtx.Keyring.Key(name)
+	pub := key.GetPubKey()
+
+	//
+	// Recover
+	recPub, _, err := btcSecp256k1.RecoverCompact(btcSecp256k1.S256(), sig, ddd)
+	if err != nil {
+		log.Println("btcSecp256k1.RecoverCompact", err)
+	}
+
+	pk := recPub.SerializeCompressed()
+	bebe := (secp256k1.PubKey)(pk)
+
+	log.Println(bebe.Address(), pub.Address())
+
+	reply, err := c.Relay(ctx, relayRequest)
 	if err != nil {
 		log.Println(err)
 		return
