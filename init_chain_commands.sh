db7bf1f249cb1fa2c780217f3f6d42bea6896b5c
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2022-04-26 19:46:25 +0300

    added jsonrpc terra changes

--- init_chain_commands.sh
+++ init_chain_commands.sh
@@ -6,18 +6,18 @@ lavad tx gov vote 2 yes -y --from alice
 sleep 20
 
 #Ethereum providers
-lavad tx pairing stake-provider "ETH1" 2010stake "127.0.0.1:2221,json-rpc,1" 1 -y --from servicer1
-lavad tx pairing stake-provider "ETH1" 2000stake "127.0.0.1:2222,json-rpc,1" 1 -y --from servicer2
-lavad tx pairing stake-provider "ETH1" 2050stake "127.0.0.1:2223,json-rpc,1" 1 -y --from servicer3
-lavad tx pairing stake-provider "ETH1" 2020stake "127.0.0.1:2224,json-rpc,1" 1 -y --from servicer4
-lavad tx pairing stake-provider "ETH1" 2030stake "127.0.0.1:2225,json-rpc,1" 1 -y --from servicer5
+lavad tx pairing stake-provider "ETH1" 2010stake "127.0.0.1:2221,jsonrpc,1" 1 -y --from servicer1
+lavad tx pairing stake-provider "ETH1" 2000stake "127.0.0.1:2222,jsonrpc,1" 1 -y --from servicer2
+lavad tx pairing stake-provider "ETH1" 2050stake "127.0.0.1:2223,jsonrpc,1" 1 -y --from servicer3
+lavad tx pairing stake-provider "ETH1" 2020stake "127.0.0.1:2224,jsonrpc,1" 1 -y --from servicer4
+lavad tx pairing stake-provider "ETH1" 2030stake "127.0.0.1:2225,jsonrpc,1" 1 -y --from servicer5
 
 #Terra providers
-lavad tx pairing stake-provider "COS1" 2010stake "127.0.0.1:2241,json-rpc,1 127.0.0.1:2231,rest,1" 1 -y --from servicer1
-lavad tx pairing stake-provider "COS1" 2000stake "127.0.0.1:2242,json-rpc,1 127.0.0.1:2232,rest,1" 1 -y --from servicer2
-lavad tx pairing stake-provider "COS1" 2050stake "127.0.0.1:2243,json-rpc,1 127.0.0.1:2233,rest,1" 1 -y --from servicer3
-lavad tx pairing stake-provider "COS1" 2020stake "127.0.0.1:2244,json-rpc,1 127.0.0.1:2234,rest,1" 1 -y --from servicer4
-lavad tx pairing stake-provider "COS1" 2030stake "127.0.0.1:2245,json-rpc,1 127.0.0.1:2235,rest,1" 1 -y --from servicer5
+lavad tx pairing stake-provider "COS1" 2010stake "127.0.0.1:2241,jsonrpc,1 127.0.0.1:2231,rest,1" 1 -y --from servicer1
+lavad tx pairing stake-provider "COS1" 2000stake "127.0.0.1:2242,jsonrpc,1 127.0.0.1:2232,rest,1" 1 -y --from servicer2
+lavad tx pairing stake-provider "COS1" 2050stake "127.0.0.1:2243,jsonrpc,1 127.0.0.1:2233,rest,1" 1 -y --from servicer3
+lavad tx pairing stake-provider "COS1" 2020stake "127.0.0.1:2244,jsonrpc,1 127.0.0.1:2234,rest,1" 1 -y --from servicer4
+lavad tx pairing stake-provider "COS1" 2030stake "127.0.0.1:2245,jsonrpc,1 127.0.0.1:2235,rest,1" 1 -y --from servicer5
 
 lavad tx pairing stake-client "ETH1" 200000stake 1 -y --from user1
 lavad tx pairing stake-client "COS1" 200000stake 1 -y --from user2
