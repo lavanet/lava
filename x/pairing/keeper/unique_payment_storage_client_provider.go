490fca41496c896d6c6b7fe653548ffccb4c8f20
tami <fire17@gmail.com>
2022-05-10 15:51:40 +0300

    added TODO refactor Encode/DecodeUniquePaymentKey

--- x/pairing/keeper/unique_payment_storage_client_provider.go
+++ x/pairing/keeper/unique_payment_storage_client_provider.go
@@ -87,8 +87,7 @@ func addressLengths() (int, int) {
 	return adrLengthUser, adrLengthProvider
 }
 
-//TODO: refactor! either by depending on sdk.AccAddress.Bytes()
-// or use xxx_provider_client_uid where xxx is the length of the providerAddress
+//TODO: refactor to xxx_provider_client_uid
 func (k Keeper) DecodeUniquePaymentKey(ctx sdk.Context, key string) (string, string, string) {
 	adrLengthUser, adrLengthProvider := addressLengths()
 
@@ -99,6 +98,7 @@ func (k Keeper) DecodeUniquePaymentKey(ctx sdk.Context, key string) (string, str
 	return userAddress, providerAddress, uniqueIdentifier
 }
 
+//TODO: refactor to xxx_provider_client_uid
 func (k Keeper) EncodeUniquePaymentKey(ctx sdk.Context, userAddress sdk.AccAddress, providerAddress sdk.AccAddress, uniqueIdentifier string) string {
 	adrLengthUser, adrLengthProvider := addressLengths()
 	if len(userAddress.String()) != adrLengthUser {
