57cce91dc1cbae381f27699fa2ecbc0b8901de8e
nir tayeb <nir@lavanet.xyz>
2022-06-14 15:55:29 +0300

    change path order for websocket

--- relayer/chainproxy/tendermintRPC.go
+++ relayer/chainproxy/tendermintRPC.go
@@ -75,7 +75,7 @@ func (cp *tendermintRpcChainProxy) PortalStart(ctx context.Context, privKey *btc
 	// Setup HTTP Server
 	app := fiber.New(fiber.Config{})
 
-	app.Use("/:dappId/ws", func(c *fiber.Ctx) error {
+	app.Use("/ws/:dappId", func(c *fiber.Ctx) error {
 		// IsWebSocketUpgrade returns true if the client
 		// requested upgrade to the WebSocket protocol.
 		if websocket.IsWebSocketUpgrade(c) {
@@ -85,7 +85,7 @@ func (cp *tendermintRpcChainProxy) PortalStart(ctx context.Context, privKey *btc
 		return fiber.ErrUpgradeRequired
 	})
 
-	app.Get("/:dappId/ws", websocket.New(func(c *websocket.Conn) {
+	app.Get("/ws/:dappId", websocket.New(func(c *websocket.Conn) {
 		var (
 			mt  int
 			msg []byte
@@ -112,7 +112,7 @@ func (cp *tendermintRpcChainProxy) PortalStart(ctx context.Context, privKey *btc
 		}
 	}))
 
-	app.Post("/:dappId/", func(c *fiber.Ctx) error {
+	app.Post("/:dappId", func(c *fiber.Ctx) error {
 		log.Println("jsonrpc in <<< ", string(c.Body()))
 		reply, err := SendRelay(ctx, cp, privKey, "", string(c.Body()))
 		if err != nil {
