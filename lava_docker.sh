e70d7effc9590d9d5f5071255199be3e3510d2de
Tami Bar <tami@lavanet.xyz>
2022-06-14 15:16:30 +0300

    E2E Working on Docker

--- lava_docker.sh
+++ lava_docker.sh
@@ -3,27 +3,29 @@
 
 
 ### RUN LAVA E2E TESTS - Runs all Lava e2e Tests - Full Lava Node, Providers, Test_clients
-# echo ' ::: LAVA E2E TEST DOCKER (STARPORT) :::'
-# docker run -p 4500:4500 -p 1317:1317 -p 26657:26657 --name lavaC lava_starport go test ./testutil/e2e/ -v 
+echo ' ::: LAVA E2E TEST DOCKER (STARPORT) :::'
+docker run -p 4500:4500 -p 1317:1317 -p 26657:26657 --name lavaC lava_starport go test ./testutil/e2e/ -v 
 
 
-### RUN LAVA DOCER 
-echo ' ::: LAVA DOCKER (STARPORT) :::'
-docker run -p 4500:4500 -p 1317:1317 -p 26657:26657 --name lava19 lava_starport |& grep -e lava_ -e ERR_ -e STARPORT] -e ! &
+### OR MANUALLY
 
-### INIT
-echo ' ::: LAVA INIT (on docker):::'
-echo ' ::: waiting 40 secs :::'
-sleep 40
-docker exec -it lava19 sh .scripts/init.sh
+### RUN LAVA DOCKER 
+# echo ' ::: LAVA DOCKER (STARPORT) :::'
+# docker run -p 4500:4500 -p 1317:1317 -p 26657:26657 --name lava19 lava_starport |& grep -e lava_ -e ERR_ -e STARPORT] -e ! &
 
-### PROVIDERS
-echo ' ::: LAVA PROVIDERS (on docker):::'
-docker exec -it -d lava19 sh .scripts/eth.sh
-docker exec -it -d lava19 sh .scripts/osmosis.sh
+# ### INIT
+# echo ' ::: LAVA INIT (on docker):::'
+# echo ' ::: waiting 40 secs :::'
+# sleep 40
+# docker exec -it lava19 sh .scripts/init.sh
 
-### CLIENTS
-echo ' ::: LAVA CLIENT (on docker):::'
-sleep 15
-docker exec -it lava19 sh .scripts/run_clients.sh
+# ### PROVIDERS
+# echo ' ::: LAVA PROVIDERS (on docker):::'
+# docker exec -it -d lava19 sh .scripts/eth.sh
+# docker exec -it -d lava19 sh .scripts/osmosis.sh
+
+# ### CLIENTS
+# echo ' ::: LAVA CLIENT (on docker):::'
+# sleep 15
+# docker exec -it lava19 sh .scripts/run_clients.sh
 
