29891bd6650404824acd282a19bd51ac6a0dd43c
Tami Bar <tami@lavanet.xyz>
2022-06-20 20:08:27 +0300

    removed .scripts

--- .scripts/serve_chain.sh
+++
@@ -1,3 +0,0 @@
-echo ' ::: SERVING LAVA CHAIN (ignite) :::'
-(cd $LAVA && ignite chain serve -v -r) 2>&1 | grep -e lava_ -e ERR_ -e STARPORT] -e !
-# (cd /go/lava && starport chain serve -v -r) 2>&1 | grep -e lava_ -e ERR_ -e STARPORT] -e !
