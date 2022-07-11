fec7951d74071a26d1018b38746548b2612acd97
omer mishael <omer@lavanet.xyz>
2022-07-11 12:32:04 +0300

    added latestParamChange and fixtae params stub

--- x/epochstorage/keeper/params.go
+++ x/epochstorage/keeper/params.go
@@ -1,7 +1,10 @@
 package keeper
 
 import (
+	"strconv"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/utils"
 	"github.com/lavanet/lava/x/epochstorage/types"
 )
 
@@ -11,6 +14,7 @@ func (k Keeper) GetParams(ctx sdk.Context) types.Params {
 		k.UnstakeHoldBlocks(ctx),
 		k.EpochBlocks(ctx),
 		k.EpochsToSave(ctx),
+		k.LatestParamChange(ctx),
 	)
 }
 
@@ -66,6 +70,11 @@ func (k Keeper) BlockInEpoch(ctx sdk.Context, block uint64) (res uint64) {
 	return block % blocksCycle
 }
 
+func (k Keeper) LatestParamChange(ctx sdk.Context) (res uint64) {
+	k.paramstore.Get(ctx, types.KeyLatestParamChange, &res)
+	return
+}
+
 // GetSessionStartForBlock gets a session start supports one param change
 func (k Keeper) GetEpochStartForBlock(ctx sdk.Context, block uint64) (epochStart uint64, blockInEpoch uint64) {
 	blocksInTargetSession := k.BlockInEpoch(ctx, block)
@@ -77,4 +86,36 @@ func (k Keeper) GetPreviousEpochStartForBlock(ctx sdk.Context, block uint64) (pr
 	epochStart, _ := k.GetEpochStartForBlock(ctx, block)
 	previousEpochStart, _ = k.GetEpochStartForBlock(ctx, epochStart-1) //we take one block before the target epoch so it belongs to the previous epoch
 	return
+}
+
+func (k Keeper) FixateParams(ctx sdk.Context, block uint64) {
+	latestParamChange := k.LatestParamChange(ctx)
+	if latestParamChange == 0 { // no change
+		return
+	}
+	if latestParamChange > block {
+		utils.LavaError(ctx, k.Logger(ctx), "invalid_latest_param_change", map[string]string{"error": "latestParamChange > block", "latestParamChange": strconv.FormatUint(latestParamChange, 10)}, "latest param change cant be in the future")
+		return
+	}
+	if latestParamChange < k.GetEarliestEpochStart(ctx) {
+		//latest param change is older than memory, so remove it
+		k.paramstore.Set(ctx, types.KeyLatestParamChange, 0)
+		//clean up older fixated params, they no longer matter
+		k.CleanOlderFixatedParams(ctx)
+		return
+	}
+	// we have a param change, is it in the last epoch?
+	if latestParamChange > k.GetPreviousEpochStartForBlock(ctx, block) {
+		// this is a recent change so we need to move the current fixation backwards
+		k.PushFixatedParams(ctx, block)
+	}
+}
+
+func (k Keeper) PushFixatedParams(ctx sdk.Context, block uint64) {
+
+	utils.LogLavaEvent(ctx, k.Logger(ctx), "fixated_params_after_change", map[string]string{"moduleName": types.ModuleName, "block": strconv.FormatUint(block, 10)}, "params fixated after a change")
+}
+
+func (k Keeper) CleanOlderFixatedParams(ctx sdk.Context) {
+	//TODO:
 }
