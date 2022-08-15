9289f4f9f7d20f54e7b5f39542da8424ed78feec
kajeagentspi <jmtagnepis@gmail.com>
2022-08-15 14:37:33 +0900

    Localize antehandler creation for customization

--- app/app.go
+++ app/app.go
@@ -614,20 +614,14 @@ func New(
 	app.SetInitChainer(app.InitChainer)
 	app.SetBeginBlocker(app.BeginBlocker)
 
-	anteHandler, err := ante.NewAnteHandler(
-		ante.HandlerOptions{
-			AccountKeeper:   app.AccountKeeper,
-			BankKeeper:      app.BankKeeper,
-			SignModeHandler: encodingConfig.TxConfig.SignModeHandler(),
-			FeegrantKeeper:  app.FeeGrantKeeper,
-			SigGasConsumer:  ante.DefaultSigVerificationGasConsumer,
-		},
+	app.SetAnteHandler(
+		NewAnteHandler(
+			app.AccountKeeper,
+			app.BankKeeper,
+			encodingConfig.TxConfig.SignModeHandler(),
+			app.FeeGrantKeeper,
+			ante.DefaultSigVerificationGasConsumer),
 	)
-	if err != nil {
-		panic(err)
-	}
-
-	app.SetAnteHandler(anteHandler)
 	app.SetEndBlocker(app.EndBlocker)
 
 	if loadLatest {
