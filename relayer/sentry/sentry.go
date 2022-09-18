4f486311ad664d4235a87ab1af6c0c8e837b26d3
omer mishael <omer@lavanet.xyz>
2022-09-18 19:38:15 +0300

    sessions lock and remove from wrap aren't locked same time

--- relayer/sentry/sentry.go
+++ relayer/sentry/sentry.go
@@ -1034,45 +1034,49 @@ func (s *Sentry) _findPairing(ctx context.Context) (retWrap *RelayerClientWrappe
 
 func (wrap *RelayerClientWrapper) FetchEndpointConnectionFromClientWrapper(s *Sentry, ctx context.Context, index int) (connected bool, endpointPtr *Endpoint) {
 	//assumes s.pairingMu is Rlocked here
-	wrap.SessionsLock.Lock()
-	defer wrap.SessionsLock.Unlock()
+	getConnectionFromWrap := func(s *Sentry, ctx context.Context, index int) (connected bool, endpointPtr *Endpoint, allDisabled bool) {
+		wrap.SessionsLock.Lock()
+		defer wrap.SessionsLock.Unlock()
 
-	for idx, endpoint := range wrap.Endpoints {
-		if !endpoint.Enabled {
-			continue
-		}
-		if endpoint.Client == nil {
-			connectCtx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
-			conn, err := s.connectRawClient(connectCtx, endpoint.Addr)
-			cancel()
-			if err != nil {
-				endpoint.ConnectionRefusals++
-				utils.LavaFormatError("error connecting to provider", err, &map[string]string{"provider endpoint": endpoint.Addr, "provider address": wrap.Acc, "endpoint": fmt.Sprintf("%+v", endpoint)})
-				if endpoint.ConnectionRefusals >= MaxConsecutiveConnectionAttemts {
-					endpoint.Enabled = false
-					utils.LavaFormatWarning("disabling provider endpoint", nil, &map[string]string{"Endpoint": endpoint.Addr, "address": wrap.Acc, "currentEpoch": strconv.FormatInt(s.GetBlockHeight(), 10)})
-				}
+		for idx, endpoint := range wrap.Endpoints {
+			if !endpoint.Enabled {
 				continue
 			}
+			if endpoint.Client == nil {
+				connectCtx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+				conn, err := s.connectRawClient(connectCtx, endpoint.Addr)
+				cancel()
+				if err != nil {
+					endpoint.ConnectionRefusals++
+					utils.LavaFormatError("error connecting to provider", err, &map[string]string{"provider endpoint": endpoint.Addr, "provider address": wrap.Acc, "endpoint": fmt.Sprintf("%+v", endpoint)})
+					if endpoint.ConnectionRefusals >= MaxConsecutiveConnectionAttemts {
+						endpoint.Enabled = false
+						utils.LavaFormatWarning("disabling provider endpoint", nil, &map[string]string{"Endpoint": endpoint.Addr, "address": wrap.Acc, "currentEpoch": strconv.FormatInt(s.GetBlockHeight(), 10)})
+					}
+					continue
+				}
 
-			endpoint.ConnectionRefusals = 0
-			endpoint.Client = conn
+				endpoint.ConnectionRefusals = 0
+				endpoint.Client = conn
+			}
+			wrap.Endpoints[idx] = endpoint
+			return true, endpoint, false
 		}
-		wrap.Endpoints[idx] = endpoint
-		return true, endpoint
-	}
 
-	// checking disabled endpoints, as we can disable an endpoint mid run of the previous loop, we should re test the current endpoint state
-	// before verifing all are Disabled.
-	allDisabled := true
-	for _, endpoint := range wrap.Endpoints {
-		if !endpoint.Enabled {
-			continue
+		// checking disabled endpoints, as we can disable an endpoint mid run of the previous loop, we should re test the current endpoint state
+		// before verifing all are Disabled.
+		allDisabled = true
+		for _, endpoint := range wrap.Endpoints {
+			if !endpoint.Enabled {
+				continue
+			}
+			// even one endpoint is enough for us to not purge.
+			allDisabled = false
 		}
-		// even one endpoint is enough for us to not purge.
-		allDisabled = false
+		return false, nil, allDisabled
 	}
-
+	var allDisabled bool
+	connected, endpointPtr, allDisabled = getConnectionFromWrap(s, ctx, index)
 	//we dont purge if we tried connecting and failed, only if we already disabled all endpoints
 	if allDisabled {
 		utils.LavaFormatError("purging provider after all endpoints are disabled", nil, &map[string]string{"provider endpoints": fmt.Sprintf("%v", wrap.Endpoints), "provider address": wrap.Acc})
