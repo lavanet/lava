5f16bf75050393734769c06869090e13a2449109
Ran Mishael <ran@lavanet.xyz>
2022-10-02 16:10:02 +0200

    aptos WIP

--- scripts/setup_providers.sh
+++ scripts/setup_providers.sh
@@ -70,19 +70,25 @@ screen -d -m -S arb_providers zsh -c "source ~/.zshrc; lavad server 127.0.0.1 72
 screen -S arb_providers -X screen -t win1 -X zsh -c "source ~/.zshrc; lavad server 127.0.0.1 7242 $ARB1_HTTP ARB1 jsonrpc --from servicer2 2>&1 | tee $LOGS_DIR/ARB1_2222.log"
 screen -S arb_providers -X screen -t win2 -X zsh -c "source ~/.zshrc; lavad server 127.0.0.1 7243 $ARB1_HTTP ARB1 jsonrpc --from servicer3 2>&1 | tee $LOGS_DIR/ARB1_2223.log"
 
+#Aptos providers 
+screen -d -m -S apt1_providers zsh -c "source ~/.zshrc; lavad server 127.0.0.1 10031 $APTOS_REST APT1 rest --from servicer1 2>&1 | tee $LOGS_DIR/APT1_10031.log"
+screen -S apt1_providers -X screen -t win1 -X zsh -c "source ~/.zshrc; lavad server 127.0.0.1 10032 $APTOS_REST APT1 rest --from servicer2 2>&1 | tee $LOGS_DIR/APT1_10032.log"
+screen -S apt1_providers -X screen -t win2 -X zsh -c "source ~/.zshrc; lavad server 127.0.0.1 10033 $APTOS_REST APT1 rest --from servicer3 2>&1 | tee $LOGS_DIR/APT1_10033.log"
 
 # Setup Portals
 screen -d -m -S portals zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3333 ETH1 jsonrpc --from user1 2>&1 | tee $LOGS_DIR/PORTAL_3333.log"
-screen -S portals -X screen -t win10 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3339 GTH1 jsonrpc --from user1 2>&1 | tee $LOGS_DIR/PORTAL_3339.log"
 screen -S portals -X screen -t win11 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3334 COS3 rest --from user2 2>&1 | tee $LOGS_DIR/PORTAL_3334.log"
 screen -S portals -X screen -t win12 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3335 COS3 tendermintrpc --from user2 2>&1 | tee $LOGS_DIR/PORTAL_3335.log"
 screen -S portals -X screen -t win13 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3336 FTM250 jsonrpc --from user3 2>&1 | tee $LOGS_DIR/PORTAL_3336.log"
 screen -S portals -X screen -t win14 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3337 COS4 rest --from user2 2>&1 | tee $LOGS_DIR/PORTAL_3337.log"
 screen -S portals -X screen -t win15 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3338 COS4 tendermintrpc --from user2 2>&1 | tee $LOGS_DIR/PORTAL_3338.log"
+screen -S portals -X screen -t win10 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3339 GTH1 jsonrpc --from user1 2>&1 | tee $LOGS_DIR/PORTAL_3339.log"
 screen -S portals -X screen -t win16 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3340 LAV1 rest --from user4 2>&1 | tee $LOGS_DIR/PORTAL_3340.log"
 screen -S portals -X screen -t win17 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3341 LAV1 tendermintrpc --from user4 2>&1 | tee $LOGS_DIR/PORTAL_3341.log"
-screen -S portals -X screen -t win17 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3342 CELO jsonrpc --from user3 2>&1 | tee $LOGS_DIR/PORTAL_3342.log"
-screen -S portals -X screen -t win17 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3343 ALFAJORES jsonrpc --from user3 2>&1 | tee $LOGS_DIR/PORTAL_3343.log"
-screen -S portals -X screen -t win17 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3344 ARB1 jsonrpc --from user4 2>&1 | tee $LOGS_DIR/PORTAL_3344.log"
+screen -S portals -X screen -t win18 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3342 CELO jsonrpc --from user3 2>&1 | tee $LOGS_DIR/PORTAL_3342.log"
+screen -S portals -X screen -t win19 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3343 ALFAJORES jsonrpc --from user3 2>&1 | tee $LOGS_DIR/PORTAL_3343.log"
+screen -S portals -X screen -t win20 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3344 ARB1 jsonrpc --from user4 2>&1 | tee $LOGS_DIR/PORTAL_3344.log"
+screen -S portals -X screen -t win21 -X zsh -c "source ~/.zshrc; lavad portal_server 127.0.0.1 3345 APT1 rest --from user4 2>&1 | tee $LOGS_DIR/PORTAL_3345.log"
+
 echo "--- setting up screens done ---"
 screen -ls
