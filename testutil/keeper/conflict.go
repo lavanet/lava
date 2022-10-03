d2e7f58f30b05237427aa13ab215c9597652ab5b
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2022-10-03 15:11:36 +0300

    starknet is now with data reliability

--- testutil/keeper/conflict.go
+++ testutil/keeper/conflict.go
@@ -5,24 +5,22 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/lavanet/lava/x/conflict/keeper"
 	"github.com/lavanet/lava/x/conflict/types"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/libs/log"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
 )
 
 func ConflictKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
+	epochstorage, ctx := EpochstorageKeeper(t)
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
 
 	db := tmdb.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db)
+	stateStore := ctx.MultiStore().(storetypes.CommitMultiStore)
 	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
 	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
 	require.NoError(t, stateStore.LoadLatestVersion())
@@ -36,6 +34,7 @@ func ConflictKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		memStoreKey,
 		"ConflictParams",
 	)
+
 	k := keeper.NewKeeper(
 		cdc,
 		storeKey,
@@ -44,12 +43,12 @@ func ConflictKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		nil,
 		nil,
 		nil,
-		nil,
+		epochstorage,
 		nil,
 	)
 
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
-
+	ctx = ctx.WithMultiStore(stateStore)
+	epochstorage.EpochBlocksRaw(ctx)
 	// Initialize params
 	k.SetParams(ctx, types.DefaultParams())
 
