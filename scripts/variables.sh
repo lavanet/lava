dfe0bab00c359e35d6d993b524a337761bf3de33
oren-lava <oren@lavanet.xyz>
2022-11-02 10:53:28 +0200

    variables.sh is missing from  init_chain_commands

---
+++ scripts/variables.sh
@@ -0,0 +1,87 @@
+# ETH - Mainnet
+ETH_RPC_WS=wss://kololo8ex9:ifififkwqlspAFJIjfdMCsdmasdgAKoakdFOAKSFOakfaSEFkbntb311esad@prod-pnet-ethmainnet-1.lavapro.xyz/eth/ws/
+ETH_RPC_HTTP=https://kololo8ex9:ifififkwqlspAFJIjfdMCsdmasdgAKoakdFOAKSFOakfaSEFkbntb311esad@prod-pnet-ethmainnet-1.lavapro.xyz/eth/
+# ETH - Goerli
+# TODO: change endpoint to work with HTTPS - install and configure certbot
+GTH_RPC_WS=ws://AQxDRiR34rdN9qAp:tRtY8YdKYGn9igCS7tpTqN7kjsk3s7HJeFRzzYNo@prod-pnet-ethgoerli-1/eth/ws/
+GTH_RPC_HTTP=http://AQxDRiR34rdN9qAp:tRtY8YdKYGn9igCS7tpTqN7kjsk3s7HJeFRzzYNo@prod-pnet-ethgoerli-1/eth/
+
+# Osmosis - Mainnet
+OSMO_RPC=https://bekotopopo2:99al9YwXTnvJohMW39ZBqVH5AWnWBDV8@prod-pnet-osmosisnode-1.lavapro.xyz/mainnet/rpc/
+OSMO_REST=https://bekotopopo2:99al9YwXTnvJohMW39ZBqVH5AWnWBDV8@prod-pnet-osmosisnode-1.lavapro.xyz/mainnet/rest/
+# Osmosis - Testnet
+OSMO_TEST_RPC=https://bekotopopo2:99al9YwXTnvJohMW39ZBqVH5AWnWBDV8@prod-pnet-osmosisnode-1.lavapro.xyz/testnet/rpc/
+OSMO_TEST_REST=https://bekotopopo2:99al9YwXTnvJohMW39ZBqVH5AWnWBDV8@prod-pnet-osmosisnode-1.lavapro.xyz/testnet/rest/
+
+# Fantom - Mainnet
+FTM_RPC_WS=wss://blueturkey321:Q5Q5k646YYiPRHmt2AzCzU847VGrbJo8@prod-pnet-fantomnode-1.lavapro.xyz/ftm/ws
+FTM_RPC_HTTP=https://blueturkey321:Q5Q5k646YYiPRHmt2AzCzU847VGrbJo8@prod-pnet-fantomnode-1.lavapro.xyz/ftm/http
+# Fantom - Testnet
+FTM_TEST_RPC_WS=wss://blueturkey321:Q5Q5k646YYiPRHmt2AzCzU847VGrbJo8@prod-pnet-fantomnode-1.lavapro.xyz/ftm-testnet/ws
+FTM_TEST_RPC_HTTP=https://blueturkey321:Q5Q5k646YYiPRHmt2AzCzU847VGrbJo8@prod-pnet-fantomnode-1.lavapro.xyz/ftm-testnet/http
+
+# Celo - Mainnet
+CELO_HTTP=https://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo/http
+CELO_WS=wss://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo/ws
+# Celo - Alfajores
+CELO_ALFAJORES_HTTP=https://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo-alfajores/http
+CELO_ALFAJORES_WS=wss://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo-alfajores/ws
+# Celo - Baklava
+CELO_BAKLAVA_HTTP=https://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo-baklava/http
+CELO_BAKLAVA_WS=wss://chopchipmeep1189:KSIw9C8Mc0euMGPNlZb60zKfiMXBBWoQ@prod-pnet-celo-1.lavapro.xyz/celo-baklava/ws
+
+# Juno - Mainnet
+JUNO_RPC=https://dmrcnggpzyts:p1g4pv07092mdrv72hnz44496idxwg363rm724ki@juno-node-1.lavapro.xyz/mainnet/rpc
+JUNO_REST=https://dmrcnggpzyts:p1g4pv07092mdrv72hnz44496idxwg363rm724ki@juno-node-1.lavapro.xyz/mainnet/rest
+# Juno - Testnet
+JUNO_TEST_RPC=https://dmrcnggpzyts:p1g4pv07092mdrv72hnz44496idxwg363rm724ki@juno-node-1.lavapro.xyz/testnet/rpc
+JUNO_TEST_REST=https://dmrcnggpzyts:p1g4pv07092mdrv72hnz44496idxwg363rm724ki@juno-node-1.lavapro.xyz/testnet/rest
+
+ARB1_HTTP=https://arb1.arbitrum.io/rpc
+# Evmos - Mainnet
+EVMOS_RPC=https://hxpj3iwa0hkq:iyu0i6b7ww67mhzrd8dejcrc982drad5xm80lon5@evmos-node-1.lavapro.xyz/mainnet/rpc
+EVMOS_REST=https://hxpj3iwa0hkq:iyu0i6b7ww67mhzrd8dejcrc982drad5xm80lon5@evmos-node-1.lavapro.xyz/mainnet/rest
+# Evmos - Testnet
+EVMOS_TEST_RPC=https://hxpj3iwa0hkq:iyu0i6b7ww67mhzrd8dejcrc982drad5xm80lon5@evmos-node-1.lavapro.xyz/testnet/rpc
+EVMOS_TEST_REST=https://hxpj3iwa0hkq:iyu0i6b7ww67mhzrd8dejcrc982drad5xm80lon5@evmos-node-1.lavapro.xyz/testnet/rest
+
+# Cosmos Hub - Mainnet
+GAIA_RPC=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/mainnet/rpc/
+GAIA_REST=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/mainnet/rest/
+# Cosmos Hub - Testnet
+GAIA_TEST_RPC=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/testnet/rpc
+GAIA_TEST_REST=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/testnet/rest
+
+# Starnet - Mainnet
+STARKNET_RPC=https://ql8ff96eng9upas:op9arp8oc1uatuvvc6fv11r6c5hvh6wg7hm5h33p@starknet-node-1.lavapro.xyz/mainnet/
+# Starknet - Testnet (Goerli)
+STARKNET_TEST_RPC=https://ql8ff96eng9upas:op9arp8oc1uatuvvc6fv11r6c5hvh6wg7hm5h33p@starknet-node-1.lavapro.xyz/testnet/
+
+# Lava - Testnet - RPC
+LAVA_RPC=https://public-rpc.lavanet.xyz:443/rpc/
+# Lava - Testnet - REST
+LAVA_REST=https://public-rpc.lavanet.xyz:443/rest/
+
+#TODO: accept url paths in mock proxy as host, and auto append to request URL
+ETH_HOST=https://mainnet.infura.io
+ETH_URL_PATH=v3/3755a1321ab24f938589412403c46455
+ETH_HOST=http://kololo8ex9:ifififkwqlspAFJIjfdMCsdmasdgAKoakdFOAKSFOakfaSEFkbntb311esad@prod-pnet-ethmainnet-1.lavapro.xyz
+ETH_URL_PATH=eth/
+LAVA_REST=http://0.0.0.0:1317
+LAVA_RPC=http://0.0.0.0:26657
+LAVA_GRPC=0.0.0.0:9090
+# OLD_OSMO_HOST=DiHj7kLfX6hNhECG:66JiESmAke6RHte3AKqDAyt966nNAcm7yMiYbLet@5.161.109.138 # DO NOT USE
+
+# Cosmos Hub - Mainnet
+GAIA_RPC=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/mainnet/rpc/
+GAIA_REST=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/mainnet/rest/
+# Cosmos Hub - Testnet
+GAIA_TEST_RPC=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/testnet/rpc
+GAIA_TEST_REST=https://cv8sxghsd5ny:coqiqzgzaam34gq9lf17gt0vysxug8r3me6jkx1o@gaia-node-1.lavapro.xyz/testnet/rest
+
+APTOS_REST=https://fullnode.devnet.aptoslabs.com/v1
+
+JUNO_RPC=https://rpc.juno.omniflix.co
+JUNO_REST=https://api.juno.omniflix.co
+
+POLYGON_MAINNET_RPC=https://polygon-rpc.com
