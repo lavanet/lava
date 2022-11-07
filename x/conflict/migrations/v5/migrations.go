59f766aac3cbb7bab5320b4f89da1495b98809a3
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2022-11-07 10:42:39 +0200

    added upgrade

---
+++ x/conflict/migrations/v5/migrations.go
@@ -0,0 +1,29 @@
+package v4 // migrations.go
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/x/pairing/types"
+)
+
+const (
+	ConflictVoteKeyPrefix = "ConflictVote/value/"
+)
+
+func DeleteOpenConflicts(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	log.Println("@@@ REMOVING OLD STORAGE KEYS @@@")
+	oldStore := prefix.NewStore(store, types.KeyPrefix(ConflictVoteKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(oldStore, []byte{})
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		log.Println(fmt.Sprintf("@@@ Key: %s @@@", string(iterator.Key())))
+		oldStore.Delete(iterator.Key()) // Delete old key, value
+	}
+	return nil
+}
