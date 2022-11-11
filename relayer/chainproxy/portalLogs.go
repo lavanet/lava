f7b9b752dd2f7e3baf6619fa056b0a9308524c73
k.kolaj <k.kolaj@atrax.al>
2022-11-11 15:13:29 +0100

    reformat logs in all the places in Lava, change portalErrors.go to portalLogs.go

--- relayer/chainproxy/portalLogs.go
+++ relayer/chainproxy/portalLogs.go
@@ -15,60 +15,6 @@ var ReturnMaskedErrors = "false"
 
 const webSocketCloseMessage = "websocket: close 1005 (no status)"
 
-// Input will be masked with a random GUID if returnMaskedErrors is set to true
-//func GetUniqueGuidResponseForError(responseError error) string {
-//	guID := fmt.Sprintf("GUID%d", rand.Int63())
-//	var ret string
-//	ret = "Error GUID: " + guID
-//	utils.LavaFormatError("UniqueGuidResponseForError", responseError, &map[string]string{"GUID": guID})
-//	if ReturnMaskedErrors == "false" {
-//		ret += fmt.Sprintf(", Error: %v", responseError)
-//	}
-//	return ret
-//}
-//
-//// Websocket healthy disconnections throw "websocket: close 1005 (no status)" error,
-//// We dont want to alert error monitoring for that purpses.
-//func AnalyzeWebSocketErrorAndWriteMessage(c *websocket.Conn, mt int, err error, msgSeed string) {
-//	if err.Error() == webSocketCloseMessage {
-//		utils.LavaFormatInfo("Websocket connection closed by the user, "+err.Error(), nil)
-//		return
-//	}
-//	c.WriteMessage(mt, []byte("Error Received: "+GetUniqueGuidResponseForError(err)))
-//}
-//
-//// Logging the Request and Response to the stdout.
-//func LogRequestAndResponse(module string, hasError bool, method string, path string, req string, resp string, msgSeed string, err error) {
-//	if hasError {
-//		utils.LavaFormatInfo(module, &map[string]string{"GUID": msgSeed, "request": req, "response": resp, "method": method, "path": path, "HasError": strconv.FormatBool(hasError), "error": err.Error()})
-//		return
-//	}
-//	utils.LavaFormatInfo(module, &map[string]string{"GUID": msgSeed, "request": req, "response": resp, "method": method, "path": path, "HasError": strconv.FormatBool(hasError)})
-//}
-
-//func LogNewRelicApp() {
-//	var myEnv map[string]string
-//	myEnv, err = godotenv.Read()
-//
-//	NEW_RELIC_APP_NAME := myEnv["NEW_RELIC_APP_NAME"]
-//	NEW_RELIC_LICENSE_KEY := myEnv["NEW_RELIC_LICENSE_KEY"]
-//
-//	newrelicApp, err := newrelic.NewApplication(
-//		newrelic.ConfigAppName(NEW_RELIC_APP_NAME),
-//		newrelic.ConfigLicense(NEW_RELIC_LICENSE_KEY),
-//		newrelic.ConfigAppLogEnabled(true),
-//		newrelic.ConfigAppLogForwardingEnabled(true),
-//		func(config *newrelic.Config) {
-//			zerologlog.Debug().Enabled()
-//		},
-//	)
-//	if myEnv == nil {
-//		newrelicApp = nil
-//	}
-//
-//
-//}
-
 type PortalLogs struct {
 	newRelicApplication *newrelic.Application
 }
@@ -123,8 +69,3 @@ func (cp *PortalLogs) LogStartTransaction(name string) {
 		defer txn.End()
 	}
 }
-
-// create a struct that manages all the logs f the portal (PortalLogs)
-// change the package into portalLogs package
-// the struct will have newRelic (type newRelicApp)
-//
