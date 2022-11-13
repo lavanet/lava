691073f3abae6b54b8f084067c073d46404d17e8
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2022-11-13 23:54:21 +0200

    hotfix

--- x/conflict/keeper/vote.go
+++ x/conflict/keeper/vote.go
@@ -239,8 +239,13 @@ func (k Keeper) HandleAndCloseVote(ctx sdk.Context, ConflictVote types.ConflictV
 					continue
 				}
 				ok, err := k.pairingKeeper.CreditStakeEntry(ctx, ConflictVote.ChainID, accAddress, sdk.NewCoin(epochstoragetypes.TokenDenom, rewardVoter.TruncateInt()), true)
-				if !ok || err != nil {
-					utils.LavaError(ctx, logger, "failed_credit", map[string]string{"error": err.Error()}, "failed to credit voter")
+				utils.LavaError(ctx, logger, "failed_credit", map[string]string{"error": err.Error()}, "failed to credit voter")
+				if !ok {
+					details := map[string]string{}
+					if err != nil {
+						details["error"] = err.Error()
+					}
+					utils.LavaError(ctx, logger, "failed_credit", details, "failed to credit voter")
 					continue
 				}
 			}
