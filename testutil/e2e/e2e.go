a690c962dc62d2e91f03c2f5d91c0e96dce0f8d0
kajeagentspi <jmtagnepis@gmail.com>
2022-11-16 05:06:46 +0900

    Add checks for lavad

--- testutil/e2e/e2e.go
+++ testutil/e2e/e2e.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/ignite/cli/ignite/chainconfig"
 	"github.com/ignite/cli/ignite/pkg/cache"
 	"github.com/ignite/cli/ignite/services/chain"
+	specTypes "github.com/lavanet/lava/x/spec/types"
+	"google.golang.org/grpc"
 )
 
 var wg sync.WaitGroup
@@ -36,16 +39,41 @@ func startLava(ctx context.Context) {
 	}
 
 	err = c.Serve(ctx, storage, chain.ServeForceReset())
-	if err != nil && err != context.Canceled {
+	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx := context.Background()
 	wg.Add(1)
 	go startLava(ctx)
-	time.Sleep(time.Second * 5)
-	_ = cancel
+
+	grpcConn, err := grpc.Dial("127.0.0.1:9090", grpc.WithInsecure())
+	if err != nil {
+		// this check does not get triggered even if server is down
+		fmt.Println(err)
+		return
+	}
+	specQueryClient := specTypes.NewQueryClient(grpcConn)
+
+	for {
+		// TODO
+		// This loop would wait for the lavad server to be up before chain init
+		// This one should always end but add a timer
+		_, err := specQueryClient.SpecAll(context.Background(), &specTypes.QueryAllSpecRequest{})
+		if err != nil && strings.Contains(err.Error(), "rpc error") {
+			fmt.Println("Waiting for Lava")
+			time.Sleep(time.Second)
+		} else if err == nil {
+			break
+		} else {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	fmt.Println("works")
+	// ctx.Done()
 	wg.Wait()
 }
