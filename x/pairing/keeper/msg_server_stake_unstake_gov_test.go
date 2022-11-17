967fa83d94f2b09849fc493a0506e7e67dc743dc
oren-lava <oren@lavanet.xyz>
2022-11-17 16:48:09 +0200

    CNS-29 TestUnstakeGovEpochBlocksIncrease added

--- x/pairing/keeper/msg_server_stake_unstake_gov_test.go
+++ x/pairing/keeper/msg_server_stake_unstake_gov_test.go
@@ -144,7 +144,7 @@ func TestStakeGovEpochBlocksIncrease(t *testing.T) {
 
 }
 
-// Test that if the EpochBlocks param decreases make sure the provider is staked only after the original EpochBlocks value (EpochBlocks = number of blocks in an epoch. This parameter is fixated)
+// Test that if the EpochBlocks param decreases make sure the provider is unstaked only after the original EpochBlocks value (EpochBlocks = number of blocks in an epoch. This parameter is fixated)
 func TestUnstakeGovEpochBlocksDecrease(t *testing.T) {
 
 	// setup testnet with mock spec
@@ -175,7 +175,8 @@ func TestUnstakeGovEpochBlocksDecrease(t *testing.T) {
 	}
 
 	// Unstake the provider
-	ts.servers.PairingServer.UnstakeProvider(ts.ctx, &types.MsgUnstakeProvider{Creator: providerAddress.String(), ChainID: ts.spec.Index})
+	_, err = ts.servers.PairingServer.UnstakeProvider(ts.ctx, &types.MsgUnstakeProvider{Creator: providerAddress.String(), ChainID: ts.spec.Index})
+	require.Nil(t, err)
 
 	// Advance to the next block so the EpochBlocks change apply
 	ts.ctx = testkeeper.AdvanceBlock(ts.ctx, ts.keepers) // blockHeight = 40
@@ -196,6 +197,77 @@ func TestUnstakeGovEpochBlocksDecrease(t *testing.T) {
 	}{
 		{"providerShouldntBeUnstaked", epochProviderShouldntBeUnstaked, true}, // 11 blocks have passed since the stake - not enough blocks
 		{"providerShouldBeUnstaked", epochProviderShouldBeUnstaked, false},    // 21 blocks have passed - enough blocks
+	}
+
+	sessionCounter := 0
+	for _, tt := range tests {
+		sessionCounter += 1
+		t.Run(tt.name, func(t *testing.T) {
+
+			// check if the provider is staked
+			_, found := ts.keepers.Epochstorage.GetEpochStakeEntries(sdk.UnwrapSDKContext(ts.ctx), tt.epoch, epochstoragetypes.ProviderKey, ts.spec.GetIndex())
+			require.Equal(t, tt.shouldBeStaked, found)
+		})
+	}
+}
+
+// Test that if the EpochBlocks param increases make sure the provider is unstaked after the original EpochBlocks value, and not the new enlarged one (EpochBlocks = number of blocks in an epoch. This parameter is fixated)
+func TestUnstakeGovEpochBlocksIncrease(t *testing.T) {
+
+	// setup testnet with mock spec
+	ts := setupForPaymentTest(t)
+	ts.spec = common.CreateMockSpec()
+	ts.keepers.Spec.SetSpec(sdk.UnwrapSDKContext(ts.ctx), ts.spec)
+
+	// Add a staked provider and get its address
+	err := ts.addProvider(1)
+	require.Nil(t, err)
+	providerAddress := ts.providers[len(ts.providers)-1].address
+
+	// Verify the provider paid for its stake request
+	firstProviderCurrentFunds := ts.keepers.BankKeeper.GetBalance(sdk.UnwrapSDKContext(ts.ctx), providerAddress, epochstoragetypes.TokenDenom)
+	require.Equal(t, balance-stake, firstProviderCurrentFunds.Amount.Int64())
+
+	// Advance an epoch to apply the provider's stake and because gov params can't change in block 0 (this is a bug. In the time of this writing, it's not fixed)
+	ts.ctx = testkeeper.AdvanceEpoch(ts.ctx, ts.keepers) // blockHeight = 20
+
+	// change the EpochBlocks parameter to 10
+	epochBlocksFifty := uint64(50)
+	err = testkeeper.SimulateParamChange(sdk.UnwrapSDKContext(ts.ctx), ts.keepers.ParamsKeeper, epochstoragetypes.ModuleName, string(epochstoragetypes.KeyEpochBlocks), "\""+strconv.FormatUint(epochBlocksFifty, 10)+"\"")
+	require.Nil(t, err)
+
+	// Advance to blockHeight = 39, one block before the EpochBlocks change apply
+	for i := 0; i < 19; i++ {
+		ts.ctx = testkeeper.AdvanceBlock(ts.ctx, ts.keepers)
+	}
+
+	// Unstake the provider
+	_, err = ts.servers.PairingServer.UnstakeProvider(ts.ctx, &types.MsgUnstakeProvider{Creator: providerAddress.String(), ChainID: ts.spec.Index})
+	require.Nil(t, err)
+
+	// Advance to the next block so the EpochBlocks change apply
+	ts.ctx = testkeeper.AdvanceBlock(ts.ctx, ts.keepers) // blockHeight = 40
+
+	// Advance to blockHeight = 59. from its unstake, 20 blocks have passed so it should be unstaked (it staked when EpochBlocks was 20)
+	for i := 0; i < 19; i++ {
+		ts.ctx = testkeeper.AdvanceBlock(ts.ctx, ts.keepers)
+	}
+	epochProviderShouldBeUnstaked := uint64(sdk.UnwrapSDKContext(ts.ctx).BlockHeight())
+
+	// Advance to blockHeight = 90 to complete the epoch. from its stake, 51 blocks have passed so it should definitely be staked
+	for i := 0; i < 31; i++ {
+		ts.ctx = testkeeper.AdvanceBlock(ts.ctx, ts.keepers)
+	}
+	epochProviderShouldAlsoBeUnstaked := ts.keepers.Epochstorage.GetEpochStart(sdk.UnwrapSDKContext(ts.ctx))
+
+	// define tests
+	tests := []struct {
+		name           string
+		epoch          uint64
+		shouldBeStaked bool
+	}{
+		{"providerShouldntBeUnstaked", epochProviderShouldBeUnstaked, false},   // 20 blocks have passed since the stake - should be enough blocks
+		{"providerShouldBeUnstaked", epochProviderShouldAlsoBeUnstaked, false}, // 51 blocks have passed - enough blocks
 	}
 
 	sessionCounter := 0
