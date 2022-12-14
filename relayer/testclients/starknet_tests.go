d0f983aebcd7725853765ee207b34d7f44b81d66
oren-lava <oren@lavanet.xyz>
2022-12-14 10:55:53 +0200

    CNS-17: starknet client test

---
+++ relayer/testclients/starknet_tests.go
@@ -0,0 +1,40 @@
+package testclients
+
+import (
+	"context"
+	"time"
+
+	"github.com/lavanet/lava/utils"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/lavanet/lava/relayer/chainproxy"
+)
+
+const (
+	JSONRPC_STRK_BLOCKNUMBER        = `{"jsonrpc":"2.0","method":"starknet_blockNumber","params":[],"id":1}`
+	JSONRPC_STRK_BLOCKHASHANDNUMBER = `{"jsonrpc":"2.0","method":"starknet_blockHashAndNumber","params":[],"id":1}`
+)
+
+func StarknetTests(ctx context.Context, chainID string, rpcURL string, chainProxy chainproxy.ChainProxy, privKey *btcec.PrivateKey, testDuration time.Duration) error {
+	utils.LavaFormatInfo("Starting "+chainID+" Tests", nil)
+
+	for start := time.Now(); time.Since(start) < testDuration; {
+		for j := 0; j < 10; j++ {
+
+			reply, _, err := chainproxy.SendRelay(ctx, chainProxy, privKey, rpcURL, JSONRPC_STRK_BLOCKNUMBER, "")
+			if err != nil {
+				return utils.LavaFormatError("error starknet_blockNumber", err, nil)
+			}
+			prettyPrintReply(*reply, "JSONRPC_STRK_BLOCKNUMBER")
+
+			reply, _, err = chainproxy.SendRelay(ctx, chainProxy, privKey, rpcURL, JSONRPC_STRK_BLOCKHASHANDNUMBER, "")
+			if err != nil {
+				return utils.LavaFormatError("error starknet_blockHashAndNumber", err, nil)
+			}
+			prettyPrintReply(*reply, "JSONRPC_STRK_BLOCKHASHANDNUMBER")
+
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
