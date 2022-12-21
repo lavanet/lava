55368e2395da2e2724c94f3da7354e923d33937d
Ran Mishael <ran@lavanet.xyz>
2022-12-21 12:01:11 +0100

    PRT-191 adding indents for specs.

---
+++ scripts/automation_scripts/disable_and_validate_grpc_spec_api.py
@@ -0,0 +1,20 @@
+import json
+
+input_files = [
+    "/home/user/go/src/lava/cookbook/spec_add_cosmoshub.json",
+    "/home/user/go/src/lava/cookbook/spec_add_juno.json",
+    "/home/user/go/src/lava/cookbook/spec_add_lava.json",
+    "/home/user/go/src/lava/cookbook/spec_add_osmosis.json",
+]
+
+for f in input_files:
+    with open(f,"r") as fr:
+        data = json.load(fr)
+    for spec in data["specs"]:
+        for api in spec["apis"]:
+            for apiInterface in api["apiInterfaces"]:
+                if apiInterface["interface"] == "grpc":
+                    api["enabled"] = False
+
+    with open(f,"w") as fr:
+        json.dump(data,fr,indent=4)
---
+++ scripts/automation_scripts/indent_specs.py
@@ -0,0 +1,9 @@
+import glob
+import json
+all_specs = glob.glob("/home/user/go/src/lava/cookbook/*.json")
+
+for spec in all_specs:
+    with open(spec,"r") as fr:
+        data = json.load(fr)
+    with open(spec,"w") as fw:
+        data = json.dump(data,fw,indent=4)
