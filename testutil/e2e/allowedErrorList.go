76363b3dcb74f85ff56e234f7a271b121063fe7e
kajeagentspi <jmtagnepis@gmail.com>
2023-01-06 11:05:57 +0800

    Fix typo

--- testutil/e2e/allowedErrorList.go
+++ testutil/e2e/allowedErrorList.go
@@ -1,12 +1,12 @@
 package e2e
 
-// This is a map of error strings with the description why they are whitelisted
-// Detect extra text to be sure it is the whitelisted error
+// This is a map of error strings with the description why they are allowed
+// Detect extra text to be sure it is the allowed error
 var allowedErrors = map[string]string{
-	"getSupportedApi":        "This error is whitelisted because the tendermint uri tests have a test that checks if the error is catched.",
-	"No pairings available.": "This error is caused when the network is just booted up and pairings are not yet done. If after a few seconds the pairings are still not available the e2e would fail because the initial check if the provider is responsive would time out.",
-	`error connecting to provider error="context deadline exceeded"`: "This error is also caused by the initial bootup, continuous failure would be catched by the e2e so we can whitelist this error.",
-	"purging provider after all endpoints are disabled provider":     "This error is also caused by the initial bootup, continuous failure would be catched by the e2e so we can whitelist this error.",
-	"query hash mismatch on data reliability message":                "whitelisted temporarily because of data reliability",
-	"invalid pairing with consumer":                                  "whitelisted temporarily because of data reliability",
+	"getSupportedApi":        "This error is allowed because the Tendermint URI tests have a test that checks if the error is caught.",
+	"No pairings available.": "This error is allowed because when the network is just booted up and pairings are not yet done this would happen. If after a few seconds the pairings are still not available the e2e would fail because the initial check if the provider is responsive would time out.",
+	`error connecting to provider error="context deadline exceeded"`: "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
+	"purging provider after all endpoints are disabled provider":     "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
+	"query hash mismatch on data reliability message":                "This error is allowed temporarily because of data reliability",
+	"invalid pairing with consumer":                                  "This error is allowed temporarily because of data reliability",
 }
