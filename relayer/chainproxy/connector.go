5914276da45fa67255300521235ab10de39a5f80
Ran Mishael <ran@lavanet.xyz>
2023-02-01 18:36:51 +0100

    PRT-416 Fixing connector not failing when 0 connections were established

--- relayer/chainproxy/connector.go
+++ relayer/chainproxy/connector.go
@@ -78,6 +78,9 @@ func NewConnector(ctx context.Context, nConns uint, addr string) *Connector {
 		connector.freeClients = append(connector.freeClients, rpcClient)
 	}
 	utils.LavaFormatInfo("Number of parallel connections created: "+strconv.Itoa(len(connector.freeClients)), nil)
+	if len(connector.freeClients) == 0 {
+		utils.LavaFormatFatal("Could not create any connections to the node check address", nil, &map[string]string{"address": addr})
+	}
 	go connector.connectorLoop(ctx)
 	return connector
 }
@@ -225,6 +228,9 @@ func NewGRPCConnector(ctx context.Context, nConns uint, addr string) *GRPCConnec
 			break
 		}
 		connector.freeClients = append(connector.freeClients, grpcClient)
+	}
+	if len(connector.freeClients) == 0 {
+		utils.LavaFormatFatal("Could not create any connections to the node check address", nil, &map[string]string{"address": addr})
 	}
 	go connector.connectorLoop(ctx)
 	return connector
