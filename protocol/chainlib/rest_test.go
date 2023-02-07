50b8d2bed3dec7f448aae4682392e4a66339c416
Aleksao998 <aleksaopacic988@gmail.com>
2023-02-07 20:14:59 +0100

    add base tests for chainParser

---
+++ protocol/chainlib/rest_test.go
@@ -0,0 +1,61 @@
+package chainlib
+
+import (
+	"testing"
+	"time"
+
+	spectypes "github.com/lavanet/lava/x/spec/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRestChainParser_Spec(t *testing.T) {
+	// create a new instance of RestChainParser
+	apip, err := NewRestChainParser()
+	if err != nil {
+		t.Errorf("Error creating RestChainParser: %v", err)
+	}
+
+	// set the spec
+	spec := spectypes.Spec{
+		Enabled:                       true,
+		ReliabilityThreshold:          10,
+		AllowedBlockLagForQosSync:     11,
+		AverageBlockTime:              12,
+		BlockDistanceForFinalizedData: 13,
+		BlocksInFinalizationProof:     14,
+	}
+	apip.SetSpec(spec)
+
+	// fetch data reliability params
+	enabled, dataReliabilityThreshold := apip.DataReliabilityParams()
+
+	// fetch chain block stats
+	allowedBlockLagForQosSync, averageBlockTime, blockDistanceForFinalizedData, blocksInFinalizationProof := apip.ChainBlockStats()
+
+	// convert block time
+	AverageBlockTime := time.Duration(apip.spec.AverageBlockTime) * time.Second
+
+	// check that the spec was set correctly
+	assert.Equal(t, apip.spec.Enabled, enabled)
+	assert.Equal(t, apip.spec.GetReliabilityThreshold(), dataReliabilityThreshold)
+	assert.Equal(t, apip.spec.AllowedBlockLagForQosSync, allowedBlockLagForQosSync)
+	assert.Equal(t, apip.spec.BlockDistanceForFinalizedData, blockDistanceForFinalizedData)
+	assert.Equal(t, apip.spec.BlocksInFinalizationProof, blocksInFinalizationProof)
+	assert.Equal(t, AverageBlockTime, averageBlockTime)
+}
+
+func TestRestChainParser_NilGuard(t *testing.T) {
+	var apip *RestChainParser
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("apip methods missing nill guard, panicked with: %v", r)
+		}
+	}()
+
+	apip.SetSpec(spectypes.Spec{})
+	apip.DataReliabilityParams()
+	apip.ChainBlockStats()
+	apip.getSupportedApi("")
+	apip.ParseMsg("", []byte{}, "")
+}
