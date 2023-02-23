e9121e30dc3b309fa6b8990b226c287d879aa74f
oren-lava <oren@lavanet.xyz>
2023-02-23 21:36:11 +0200

    CNS-291: fixed unit tests

--- common/fixation_entry_test.go
+++ common/fixation_entry_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lavanet/lava/common"
@@ -46,14 +47,14 @@ func TestFixationEntryAdditionAndRemoval(t *testing.T) {
 	binary.LittleEndian.PutUint64(marshaledData, 1)
 	dummyFixationKey := "dummyFix_"
 	dummyIndex := "index"
-	dummyObj := types.Entry{Index: dummyIndex, Block: 10, Data: marshaledData, References: 0}
+	dummyObj := sdk.Coin{Denom: "utest", Amount: sdk.ZeroInt()}
 
 	// init VersionedStore + context
 	vs, ctx := initCtxAndVersionedStore(t)
 
 	// add dummy entry
 	blockToAddEntry := uint64(ctx.BlockHeight())
-	err := vs.AppendEntry(ctx, dummyFixationKey, dummyObj.GetIndex(), blockToAddEntry, &dummyObj)
+	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyObj)
 	require.Nil(t, err)
 
 	// get all entry indices and make sure there is only one index
@@ -61,23 +62,28 @@ func TestFixationEntryAdditionAndRemoval(t *testing.T) {
 	require.Equal(t, 1, len(indexList))
 
 	// get the entry from the storage
-	var tempEntry types.Entry
-	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, uint64(ctx.BlockHeight()), &tempEntry, types.DO_NOTHING)
+	var dummyCoin sdk.Coin
+	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyCoin, types.SUB_REFERENCE)
 	require.Nil(t, err)
 
 	// make sure that one entry's data is the same data that was used to create it
-	require.Equal(t, marshaledData, tempEntry.GetData())
+	require.True(t, dummyCoin.IsEqual(dummyObj))
 
 	// remove the entry by advancing over the STALE_ENTRY_TIME and appending a new one (append triggers the removal func)
-	ctx.WithBlockHeight(types.STALE_ENTRY_TIME + int64(blockToAddEntry) + 1)
-	dummyObj2 := types.Entry{Index: dummyIndex, Block: 10, Data: marshaledData, References: 1}
-	blockToAddEntry = uint64(ctx.BlockHeight())
-	err = vs.AppendEntry(ctx, dummyFixationKey, dummyObj2.GetIndex(), blockToAddEntry, &dummyObj)
+	ctx = ctx.WithBlockHeight(types.STALE_ENTRY_TIME + int64(blockToAddEntry) + 1)
+	dummyObj2 := sdk.Coin{Denom: "utest", Amount: sdk.OneInt()}
+	blockToAddEntryAfterStale := uint64(ctx.BlockHeight())
+	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntryAfterStale, &dummyObj2)
 	require.Nil(t, err)
 
 	// make sure there the old entry was deleted (check block)
-	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, dummyObj.Block, &tempEntry, types.SUB_REFERENCE)
-	require.Equal(t, dummyObj2.Block, tempEntry.Block)
+	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyCoin, types.DO_NOTHING)
+	require.NotNil(t, err)
+
+	// get the latest version and make sure it's equal to dummyObj2
+	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, blockToAddEntryAfterStale, &dummyCoin, types.DO_NOTHING)
+	require.Nil(t, err)
+	require.True(t, dummyCoin.IsEqual(dummyObj2))
 
 	// make sure dummy index is still in the entry index list
 	indexList = vs.GetAllEntryIndices(ctx, dummyFixationKey)
@@ -109,155 +115,142 @@ func TestAdditionOfTwoEntriesWithSameIndexInSameBlock(t *testing.T) {
 	require.Equal(t, 1, len(indexList))
 
 	// get the entry from the storage
-	var tempEntry sdk.Coin
-	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &tempEntry, types.DO_NOTHING)
+	var dummyCoin sdk.Coin
+	err = vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyCoin, types.DO_NOTHING)
 	require.Nil(t, err)
 
 	// make sure that one entry's data is the same data of the second dummy entry
-	require.True(t, tempEntry.IsEqual(dummyObj2))
+	require.True(t, dummyCoin.IsEqual(dummyObj2))
 
 	// make sure dummy index is still in the entry index list
 	indexList = vs.GetAllEntryIndices(ctx, dummyFixationKey)
 	require.Equal(t, 1, len(indexList))
 }
 
-// // Test adding entry versions and getting an older version
-// func TestEntryVersions(t *testing.T) {
-// 	// create dummy data for two dummy entries
-// 	marshaledData := make([]byte, 8)
-// 	binary.LittleEndian.PutUint64(marshaledData, 0)
-// 	marshaledData2 := make([]byte, 8)
-// 	binary.LittleEndian.PutUint64(marshaledData2, 1)
-// 	dummyIndex := "index"
-// 	dummyFixationKey := "dummyFix_"
-// 	dummyEntry := types.Entry{Index: "test", Block: 10, Data: marshaledData, References: 1}
-// 	dummyEntry2 := types.Entry{Index: "test", Block: 20, Data: marshaledData2, References: 1}
+// Test adding entry versions and getting an older version
+func TestEntryVersions(t *testing.T) {
+	// create dummy data for two dummy entries
+	dummyIndex := "index"
+	dummyFixationKey := "dummyFix_"
+	dummyObj := sdk.Coin{Denom: "utest", Amount: sdk.ZeroInt()}
+	dummyObj2 := sdk.Coin{Denom: "utest", Amount: sdk.OneInt()}
 
-// 	// init VersionedStore + context
-// 	vs, ctx := initCtxAndVersionedStore(t)
+	// init VersionedStore + context
+	vs, ctx := initCtxAndVersionedStore(t)
 
-// 	// add the first dummy entry
-// 	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry.Block, &dummyEntry)
-// 	require.Nil(t, err)
+	// add the first dummy entry
+	blockToAddFirstEntry := uint64(10)
+	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddFirstEntry, &dummyObj)
+	require.Nil(t, err)
 
-// 	// add the second dummy entry
-// 	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry2.Block, &dummyEntry2)
-// 	require.Nil(t, err)
+	// add the second dummy entry
+	blockToAddSecondEntry := uint64(20)
+	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddSecondEntry, &dummyObj2)
+	require.Nil(t, err)
 
-// 	// get all entries with dummyIndex and make sure there are two entry
-// 	var tempEntry types.Entry
-// 	entries := make([]*types.Entry, 0)
-// 	marshalers, err := vs.GetAllEntryVersionsByIndex(ctx, dummyFixationKey, dummyIndex, &tempEntry)
-// 	for _, marshaler := range marshalers {
-// 		entry := marshaler.(*types.Entry) // Type assertion to convert ProtoMarshaler to Entry
-// 		entries = append(entries, entry)
-// 	}
-// 	require.Nil(t, err)
-// 	require.Equal(t, 2, len(entries))
+	// get the older version from block blockToAddFirstEntry
+	var dummyCoin sdk.Coin
+	found := vs.GetEntry(ctx, dummyFixationKey, dummyIndex, blockToAddFirstEntry, &dummyCoin, types.DO_NOTHING)
+	require.True(t, found)
 
-// 	// get the older version from block 0
-// 	var oldEntry types.Entry
-// 	found, err := vs.GetEntryForBlock(ctx, dummyFixationKey, dummyIndex, 13, &oldEntry, types.DO_NOTHING)
-// 	require.True(t, found)
-// 	require.Nil(t, err)
+	// verify the data matches the old entry from storage
+	require.True(t, dummyCoin.IsEqual(dummyObj))
 
-// 	// verify the block and data matches the old entry from storage
-// 	require.Equal(t, 10, oldEntry.Block)
-// 	require.Equal(t, marshaledData, oldEntry.Data)
+	// make sure dummy index is still in the entry index list
+	indexList := vs.GetAllEntryIndices(ctx, dummyFixationKey)
+	require.Equal(t, 1, len(indexList))
+}
 
-// 	// make sure there is only one uniqueIndex object with this index
-// 	uniqueIndices := vs.GetAllFixationEntryUniqueIndex(ctx, dummyFixationKey)
-// 	require.Equal(t, 1, len(uniqueIndices))
-// }
+// Test adding entry versions with different fixation keys
+func TestDifferentFixationKeys(t *testing.T) {
+	// create dummy data for two dummy entries
+	dummyIndex := "index"
+	dummyFixationKey := "dummyFix_"
+	dummyFixationKey2 := "dummyFix2_"
+	dummyObj := sdk.Coin{Denom: "utest", Amount: sdk.ZeroInt()}
+	dummyObj2 := sdk.Coin{Denom: "utest", Amount: sdk.OneInt()}
+	blockToAddEntry := uint64(10)
+	// init VersionedStore + context
+	vs, ctx := initCtxAndVersionedStore(t)
 
-// // TODO: test sorting of entries when getting all, test that latest is really the latest
-// // Test adding entry versions with different fixation keys
-// func TestDifferentFixationKeys(t *testing.T) {
-// 	// create dummy data for two dummy entries
-// 	dummyIndex := "index"
-// 	dummyFixationKey := "dummyFix_"
-// 	dummyFixationKey2 := "dummyFix2_"
-// 	dummyEntry := types.Entry{Index: "test", Block: 10, Data: []byte{0xa}, References: 1}
-// 	dummyEntry2 := types.Entry{Index: "test", Block: 20, Data: []byte{0xb}, References: 1}
+	// add the first dummy entry
+	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyObj)
+	require.Nil(t, err)
 
-// 	// init VersionedStore + context
-// 	vs, ctx := initCtxAndVersionedStore(t)
+	// add the second dummy entry
+	err = vs.AppendEntry(ctx, dummyFixationKey2, dummyIndex, blockToAddEntry, &dummyObj2)
+	require.Nil(t, err)
 
-// 	// add the first dummy entry
-// 	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry.Block, &dummyEntry)
-// 	require.Nil(t, err)
+	// get all indices with dummyFixationKey and dummyIndex. make sure there is one entry
+	indexList := vs.GetAllEntryIndices(ctx, dummyFixationKey)
+	require.Equal(t, 1, len(indexList))
 
-// 	// add the second dummy entry
-// 	err = vs.AppendEntry(ctx, dummyFixationKey2, dummyIndex, dummyEntry2.Block, &dummyEntry2)
-// 	require.Nil(t, err)
+	// verify the data matches the entry from dummyFixationKey storage
+	var dummyCoin sdk.Coin
+	found := vs.GetEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyCoin, types.DO_NOTHING)
+	require.True(t, found)
+	require.True(t, dummyCoin.IsEqual(dummyObj))
 
-// 	// get all entries with dummyIndex and make sure there is one entry
-// 	var tempEntry types.Entry
-// 	entries := make([]*types.Entry, 0)
-// 	marshalers, err := vs.GetAllEntryVersionsByIndex(ctx, dummyFixationKey, dummyIndex, &tempEntry)
-// 	for _, marshaler := range marshalers {
-// 		entry := marshaler.(*types.Entry) // Type assertion to convert ProtoMarshaler to Entry
-// 		entries = append(entries, entry)
-// 	}
-// 	require.Nil(t, err)
-// 	require.Equal(t, 1, len(entries))
+	// get all indices with dummyFixationKey2 and dummyIndex. make sure there is one entry
+	indexList = vs.GetAllEntryIndices(ctx, dummyFixationKey2)
+	require.Equal(t, 1, len(indexList))
 
-// 	// verify the block and data matches the old entry from storage
-// 	require.Equal(t, uint64(10), entries[0].Block)
-// 	require.Equal(t, []byte{0xa}, entries[0].Data)
+	// verify the data matches the entry from dummyFixationKey storage
+	found = vs.GetEntry(ctx, dummyFixationKey2, dummyIndex, blockToAddEntry, &dummyCoin, types.DO_NOTHING)
+	require.True(t, found)
+	require.True(t, dummyCoin.IsEqual(dummyObj2))
+}
 
-// 	// make sure there is only one uniqueIndex object with this index
-// 	uniqueIndices := vs.GetAllFixationEntryUniqueIndex(ctx, dummyFixationKey)
-// 	require.Equal(t, 1, len(uniqueIndices))
-// }
+// Test that the appended entries are sorted (first element is oldest)
+func TestEntriesSort(t *testing.T) {
+	// create dummy data for two dummy entries
+	dummyIndex := "index"
+	dummyFixationKey := "dummyFix_"
+	dummyObj := sdk.Coin{Denom: "utest", Amount: sdk.ZeroInt()}
+	dummyObj2 := sdk.Coin{Denom: "utest", Amount: sdk.OneInt()}
+	dummyObj3 := sdk.Coin{Denom: "utest", Amount: sdk.OneInt().Mul(sdk.NewIntFromUint64(2))}
 
-// func TestEntriesSort(t *testing.T) {
-// 	// create dummy data for two dummy entries
-// 	dummyIndex := "index"
-// 	dummyFixationKey := "dummyFix_"
-// 	dummyEntry := types.Entry{Index: "test", Block: 10, Data: []byte{0xa}, References: 1}
-// 	dummyEntry2 := types.Entry{Index: "test", Block: 20, Data: []byte{0xb}, References: 1}
-// 	dummyEntry3 := types.Entry{Index: "test", Block: 30, Data: []byte{0xc}, References: 1}
+	// init VersionedStore + context
+	vs, ctx := initCtxAndVersionedStore(t)
 
-// 	// init VersionedStore + context
-// 	vs, ctx := initCtxAndVersionedStore(t)
+	// add the first dummy entry
+	blockToAddEntry := uint64(10)
+	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyObj)
+	require.Nil(t, err)
 
-// 	// add the first dummy entry
-// 	err := vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry.Block, &dummyEntry)
-// 	require.Nil(t, err)
+	// add the second dummy entry
+	blockToAddEntry += 10
+	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyObj2)
+	require.Nil(t, err)
 
-// 	// add the second dummy entry
-// 	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry2.Block, &dummyEntry2)
-// 	require.Nil(t, err)
+	// add the third dummy entry
+	blockToAddEntry += 10
+	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, blockToAddEntry, &dummyObj3)
+	require.Nil(t, err)
 
-// 	// add the third dummy entry
-// 	err = vs.AppendEntry(ctx, dummyFixationKey, dummyIndex, dummyEntry3.Block, &dummyEntry3)
-// 	require.Nil(t, err)
+	// get the relevant store and init an iterator and verify the entries are organized from oldest to latest (first element is oldest)
+	store := prefix.NewStore(ctx.KVStore(vs.GetStoreKey()), types.KeyPrefix(types.EntryKey+dummyFixationKey+dummyIndex))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+	defer iterator.Close()
 
-// 	// get all entries with dummyIndex and make sure there is one entry
-// 	var tempEntry types.Entry
-// 	entries := make([]*types.Entry, 0)
-// 	marshalers, err := vs.GetAllEntryVersionsByIndex(ctx, dummyFixationKey, dummyIndex, &tempEntry)
-// 	for _, marshaler := range marshalers {
-// 		entry := marshaler.(*types.Entry) // Type assertion to convert ProtoMarshaler to Entry
-// 		entries = append(entries, entry)
-// 	}
-// 	require.Nil(t, err)
-// 	require.Equal(t, 3, len(entries))
+	// iterate over entries
+	proposedBlock := uint64(0)
+	sorted := false
+	var oldEntry types.Entry
+	for ; iterator.Valid(); iterator.Next() {
+		// umarshal the old entry version
+		vs.GetCdc().MustUnmarshal(iterator.Value(), &oldEntry)
 
-// 	// verify the entries are organized from oldest to latest (first element is oldest)
-// 	proposedBlock := uint64(0)
-// 	sorted := false
-// 	for _, entry := range entries {
-// 		if proposedBlock < entry.Block {
-// 			sorted = true
-// 		} else {
-// 			sorted = false
-// 		}
-// 		proposedBlock = entry.Block
-// 	}
-// 	require.True(t, sorted)
+		// proposedBlock should always be smaller than the next entry's block if the elements in the store are sorted
+		if proposedBlock < oldEntry.Block {
+			sorted = true
+		} else {
+			sorted = false
+		}
+		proposedBlock = oldEntry.Block
+	}
+	require.True(t, sorted)
 
-// 	// verify the last element is the latest entry
-// 	require.Equal(t, dummyEntry3.Block, proposedBlock)
-// }
+	// verify the last element is the latest entry
+	require.Equal(t, oldEntry.Block, proposedBlock)
+}
