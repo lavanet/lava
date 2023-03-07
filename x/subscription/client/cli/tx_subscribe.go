ea4cf946596624f5edb947081e095cc1e18b9d0b
Oren Laadan <orenl@lavanet.xyz>
2023-03-07 17:21:03 -0500

    CNS-175: allow optional args in subscription cli
    subscribe:
      lavad tx subscription subscribe PLAN [CONSUMER] [IS_YEARLY] [flags]
    
    - PLAN is mandatory
    - CONSUMER is optional (default is creator's address)
    - IS_YEARLY is optional (default is false)
    
    current-subscription:
      lavad query subscription current-subscription [CONSUMER]
    
    - CONSUMER is optional (default is creator's address)

--- x/subscription/client/cli/tx_subscribe.go
+++ x/subscription/client/cli/tx_subscribe.go
@@ -1,8 +1,6 @@
 package cli
 
 import (
-	"strconv"
-
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
@@ -11,28 +9,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var _ = strconv.Itoa(0)
-
 func CmdSubscribe() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "subscribe [consumer] [index] [is-yearly]",
+		Use:   "subscribe [index] [consumer] [is-yearly]",
 		Short: "Subscribe to a service plan",
-		Args:  cobra.ExactArgs(3),
+		Args:  cobra.RangeArgs(1, 3),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argConsumer := args[0]
-			argIndex := args[1]
-			argIsYearly, err := cast.ToBoolE(args[2])
-			if err != nil {
-				return err
-			}
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
+			creator := clientCtx.GetFromAddress().String()
+			argIndex := args[0]
+
+			argConsumer := creator
+			if len(args) >= 2 {
+				argConsumer = args[1]
+			}
+
+			argIsYearly := false
+			if len(args) == 3 {
+				argIsYearly, err = cast.ToBoolE(args[2])
+				if err != nil {
+					return err
+				}
+			}
+
 			msg := types.NewMsgSubscribe(
-				clientCtx.GetFromAddress().String(),
+				creator,
 				argConsumer,
 				argIndex,
 				argIsYearly,
