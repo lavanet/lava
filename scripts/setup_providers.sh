73e58acbb9e9c07ea95552f28f6086492ee13ee1
Omer <100387053+omerlavanet@users.noreply.github.com>
2023-03-08 18:27:47 +0200

    CNS-329-add-sui-spec (#345)
    * add suit spec
    
    * we are using get and not post for osme reason on type..
    
    * added parsing support for non string unmarshaling

--- scripts/setup_providers.sh
+++ scripts/setup_providers.sh
@@ -66,10 +66,16 @@ screen -S polygon_providers -X screen -t win2 -X bash -c "source ~/.bashrc; lava
 screen -d -m -S optimism_providers bash -c "source ~/.bashrc; lavad server 127.0.0.1 6003 $OPTIMISM_RPC OPTM jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer1 2>&1 | tee $LOGS_DIR/OPTM_6003.log"
 screen -S optimism_providers -X screen -t win1 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6004 $OPTIMISM_RPC OPTM jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer2 2>&1 | tee $LOGS_DIR/OPTM_6004.log"
 screen -S optimism_providers -X screen -t win2 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6005 $OPTIMISM_RPC OPTM jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer3 2>&1 | tee $LOGS_DIR/OPTM_6005.log"
+
 # Base providers
 screen -d -m -S base_providers bash -c "source ~/.bashrc; lavad server 127.0.0.1 6000 $BASE_GOERLI_RPC BASET jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer1 2>&1 | tee $LOGS_DIR/BASET_6000.log"
 screen -S base_providers -X screen -t win1 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6001 $BASE_GOERLI_RPC BASET jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer2 2>&1 | tee $LOGS_DIR/BASET_6001.log"
 screen -S base_providers -X screen -t win2 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6002 $BASE_GOERLI_RPC BASET jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer3 2>&1 | tee $LOGS_DIR/BASET_6002.log"
+
+# SUI providers
+screen -d -m -S sui_providers bash -c "source ~/.bashrc; lavad server 127.0.0.1 6500 $SUI_RPC SUIT jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer1 2>&1 | tee $LOGS_DIR/SUIT_6500.log"
+screen -S sui_providers -X screen -t win1 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6501 $SUI_RPC SUIT jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer2 2>&1 | tee $LOGS_DIR/SUIT_6501.log"
+screen -S sui_providers -X screen -t win2 -X bash -c "source ~/.bashrc; lavad server 127.0.0.1 6502 $SUI_RPC SUIT jsonrpc $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer3 2>&1 | tee $LOGS_DIR/SUIT_6502.log"
 
 # Cosmos-SDK Chains
 
@@ -168,6 +174,7 @@ screen -S portals -X screen -t win15 -X bash -c "source ~/.bashrc; lavad rpccons
 screen -S portals -X screen -t win18 -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3351 POLYGON1 jsonrpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_3351.log"
 screen -S portals -X screen -t win21 -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3362 OPTM jsonrpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_OPTM.log"
 screen -S portals -X screen -t win20 -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3361 BASET jsonrpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_BASET.log"
+screen -S portals -X screen -t win20 -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3364 SUIT jsonrpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_SUIT.log"
 # Cosmos-SDK based chains
 screen -S portals -X screen -t win1  -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3334 COS3 rest 127.0.0.1:3335 COS3 tendermintrpc 127.0.0.1:3353 COS3 grpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_COS3_3334.log"
 screen -S portals -X screen -t win4  -X bash -c "source ~/.bashrc; lavad rpcconsumer 127.0.0.1:3337 COS4 rest 127.0.0.1:3338 COS4 tendermintrpc 127.0.0.1:3354 COS4 grpc $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 2>&1 | tee $LOGS_DIR/PORTAL_COS4_3337.log"
