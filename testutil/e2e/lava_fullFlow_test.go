4c33a4e9884946597f5069844167b2b5d1bc3c82
Oren Laadan <orenl@lavanet.xyz>
2023-04-02 16:36:55 +0800

    CNS-337: sort out test timeout
    The e2e timeout was inconsistent:
    
    First, "go test" default timeout is 10 minutes, which it can be changed using
    "go test -timeout ...". However, the e2e test internally runs another "go test"
    without any argument, so even if the user specified a longer timeout for the
    test, the latter would fail within the default timeout.
    
    Second, in at least one place the timeout was hard-coded fixed to 10 minutes.
    
    This is fixed by settings the timeout the intenral "go test" to zero, and then
    insteead settings the context.WithTimeout to the timeout requested by the user.

--- testutil/e2e/lava_fullFlow_test.go
+++ testutil/e2e/lava_fullFlow_test.go
@@ -2,8 +2,16 @@ package e2e
 
 import (
 	"testing"
+	"time"
 )
 
 func TestLava(t *testing.T) {
-	runE2E()
+	// default timeout same as `go test`
+	timeout := time.Minute * 10
+
+	if deadline, ok := t.Deadline(); ok {
+		timeout = deadline.Sub(time.Now()).Round(10*time.Second)
+	}
+
+	runE2E(timeout)
 }
