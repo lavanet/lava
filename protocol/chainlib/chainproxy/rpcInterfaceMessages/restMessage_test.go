d7d0b6918647c6009f28130ab232948a1955e385
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-04-06 12:36:19 +0300

    protocol test fix

--- protocol/chainlib/chainproxy/rpcInterfaceMessages/restMessage_test.go
+++ protocol/chainlib/chainproxy/rpcInterfaceMessages/restMessage_test.go
@@ -7,14 +7,15 @@ import (
 func TestRestMessage(t *testing.T) {
 	// Test GetParams method
 	restMessage := RestMessage{
-		Path: "eth_getTransactionByHash",
-		Msg:  []byte{1, 2, 3, 4, 5},
+		Path:     "eth_getTransactionByHash",
+		Msg:      []byte{1, 2, 3, 4, 5},
+		SpecPath: "eth_getTransactionByHash",
 	}
 
 	// Test GetParams method
 	params := restMessage.GetParams()
-	if params != nil {
-		t.Errorf("Expected nil, but got %v", params)
+	if params == nil {
+		t.Errorf("Expected params, but got nil")
 	}
 
 	// Test GetResult method
