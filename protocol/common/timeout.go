ed7d0d0d1a90e909411c585ac5311969fefba0f0
Ran Mishael <ran@lavanet.xyz>
2023-04-09 15:55:55 +0200

    added timeout

---
+++ protocol/common/timeout.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"context"
+	"math"
+	"time"
+)
+
+func GetRemainingTimeoutFromContext(ctx context.Context) (timeRemaining time.Duration) {
+	deadline, ok := ctx.Deadline()
+	if ok {
+		return time.Until(deadline)
+	}
+	return time.Duration(math.MaxInt64)
+}
+
+func LowerContextTimeout(ctx context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+	if GetRemainingTimeoutFromContext(ctx) > timeout {
+		return context.WithTimeout(ctx, timeout)
+	}
+	return context.WithCancel(ctx)
+}
