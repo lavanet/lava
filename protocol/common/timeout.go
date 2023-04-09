61c3b71c6145b7d15d269592f148a3a8fc8ea2fe
Ran Mishael <ran@lavanet.xyz>
2023-04-09 17:36:30 +0200

    fixing timeout.

--- protocol/common/timeout.go
+++ protocol/common/timeout.go
@@ -2,9 +2,14 @@ package common
 
 import (
 	"context"
+	"errors"
 	"math"
 	"time"
 )
+
+func ContextOutOfTime(ctx context.Context) bool {
+	return errors.Is(ctx.Err(), context.DeadlineExceeded)
+}
 
 func GetRemainingTimeoutFromContext(ctx context.Context) (timeRemaining time.Duration) {
 	deadline, ok := ctx.Deadline()
--- protocol/rpcprovider/rpcprovider_server.go
+++ protocol/rpcprovider/rpcprovider_server.go
@@ -105,23 +105,29 @@ func (rpcps *RPCProviderServer) Relay(ctx context.Context, request *pairingtypes
 
 	// Try sending relay
 	reply, err := rpcps.TryRelay(ctx, request, consumerAddress, chainMessage)
-	if err != nil {
+
+	if err != nil || common.ContextOutOfTime(ctx) {
 		// failed to send relay. we need to adjust session state. cuSum and relayNumber.
 		relayFailureError := rpcps.providerSessionManager.OnSessionFailure(relaySession, request.RelaySession.RelayNum)
 		if relayFailureError != nil {
-			err = sdkerrors.Wrapf(relayFailureError, "On relay failure: "+err.Error())
+			var extraInfo string
+			if err != nil {
+				extraInfo = err.Error()
+			}
+			err = sdkerrors.Wrapf(relayFailureError, "On relay failure: "+extraInfo)
 		}
 		err = utils.LavaFormatError("TryRelay Failed", err,
 			utils.Attribute{Key: "request.SessionId", Value: request.RelaySession.SessionId},
 			utils.Attribute{Key: "request.userAddr", Value: consumerAddress},
 			utils.Attribute{Key: "GUID", Value: ctx},
+			utils.Attribute{Key: "timed_out", Value: common.ContextOutOfTime(ctx)},
 		)
 	} else {
 		// On successful relay
 		pairingEpoch := relaySession.PairingEpoch
 		relayError := rpcps.providerSessionManager.OnSessionDone(relaySession, request.RelaySession.RelayNum)
 		if relayError != nil {
-			err = sdkerrors.Wrapf(relayError, "OnSession Done failure: "+err.Error())
+			utils.LavaFormatError("OnSession Done failure: ", relayError)
 		} else {
 			if request.DataReliability == nil {
 				// SendProof gets the request copy, as in the case of data reliability enabled the request.blockNumber is changed.
