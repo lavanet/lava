80adc1f0c819336483e0cb61f76ab898f6e84d95
omer mishael <omer@lavanet.xyz>
2023-04-18 17:55:27 +0300

    added support for bulk changes on multiple chains

--- x/pairing/client/cli/tx_stake_provider.go
+++ x/pairing/client/cli/tx_stake_provider.go
@@ -18,6 +18,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	BULK_ARG_COUNT = 4
+)
+
 var _ = strconv.Itoa(0)
 
 func CmdStakeProvider() *cobra.Command {
@@ -89,44 +93,29 @@ func CmdStakeProvider() *cobra.Command {
 
 func CmdBulkStakeProvider() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "bulk-stake-provider [chain-id,chain-id,chain-id...] [amount] [endpoint-url,geolocation endpoint-url,geolocation] [geolocation] --from <address> --provider-moniker <moniker>",
-		Short: "used to stake with a provider on a given endpoint in all of it's api interfaces and all chains with the same stake, each chain will require it's own stake",
+		Use:   "bulk-stake-provider [chain-id,chain-id,chain-id...] [amount] [endpoint-url,geolocation endpoint-url,geolocation] [geolocation] {repeat arguments for another bulk} --from <address> --provider-moniker <moniker>",
+		Short: "used to stake with a provider on a given endpoint in all of it's api interfaces and all chains with the same stake, each chain will require it's own stake, this command can be run in bulk, by repeating the arguments",
 		Long: `args:
 		[chain-id,chain-id] is the specs the provider wishes to support separated by a ','
 		[amount] is the ulava amount to be staked
 		[endpoint-url,geolocation endpoint-url,geolocation...] are a space separated list of HOST:PORT,geolocation, should be defined within "quotes"
-		[geolocation] should be the geolocation code to be staked for`,
-		Args: cobra.ExactArgs(4),
+		[geolocation] should be the geolocation code to be staked for
+		{repeat for another bulk} - creates a new Msg within the transaction with different arguments, can be used to run many changes in many chains without waiting for a new block`,
+		Example: `lavad tx pairing bulk-stake-provider ETH1,LAV1 500000ulava "my-provider-grpc-addr.com:9090,1" 1 -y --from servicer1 --provider-moniker "my-moniker" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+		bulk send: two bulks, listen for ETH1,LAV1 in one endpoint and COS3,COS5 in another
+		lavad tx pairing bulk-stake-provider ETH1,LAV1 500000ulava "my-provider-grpc-addr.com:9090,1" 1 COS3,COS5 500000ulava "my-other-grpc-addr.com:1111,1" 1 -y --from servicer1 --provider-moniker "my-moniker" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args)%BULK_ARG_COUNT != 0 {
+				return fmt.Errorf("invalid number of arguments, needs to be a multiple of %d, arg count: %d", BULK_ARG_COUNT, len(args))
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argChainIDs := args[0]
-			chainIDs := strings.Split(argChainIDs, ",")
-			argAmount, err := sdk.ParseCoinNormalized(args[1])
-			if err != nil {
-				return err
-			}
-			tmpArg := strings.Fields(args[2])
-			argEndpoints := map[uint64]epochstoragetypes.Endpoint{}
-			for _, endpointStr := range tmpArg {
-				splitted := strings.Split(endpointStr, ",")
-				if len(splitted) != 2 {
-					return fmt.Errorf("invalid argument format in endpoints, must be: HOST:PORT,geolocation HOST:PORT,geolocation, received: %s", endpointStr)
-				}
-				geoloc, err := strconv.ParseUint(splitted[1], 10, 64)
-				if err != nil {
-					return fmt.Errorf("invalid argument format in endpoints, geolocation must be a number")
-				}
-				endpoint := epochstoragetypes.Endpoint{IPPORT: splitted[0], UseType: "STUB", Geolocation: geoloc}
-				argEndpoints[geoloc] = endpoint
-			}
-			argGeolocation, err := cast.ToUint64E(args[3])
-			if err != nil {
-				return err
-			}
+
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
-
 			moniker, err := cmd.Flags().GetString(types.FlagMoniker)
 			if err != nil {
 				return err
@@ -136,39 +125,80 @@ func CmdBulkStakeProvider() *cobra.Command {
 			if err != nil {
 				return utils.LavaFormatError("could not get the list of all chains, in order to construct the transaction", err)
 			}
-			chainsToEndpointsMap := map[string][]string{}
-			for _, chainStructInfo := range allChains.ChainInfoList {
-				chainsToEndpointsMap[chainStructInfo.ChainID] = chainStructInfo.EnabledApiInterfaces
+
+			handleBulk := func(cmd *cobra.Command, args []string) (msgs []sdk.Msg, err error) {
+				if len(args) != BULK_ARG_COUNT {
+					return nil, fmt.Errorf("invalid argument length %d should be %d", len(args), BULK_ARG_COUNT)
+				}
+				argChainIDs := args[0]
+				chainIDs := strings.Split(argChainIDs, ",")
+				argAmount, err := sdk.ParseCoinNormalized(args[1])
+				if err != nil {
+					return nil, err
+				}
+				tmpArg := strings.Fields(args[2])
+				argEndpoints := map[uint64]epochstoragetypes.Endpoint{}
+				for _, endpointStr := range tmpArg {
+					splitted := strings.Split(endpointStr, ",")
+					if len(splitted) != 2 {
+						return nil, fmt.Errorf("invalid argument format in endpoints, must be: HOST:PORT,geolocation HOST:PORT,geolocation, received: %s", endpointStr)
+					}
+					geoloc, err := strconv.ParseUint(splitted[1], 10, 64)
+					if err != nil {
+						return nil, fmt.Errorf("invalid argument format in endpoints, geolocation must be a number")
+					}
+					endpoint := epochstoragetypes.Endpoint{IPPORT: splitted[0], UseType: "STUB", Geolocation: geoloc}
+					argEndpoints[geoloc] = endpoint
+				}
+				argGeolocation, err := cast.ToUint64E(args[3])
+				if err != nil {
+					return nil, err
+				}
+
+				chainsToEndpointsMap := map[string][]string{}
+				for _, chainStructInfo := range allChains.ChainInfoList {
+					chainsToEndpointsMap[chainStructInfo.ChainID] = chainStructInfo.EnabledApiInterfaces
+				}
+				for _, chainID := range chainIDs {
+					if chainID == "" {
+						continue
+					}
+					interfacesForThisChainID := chainsToEndpointsMap[chainID]
+					allEndpoints := []epochstoragetypes.Endpoint{}
+					for geoloc, endpointForGeoloc := range argEndpoints {
+						endpoints := make([]epochstoragetypes.Endpoint, len(interfacesForThisChainID))
+						for idx, interfaceName := range interfacesForThisChainID {
+							endpoints[idx] = epochstoragetypes.Endpoint{IPPORT: endpointForGeoloc.IPPORT, Geolocation: geoloc, UseType: interfaceName}
+						}
+						allEndpoints = append(allEndpoints, endpoints...)
+					}
+
+					msg := types.NewMsgStakeProvider(
+						clientCtx.GetFromAddress().String(),
+						chainID,
+						argAmount,
+						allEndpoints,
+						argGeolocation,
+						moniker,
+					)
+					if err := msg.ValidateBasic(); err != nil {
+						return nil, err
+					}
+					msgs = append(msgs, msg)
+				}
+				return msgs, nil
 			}
 			msgs := []sdk.Msg{}
-			for _, chainID := range chainIDs {
-				if chainID == "" {
-					continue
+			for argRange := 0; argRange < len(args); argRange += BULK_ARG_COUNT {
+				if argRange+BULK_ARG_COUNT > len(args) {
+					return fmt.Errorf("invalid argument count %d > %d", argRange+BULK_ARG_COUNT, len(args))
 				}
-				interfacesForThisChainID := chainsToEndpointsMap[chainID]
-				allEndpoints := []epochstoragetypes.Endpoint{}
-				for geoloc, endpointForGeoloc := range argEndpoints {
-					endpoints := make([]epochstoragetypes.Endpoint, len(interfacesForThisChainID))
-					for idx, interfaceName := range interfacesForThisChainID {
-						endpoints[idx] = epochstoragetypes.Endpoint{IPPORT: endpointForGeoloc.IPPORT, Geolocation: geoloc, UseType: interfaceName}
-					}
-					allEndpoints = append(allEndpoints, endpoints...)
-				}
-
-				msg := types.NewMsgStakeProvider(
-					clientCtx.GetFromAddress().String(),
-					chainID,
-					argAmount,
-					allEndpoints,
-					argGeolocation,
-					moniker,
-				)
-				if err := msg.ValidateBasic(); err != nil {
+				newMsgs, err := handleBulk(cmd, args[argRange:argRange+BULK_ARG_COUNT])
+				if err != nil {
 					return err
 				}
-				msgs = append(msgs, msg)
+				msgs = append(msgs, newMsgs...)
 			}
-
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msgs...)
 		},
 	}
