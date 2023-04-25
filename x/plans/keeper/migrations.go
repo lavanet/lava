b3a8eebd4137a556b00b8d9d51fe92624dcfe30d
oren-lava <oren@lavanet.xyz>
2023-04-25 12:38:00 +0300

    CNS-407: implemented plans migrator

--- x/plans/keeper/migrations.go
+++ x/plans/keeper/migrations.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v3 "github.com/lavanet/lava/x/plans/migrations/v3"
+	"github.com/lavanet/lava/x/plans/types"
+	projecttypes "github.com/lavanet/lava/x/projects/types"
 )
 
 type Migrator struct {
@@ -19,6 +22,42 @@ func NewMigrator(keeper Keeper) Migrator {
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 	if err := m.keeper.plansFS.MigrateVersion(ctx); err != nil {
 		return fmt.Errorf("%w: plans fixation-store", err)
+	}
+	return nil
+}
+
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	planIndices := m.keeper.GetAllPlanIndices(ctx)
+
+	for _, planIndex := range planIndices {
+		var oldPlanStruct v3.Plan
+		if found := m.keeper.plansFS.FindEntry(ctx, planIndex, uint64(ctx.BlockHeight()), &oldPlanStruct); !found {
+			return fmt.Errorf("could not find plan with index %s", planIndex)
+		}
+
+		planPolicy := projecttypes.Policy{
+			GeolocationProfile: uint64(1),
+			TotalCuLimit:       oldPlanStruct.ComputeUnits,
+			EpochCuLimit:       oldPlanStruct.ComputeUnitsPerEpoch,
+			MaxProvidersToPair: oldPlanStruct.MaxProvidersToPair,
+		}
+
+		newPlanStruct := types.Plan{
+			Index:                    oldPlanStruct.Index,
+			Block:                    oldPlanStruct.Block,
+			Price:                    oldPlanStruct.Price,
+			OveruseRate:              oldPlanStruct.OveruseRate,
+			AllowOveruse:             oldPlanStruct.AllowOveruse,
+			Description:              oldPlanStruct.Description,
+			Type:                     oldPlanStruct.Type,
+			AnnualDiscountPercentage: oldPlanStruct.AnnualDiscountPercentage,
+			PlanPolicy:               planPolicy,
+		}
+
+		err := m.keeper.plansFS.ModifyEntry(ctx, planIndex, uint64(ctx.BlockHeight()), &newPlanStruct)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
