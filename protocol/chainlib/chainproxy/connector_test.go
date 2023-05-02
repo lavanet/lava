000eaa24dc30c5c31297679a4a0d36909d2c0092
Ran Mishael <106548467+ranlavanet@users.noreply.github.com>
2023-05-02 10:26:04 +0200

    PRT-579 provider hosted node grpc secured (#458)
    * adding grpc secured WIP
    
    * modified grpc connector to supp tls
    
    * WIP grpc TLS
    
    * removing comments
    
    * WIP on TLS issue
    
    * merge main
    
    * adding TLS support for grpc (cacert + cert + default cert)
    
    * removed some unused lines from test

--- protocol/chainlib/chainproxy/connector_test.go
+++ protocol/chainlib/chainproxy/connector_test.go
@@ -11,12 +11,15 @@ import (
 
 	"github.com/lavanet/lava/protocol/chainlib/chainproxy/rpcclient"
 	"github.com/lavanet/lava/protocol/common"
+	pb_pkg "github.com/lavanet/lava/x/spec/types"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 const (
 	listenerAddress    = "localhost:1234"
+	port               = "1234"
 	listenerAddressTcp = "http://localhost:1234"
 	numberOfClients    = 5
 )
@@ -35,6 +38,28 @@ func createGRPCServer(t *testing.T) *grpc.Server {
 	lis, err := net.Listen("tcp", listenerAddress)
 	require.Nil(t, err)
 	s := grpc.NewServer()
+	go s.Serve(lis) // serve in a different thread
+	return s
+}
+
+type implementedLavanetLavaSpec struct {
+	pb_pkg.UnimplementedQueryServer
+}
+
+func (is *implementedLavanetLavaSpec) ShowChainInfo(ctx context.Context, req *pb_pkg.QueryShowChainInfoRequest) (*pb_pkg.QueryShowChainInfoResponse, error) {
+	md := metadata.New(map[string]string{"content-type": "text/html"})
+	grpc.SendHeader(ctx, md)
+
+	result := &pb_pkg.QueryShowChainInfoResponse{ChainID: "Test"}
+	return result, nil
+}
+
+func createGRPCServerWithRegisteredProto(t *testing.T) *grpc.Server {
+	lis, err := net.Listen("tcp", listenerAddress)
+	require.Nil(t, err)
+	s := grpc.NewServer()
+	lavanetlavaspec := &implementedLavanetLavaSpec{}
+	pb_pkg.RegisterQueryServer(s, lavanetlavaspec)
 	go s.Serve(lis) // serve in a different thread
 	return s
 }
@@ -111,4 +136,34 @@ func TestConnectorGrpc(t *testing.T) {
 	}
 	require.Equal(t, int(conn.usedClients), 0)                // checking we dont have clients used
 	require.Equal(t, increasedClients, len(conn.freeClients)) // checking we cleaned clients
+}
+
+func TestConnectorGrpcAndInvoke(t *testing.T) {
+	server := createGRPCServerWithRegisteredProto(t) // create a grpcServer so we can connect to its endpoint and validate everything works.
+	defer server.Stop()
+	ctx := context.Background()
+	conn, err := NewGRPCConnector(ctx, numberOfClients, common.NodeUrl{Url: listenerAddress})
+	require.Nil(t, err)
+	for { // wait for the routine to finish connecting
+		if len(conn.freeClients) == numberOfClients {
+			break
+		}
+	}
+	// require.Equal(t, len(conn.freeClients), numberOfClients)
+	increasedClients := numberOfClients * 2 // increase to double the number of clients
+	rpcList := make([]*grpc.ClientConn, increasedClients)
+	for i := 0; i < increasedClients; i++ {
+		rpc, err := conn.GetRpc(ctx, true)
+		require.Nil(t, err)
+		rpcList[i] = rpc
+		response := &pb_pkg.QueryShowChainInfoResponse{}
+		err = grpc.Invoke(ctx, "lavanet.lava.spec.Query/ShowChainInfo", &pb_pkg.QueryShowChainInfoRequest{}, response, rpc)
+		require.Equal(t, "Test", response.ChainID)
+		require.Nil(t, err)
+	}
+	require.Equal(t, increasedClients, int(conn.usedClients)) // checking we have used clients
+	for i := 0; i < increasedClients; i++ {
+		conn.ReturnRpc(rpcList[i])
+	}
+	require.Equal(t, int(conn.usedClients), 0) // checking we dont have clients used
 }
