e2d9237e282edfc7a0e72ebb7a3e7b1e1af2a918
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-05-07 09:39:44 +0000

    rename

--- x/pairing/keeper/msg_server_freeze.go
+++ x/pairing/keeper/msg_server_freeze.go
@@ -12,7 +12,7 @@ import (
 	"github.com/lavanet/lava/x/pairing/types"
 )
 
-const FROZEN_BLOCK = math.MaxInt64
+const FrozenBlock = math.MaxInt64
 
 func (k msgServer) FreezeProvider(goCtx context.Context, msg *types.MsgFreezeProvider) (*types.MsgFreezeProviderResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
@@ -35,7 +35,7 @@ func (k Keeper) FreezeProvider(ctx sdk.Context, provider string, chainIDs []stri
 		}
 
 		// freeze the provider by making the StakeAppliedBlock be max. This will remove the provider from the pairing list in the next epoch
-		stakeEntry.StakeAppliedBlock = FROZEN_BLOCK
+		stakeEntry.StakeAppliedBlock = FrozenBlock
 		k.epochStorageKeeper.ModifyStakeEntryCurrent(ctx, epochstoragetypes.ProviderKey, chainId, stakeEntry, index)
 	}
 
