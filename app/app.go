4e76e9c94fd27ad672de42fb160cd3f21fced5ea
Ran Mishael <ran@lavanet.xyz>
2023-05-11 17:46:45 +0200

    forgot handler

--- app/app.go
+++ app/app.go
@@ -157,6 +157,7 @@ var Upgrades = []upgrades.Upgrade{
 	upgrades.Upgrade_0_10_0,
 	upgrades.Upgrade_0_10_1,
 	upgrades.Upgrade_0_11_0,
+	upgrades.Upgrade_0_11_1,
 }
 
 // this line is used by starport scaffolding # stargate/wasm/app/enabledProposals
