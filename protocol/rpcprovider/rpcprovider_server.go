0d456af155c05f369ff65bbae5d4d3c8beae7e94
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-05-22 01:50:31 +0300

    append lint fix

--- protocol/rpcprovider/rpcprovider_server.go
+++ protocol/rpcprovider/rpcprovider_server.go
@@ -167,9 +167,7 @@ func (rpcps *RPCProviderServer) initRelay(ctx context.Context, request *pairingt
 	var metaDataMap []pairingtypes.Metadata
 	relayMetaData := request.RelayData.GetMetadata()
 	if len(relayMetaData) > 0 {
-		for _, metaData := range relayMetaData {
-			metaDataMap = append(metaDataMap, metaData)
-		}
+		metaDataMap = append(metaDataMap, relayMetaData...)
 	}
 	// parse the message to extract the cu and chainMessage for sending it
 	chainMessage, err = rpcps.chainParser.ParseMsg(request.RelayData.ApiUrl, request.RelayData.Data, request.RelayData.ConnectionType, metaDataMap)
