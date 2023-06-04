3fa85ca6f94702b83381f3a23a824b9ff5cd5c32
oren-lava <oren@lavanet.xyz>
2023-06-04 18:05:23 +0300

    fixed remove subscription

--- x/subscription/keeper/epoch_start.go
+++ x/subscription/keeper/epoch_start.go
@@ -37,9 +37,6 @@ func (k Keeper) EpochStart(ctx sdk.Context) {
 	})
 
 	for _, sub := range subExpired {
-		sub.PrevExpiryBlock = block
-		sub.PrevCuLeft = sub.MonthCuLeft
-
 		// subscription has been dead for EpochsToSave epochs: delete
 		// TODO: disable all projects registered in this subscription
 		// TODO: THIS WILL BE HANDLED AUTOMATICALLY BY FIXATION-STORE
@@ -48,6 +45,9 @@ func (k Keeper) EpochStart(ctx sdk.Context) {
 			k.RemoveSubscription(ctx, sub.Consumer)
 			continue
 		}
+
+		sub.PrevExpiryBlock = block
+		sub.PrevCuLeft = sub.MonthCuLeft
 
 		if sub.DurationLeft == 0 {
 			panic("Subscription: EpochStart: negative DurationLeft for consumer " + sub.Consumer)
