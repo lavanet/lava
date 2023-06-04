878bc2d34d278d79cad532c772911052a230a41a
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-06-04 15:40:16 +0300

    badge signer validation added

--- protocol/rpcprovider/rpcprovider_server.go
+++ protocol/rpcprovider/rpcprovider_server.go
@@ -326,6 +326,12 @@ func (rpcps *RPCProviderServer) verifyRelaySession(ctx context.Context, request 
 	var consumerAddressString string
 
 	if request.RelaySession.Badge != nil {
+		// Check-1: badge signer == badge.Address
+		badgeUserSigner, _ := sigs.ExtractSignerAddress(request.RelaySession)
+		if badgeUserSigner.String() != request.RelaySession.Badge.Address {
+			return nil, nil, utils.LavaFormatWarning("did not pass badge signer validation", err, utils.Attribute{Key: "GUID", Value: ctx})
+		}
+		// Consumer address extracted from the badge
 		extractedConsumerAddress, err = sigs.ExtractSignerAddressFromBadge(*request.RelaySession.Badge)
 		if err != nil {
 			return nil, nil, utils.LavaFormatWarning("extract signer address from relay", err, utils.Attribute{Key: "GUID", Value: ctx})
