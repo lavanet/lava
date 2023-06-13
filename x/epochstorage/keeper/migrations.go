37e78263c3646216b8b8fcb7cf6bccd3386095a4
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-06-13 10:09:10 +0000

    remove migration unstaking clients

--- x/epochstorage/keeper/migrations.go
+++ x/epochstorage/keeper/migrations.go
@@ -1,11 +1,7 @@
 package keeper
 
 import (
-	"unicode"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/lavanet/lava/utils"
-	"github.com/lavanet/lava/x/epochstorage/types"
 )
 
 type Migrator struct {
@@ -20,43 +16,14 @@ func NewMigrator(keeper Keeper) Migrator {
 // - refund all clients stake
 // - migrate providers to a new key
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
-	const PairingModuleName = "pairing"
 	const ClientKey = "client"
 	const ProviderKey = "provider"
 
 	storage := m.keeper.GetAllStakeStorage(ctx)
-
 	for _, storage := range storage {
-		m.keeper.RemoveStakeStorage(ctx, storage.Index)
-
 		// handle client keys
 		if storage.Index[:len(ClientKey)] == ClientKey {
-			if len(storage.Index) > len(ClientKey) && !unicode.IsNumber(rune(storage.Index[len(ClientKey)])) {
-				for _, entry := range storage.StakeEntries {
-					moduleBalance := m.keeper.bankKeeper.GetBalance(ctx, m.keeper.accountKeeper.GetModuleAddress(PairingModuleName), types.TokenDenom)
-					if moduleBalance.IsLT(entry.Stake) {
-						utils.LavaFormatError("module pairing does'nt have enough balance", nil,
-							utils.Attribute{Key: "balance", Value: moduleBalance.Amount.Int64()},
-							utils.Attribute{Key: "client", Value: entry.Address},
-							utils.Attribute{Key: "amount", Value: entry.Stake.Amount.Int64()})
-						continue
-					}
-					receiverAddr, err := sdk.AccAddressFromBech32(entry.Address)
-					if err != nil {
-						utils.LavaFormatError("error getting AccAddress", err, utils.Attribute{Key: "client", Value: entry.Address})
-						continue
-					}
-
-					err = m.keeper.bankKeeper.SendCoinsFromModuleToAccount(ctx, PairingModuleName, receiverAddr, []sdk.Coin{entry.Stake})
-					if err != nil {
-						utils.LavaFormatError("invalid transfer coins from module to account", err,
-							utils.Attribute{Key: "balance", Value: moduleBalance.Amount.Int64()},
-							utils.Attribute{Key: "client", Value: entry.Address},
-							utils.Attribute{Key: "amount", Value: entry.Stake.Amount.Int64()})
-						continue
-					}
-				}
-			}
+			m.keeper.RemoveStakeStorage(ctx, storage.Index)
 		} else if storage.Index[:len(ProviderKey)] == ProviderKey { // handle provider keys
 			if len(storage.Index) > len(ProviderKey) {
 				storage.Index = storage.Index[len(ProviderKey):]
