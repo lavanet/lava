db8ea395c360c967f93e627c5c8f9aa91f672cfc
Oren Laadan <orenl@lavanet.xyz>
2023-06-18 19:41:54 +0800

    CNS-434: move TestSubscriptionExpire to subscription_test.go

--- x/subscription/keeper/subscription_test.go
+++ x/subscription/keeper/subscription_test.go
@@ -11,6 +11,7 @@ import (
 	commontypes "github.com/lavanet/lava/common/types"
 	"github.com/lavanet/lava/testutil/common"
 	keepertest "github.com/lavanet/lava/testutil/keeper"
+	"github.com/lavanet/lava/utils/sigs"
 	epochstoragetypes "github.com/lavanet/lava/x/epochstorage/types"
 	planstypes "github.com/lavanet/lava/x/plans/types"
 	projectstypes "github.com/lavanet/lava/x/projects/types"
@@ -532,9 +533,59 @@ func TestExpiryTime(t *testing.T) {
 			// will expire and remove
 			timestamp := ts.ctx.BlockTime()
 			ts.advanceMonths(timestamp, int(tt.months))
-		//	ts.advanteUntilStale()
+			ts.advanceUntilStale()
 		})
 	}
+}
+
+func TestSubscriptionExpire(t *testing.T) {
+	ts := setupTestStruct(t, 1)
+	keeper := ts.keepers.Subscription
+
+	_, account := sigs.GenerateFloatingKey()
+	coins := sdk.NewCoins(sdk.NewCoin(epochstoragetypes.TokenDenom, sdk.NewInt(10000)))
+	ts.keepers.BankKeeper.SetBalance(ts.ctx, account, coins)
+
+	creator := account.String()
+	consumer := account.String()
+
+	blocksToSave, err := ts.keepers.Epochstorage.BlocksToSave(ts.ctx, uint64(ts.ctx.BlockHeight()))
+	require.Nil(t, err)
+
+	err = keeper.CreateSubscription(ts.ctx, creator, consumer, "mockPlan1", 1)
+	require.Nil(t, err)
+
+	block := uint64(ts.ctx.BlockHeight())
+	timestamp := ts.ctx.BlockTime()
+
+	// fill memory
+	for uint64(ts.ctx.BlockHeight()) < blocksToSave {
+		ts.advanceBlock()
+	}
+
+	_, found := keeper.GetSubscription(ts.ctx, account.String())
+	require.True(t, found)
+
+	err = keeper.ChargeComputeUnitsToSubscription(ts.ctx, account.String(), block, 10)
+	require.Nil(t, err)
+
+	// fast-forward one month
+	ts.advanceMonths(timestamp, 1)
+	_, found = keeper.GetSubscription(ts.ctx, creator)
+	require.False(t, found)
+
+	// subscription no longer searchable, but can still charge for previous usage
+	_, found = keeper.GetSubscription(ts.ctx, account.String())
+	require.False(t, found)
+
+	err = keeper.ChargeComputeUnitsToSubscription(ts.ctx, account.String(), block, 10)
+	require.Nil(t, err)
+
+	ts.advanceUntilStale()
+
+	// subscription no longer charge-able for previous usage
+	err = keeper.ChargeComputeUnitsToSubscription(ts.ctx, account.String(), block, 10)
+	require.NotNil(t, err)
 }
 
 func TestPrice(t *testing.T) {
