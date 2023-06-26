b4d93b48dc5628b16566e936b389233e9dd66f5f
Oren Laadan <orenl@lavanet.xyz>
2023-06-26 11:59:50 +0800

    CNS-467: downgrade panic in x/spec/keeper/spec.go

--- x/pairing/types/expected_keepers.go
+++ x/pairing/types/expected_keepers.go
@@ -15,7 +15,7 @@ type SpecKeeper interface {
 	IsSpecFoundAndActive(ctx sdk.Context, chainID string) (foundAndActive bool, found bool)
 	GetSpec(ctx sdk.Context, index string) (val spectypes.Spec, found bool)
 	GeolocationCount(ctx sdk.Context) uint64
-	GetExpectedInterfacesForSpec(ctx sdk.Context, chainID string, mandatory bool) (expectedInterfaces map[epochstoragetypes.EndpointService]struct{})
+	GetExpectedInterfacesForSpec(ctx sdk.Context, chainID string, mandatory bool) (expectedInterfaces map[epochstoragetypes.EndpointService]struct{}, err error)
 	GetAllChainIDs(ctx sdk.Context) (chainIDs []string)
 }
 
