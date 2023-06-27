977d78fb6e68ecd92e94ac6b5192bc2f2f9aabc7
oren-lava <oren@lavanet.xyz>
2023-06-27 14:22:48 +0300

    CNS-477: removed RemoveDuplicatesFromSlice (not needed)

--- common/types/utils.go
+++ common/types/utils.go
@@ -63,15 +63,3 @@ func Intersection[T comparable](arrays ...[]T) []T {
 
 	return intersection
 }
-
-func RemoveDuplicatesFromSlice[T comparable](s []T) []T {
-	allKeys := make(map[T]bool)
-	list := []T{}
-	for _, item := range s {
-		if _, value := allKeys[item]; !value {
-			allKeys[item] = true
-			list = append(list, item)
-		}
-	}
-	return list
-}
