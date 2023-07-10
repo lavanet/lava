d5c4051a5eb7333804ed1273a1b60894f43a2636
Ran Mishael <106548467+ranlavanet@users.noreply.github.com>
2023-07-10 09:01:00 +0200

    PRT-730 node error handling (#596)
    * adding better error handling and fixing a bug in grpc error handling
    
    * fixing lint issue
    
    * fixing unitest error
    
    * changing errorhandler to private
    
    * fixing all changes requested

--- protocol/chainlib/rest.go
+++ protocol/chainlib/rest.go
@@ -353,7 +353,7 @@ func NewRestChainProxy(ctx context.Context, nConns uint, rpcProviderEndpoint *la
 	nodeUrl := rpcProviderEndpoint.NodeUrls[0]
 	nodeUrl.Url = strings.TrimSuffix(rpcProviderEndpoint.NodeUrls[0].Url, "/")
 	rcp := &RestChainProxy{
-		BaseChainProxy: BaseChainProxy{averageBlockTime: averageBlockTime, NodeUrl: rpcProviderEndpoint.NodeUrls[0]},
+		BaseChainProxy: BaseChainProxy{averageBlockTime: averageBlockTime, NodeUrl: rpcProviderEndpoint.NodeUrls[0], ErrorHandler: &RestErrorHandler{}},
 	}
 	return rcp, nil
 }
@@ -416,9 +416,10 @@ func (rcp *RestChainProxy) SendNodeMsg(ctx context.Context, ch chan interface{},
 	}
 	res, err := httpClient.Do(req)
 	if err != nil {
-		if err != nil && connectCtx.Err() == context.DeadlineExceeded {
-			// Not an rpc error, return provider error without disclosing the endpoint address
-			return nil, "", nil, utils.LavaFormatError("Provider Failed Sending Message", context.DeadlineExceeded)
+		// Validate if the error is related to the provider connection to the node or it is a valid error
+		// in case the error is valid (e.g. bad input parameters) the error will return in the form of a valid error reply
+		if parsedError := rcp.HandleNodeError(ctx, err); parsedError != nil {
+			return nil, "", nil, parsedError
 		}
 		return nil, "", nil, err
 	}
--- scripts/start_env_dev.sh
+++ scripts/start_env_dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+echo "dont use this script with vscode debugger"
+killall lava-protocol
 make build-protocol
 rm -rf build
 ignite chain serve -v -r 2>&1 | grep -e lava_ -e ERR_ -e STARPORT] -e !
