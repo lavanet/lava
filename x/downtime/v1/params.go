baaf705641afc8b4fe97b153601ca37000dd2ddf
unknown unknown <unknown@unknown>
2023-07-14 11:31:40 +0200

    add params implementation

---
+++ x/downtime/v1/params.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	"fmt"
+	"time"
+
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+)
+
+var (
+	ParamKeyDowntimeDuration        = []byte("DowntimeDuration")
+	DefaultParamKeyDowntimeDuration = 30 * time.Minute
+)
+
+func DefaultParams() Params {
+	return Params{
+		DowntimeDuration: DefaultParamKeyDowntimeDuration,
+	}
+}
+
+var _ paramtypes.ParamSet = (*Params)(nil)
+
+func (m *Params) ParamSetPairs() paramtypes.ParamSetPairs {
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(ParamKeyDowntimeDuration, &m.DowntimeDuration, validateDowntimeDuration),
+	}
+}
+
+func validateDowntimeDuration(value interface{}) error {
+	concrete, ok := value.(time.Duration)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T, wanted: %T", value, time.Duration(0))
+	}
+
+	if concrete <= 0 {
+		return fmt.Errorf("invalid downtime duration: %s", concrete)
+	}
+	return nil
+}
