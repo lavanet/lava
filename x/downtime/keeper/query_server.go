5fba6f0582afcca5b356016316bdfe9291c554f4
unknown unknown <unknown@unknown>
2023-07-17 16:15:05 +0200

    implement query downtime

--- x/downtime/keeper/query_server.go
+++ x/downtime/keeper/query_server.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"time"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	v1 "github.com/lavanet/lava/x/downtime/v1"
 )
 
@@ -13,8 +15,19 @@ type queryServer struct {
 }
 
 func (q queryServer) QueryDowntime(ctx context.Context, request *v1.QueryDowntimeRequest) (*v1.QueryDowntimeResponse, error) {
-	// TODO implement me
-	panic("implement me")
+	resp := new(v1.QueryDowntimeResponse)
+	resp.CumulativeDowntimeDuration = 0 * time.Second
+
+	q.k.IterateDowntimes(sdk.UnwrapSDKContext(ctx), request.StartBlock, request.EndBlock, func(height uint64, downtime time.Duration) (stop bool) {
+		resp.Downtimes = append(resp.Downtimes, &v1.Downtime{
+			Block:    height,
+			Duration: downtime,
+		})
+		resp.CumulativeDowntimeDuration += downtime
+		return false
+	})
+
+	return resp, nil
 }
 
 func NewQueryServer(k Keeper) v1.QueryServer {
