57091cef519cfd546d9d03b93278611aafcc8444
unknown unknown <unknown@unknown>
2023-07-17 16:23:18 +0200

    add error + query server test

--- x/downtime/keeper/query_server.go
+++ x/downtime/keeper/query_server.go
@@ -6,6 +6,8 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v1 "github.com/lavanet/lava/x/downtime/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var _ v1.QueryServer = queryServer{}
@@ -17,6 +19,10 @@ type queryServer struct {
 func (q queryServer) QueryDowntime(ctx context.Context, request *v1.QueryDowntimeRequest) (*v1.QueryDowntimeResponse, error) {
 	resp := new(v1.QueryDowntimeResponse)
 	resp.CumulativeDowntimeDuration = 0 * time.Second
+
+	if request.StartBlock > request.EndBlock {
+		return nil, status.Error(codes.InvalidArgument, "start block must be less than or equal to end block")
+	}
 
 	q.k.IterateDowntimes(sdk.UnwrapSDKContext(ctx), request.StartBlock, request.EndBlock, func(height uint64, downtime time.Duration) (stop bool) {
 		resp.Downtimes = append(resp.Downtimes, &v1.Downtime{
