f0384eca3d5fc74b69c69b1fddaa095620b88027
unknown unknown <unknown@unknown>
2023-07-18 12:08:45 +0200

    add genesis defaults

--- x/downtime/v1/genesis.go
+++ x/downtime/v1/genesis.go
@@ -2,7 +2,11 @@ package v1
 
 // DefaultGenesisState returns a default genesis state.
 func DefaultGenesisState() *GenesisState {
-	return &GenesisState{}
+	return &GenesisState{
+		Params:                     DefaultParams(),
+		Downtimes:                  nil,
+		DowntimesGarbageCollection: nil,
+	}
 }
 
 func (m *GenesisState) Validate() error { return nil }
