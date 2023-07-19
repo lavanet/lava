e27bbeed5e0850e83f48188c590802cab1ff7199
Aleksao998 <aleksaopacic988@gmail.com>
2023-07-19 14:17:59 +0200

    Fix test

--- protocol/common/errors.go
+++ protocol/common/errors.go
@@ -2,4 +2,4 @@ package common
 
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-var ContextDeadlineExceededError = sdkerrors.New("ContextDeadlineExceeded Error", 300, "Context deadline exceeded")
+var ContextDeadlineExceededError = sdkerrors.New("ContextDeadlineExceeded Error", 300, "context deadline exceeded")
