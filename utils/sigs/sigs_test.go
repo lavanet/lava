169c999ee6a426f84babd00533c2130119f9580c
oren-lava <oren@lavanet.xyz>
2023-07-25 18:38:18 +0300

    CNS-431: added hash round unit test

--- utils/sigs/sigs_test.go
+++ utils/sigs/sigs_test.go
@@ -7,8 +7,9 @@ import (
 )
 
 type MockSignable struct {
-	data string
-	sig  []byte
+	data       string
+	sig        []byte
+	hashRounds int
 }
 
 func (ms MockSignable) GetSignature() []byte {
@@ -20,16 +21,44 @@ func (ms MockSignable) DataToSign() []byte {
 }
 
 func (ms MockSignable) HashRounds() int {
-	return 1
+	return ms.hashRounds
+}
+
+func NewMockSignable(data string, hashRounds int) MockSignable {
+	return MockSignable{data: data, hashRounds: hashRounds}
 }
 
 func TestSignAndExtract(t *testing.T) {
 	sk, addr := GenerateFloatingKey()
-	mock := MockSignable{data: "hello"}
+	mock := NewMockSignable("hello", 1)
 	sig, err := Sign(sk, mock)
 	require.Nil(t, err)
 	mock.sig = sig
 	extractedAddr, err := ExtractSignerAddress(mock)
 	require.Nil(t, err)
 	require.Equal(t, addr, extractedAddr)
+}
+
+func TestHashRounds(t *testing.T) {
+	sk, _ := GenerateFloatingKey()
+	data := "hello"
+
+	mock0 := NewMockSignable(data, 0)
+	mock1 := NewMockSignable(data, 1)
+	mock2 := NewMockSignable(data, 2)
+
+	// change mock0 and mock1 data (with hashes) so it'll match mock2 data
+	mock0.data = string(HashMsg(HashMsg(mock0.DataToSign())))
+	mock1.data = string(HashMsg(mock1.DataToSign()))
+
+	sig0, err := Sign(sk, mock0)
+	require.Nil(t, err)
+	sig1, err := Sign(sk, mock1)
+	require.Nil(t, err)
+	sig2, err := Sign(sk, mock2)
+	require.Nil(t, err)
+
+	// check all sigs are the same
+	require.Equal(t, sig0, sig1)
+	require.Equal(t, sig0, sig2)
 }
