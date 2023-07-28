2801b8dc7ffc59f18ecc75cc2aecaea293b63eb6
Rex Milce <rexmilce@proton.me>
2023-07-28 11:42:17 +0200

    remove rest proposals

--- x/spec/client/proposal_handler.go
+++ x/spec/client/proposal_handler.go
@@ -3,8 +3,7 @@ package client
 import (
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 	"github.com/lavanet/lava/x/spec/client/cli"
-	"github.com/lavanet/lava/x/spec/client/rest"
 )
 
 // SpecAddProposalHandler is the param change proposal handler.
-var SpecAddProposalHandler = govclient.NewProposalHandler(cli.NewSubmitSpecAddProposalTxCmd, rest.ProposalRESTHandler)
+var SpecAddProposalHandler = govclient.NewProposalHandler(cli.NewSubmitSpecAddProposalTxCmd)
