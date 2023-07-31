311b9b31c816dab697d2746145740141d70e02f8
oren-lava <oren@lavanet.xyz>
2023-07-31 11:24:08 +0300

    CNS-565: implemented geolocations query

--- x/plans/keeper/grpc_query_geolocations.go
+++ x/plans/keeper/grpc_query_geolocations.go
@@ -2,22 +2,41 @@ package keeper
 
 import (
 	"context"
+	"sort"
+	"strconv"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lavanet/lava/x/plans/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+type geoInfo struct {
+	name string
+	val  int32
+}
 
 func (k Keeper) Geolocations(goCtx context.Context, req *types.QueryGeolocationsRequest) (*types.QueryGeolocationsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	var geos []geoInfo
+	for geo, geoInt := range types.Geolocation_value {
+		geos = append(geos, geoInfo{name: geo, val: geoInt})
+	}
 
-	// TODO: Process the query
-	_ = ctx
+	sort.Slice(geos, func(i, j int) bool {
+		return geos[i].val < geos[j].val
+	})
 
-	return &types.QueryGeolocationsResponse{}, nil
+	var geosStr []string
+	for _, info := range geos {
+		geosStr = append(geosStr, info.String())
+	}
+
+	return &types.QueryGeolocationsResponse{Geos: geosStr}, nil
+}
+
+func (gi geoInfo) String() string {
+	return gi.name + ": 0x" + strconv.FormatInt(int64(gi.val), 16)
 }
