d3dd8b0e484199531a7eeefa1dda1bb98c79ee2e
oren-lava <oren@lavanet.xyz>
2023-07-31 11:29:14 +0300

    CNS-565: fix lint issue

--- x/plans/client/cli/query_geolocations.go
+++ x/plans/client/cli/query_geolocations.go
@@ -17,7 +17,6 @@ func CmdGeolocations() *cobra.Command {
 		Short: "Query to show all the available geolocations",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
