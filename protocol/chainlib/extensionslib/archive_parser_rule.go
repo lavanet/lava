62d9a42e1ff561b878f355fbfdcab556d3f9007d
omer mishael <omer@lavanet.xyz>
2023-08-03 18:57:13 +0300

    archive parser rule doesn;t set archive to latest requests

--- protocol/chainlib/extensionslib/archive_parser_rule.go
+++ protocol/chainlib/extensionslib/archive_parser_rule.go
@@ -8,10 +8,14 @@ type ArchiveParserRule struct {
 
 func (arp ArchiveParserRule) isPassingRule(extensionChainMessage ExtensionsChainMessage, latestBlock uint64) bool {
 	requestedBlock := extensionChainMessage.RequestedBlock()
+	if requestedBlock < 0 {
+		// if asking for the latest block, or an api that doesn't have a specific block requested then it's not archive
+		return false
+	}
 	if latestBlock == 0 {
 		return true
 	}
-	if requestedBlock < 0 || uint64(requestedBlock) >= latestBlock {
+	if uint64(requestedBlock) >= latestBlock {
 		return false
 	}
 	if arp.extension.Rule != nil && arp.extension.Rule.Block != 0 {
