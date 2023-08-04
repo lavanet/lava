2572c2c4b1f5c2a97c4a02276de33c2a99354eac
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-08-04 19:49:44 +0300

    lv version monitor implemented

---
+++ ecosystem/lavavisor/pkg/monitor/version_monitor.go
@@ -0,0 +1,51 @@
+package version_montior
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/lavanet/lava/utils"
+	protocoltypes "github.com/lavanet/lava/x/protocol/types"
+)
+
+func getBinaryVersion(binaryPath string) (string, error) {
+	cmd := exec.Command(binaryPath, "-v")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", utils.LavaFormatError("failed to execute command", err)
+	}
+
+	// output format is "lava-protocol version x.x.x"
+	version := strings.Split(string(output), " ")[2]
+	return version, nil
+}
+
+func ValidateProtocolBinaryVersion(incoming *protocoltypes.Version, binaryPath string) error {
+	binaryVersion, err := getBinaryVersion(binaryPath)
+	if err != nil {
+		return utils.LavaFormatError("failed to get binary version", err)
+	}
+
+	// check min version
+	if incoming.ConsumerMin != binaryVersion || incoming.ProviderMin != binaryVersion {
+		utils.LavaFormatPanic("minimum protocol version mismatch!, you must update your protocol version to at least the minimum required protocol version",
+			nil,
+			utils.Attribute{Key: "required (on-chain) consumer minimum version:", Value: incoming.ConsumerMin},
+			utils.Attribute{Key: "required (on-chain) provider minimum version", Value: incoming.ProviderMin},
+			utils.Attribute{Key: "binary consumer version: ", Value: binaryVersion},
+			utils.Attribute{Key: "binary provider version: ", Value: binaryVersion},
+		)
+	}
+	// check target version
+	if incoming.ConsumerTarget != binaryVersion || incoming.ProviderTarget != binaryVersion {
+		return utils.LavaFormatError("target protocol version mismatch, there is a newer version available. We highly recommend to upgrade.",
+			nil,
+			utils.Attribute{Key: "required (on-chain) consumer target version:", Value: incoming.ConsumerTarget},
+			utils.Attribute{Key: "required (on-chain) provider target version", Value: incoming.ProviderTarget},
+			utils.Attribute{Key: "binary consumer version: ", Value: binaryVersion},
+			utils.Attribute{Key: "binary provider version: ", Value: binaryVersion},
+		)
+	}
+	// version is ok.
+	return nil
+}
