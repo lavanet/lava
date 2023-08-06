050287a5a43e540851ed8b2410cbf6d29140a952
juliant <julian.tabaku@outlook.com>
2023-08-06 17:25:18 +0200

    remove the csv and tsv files of the badge. merge with main

--- protocol/badgegenerator/constants.go
+++ protocol/badgegenerator/constants.go
@@ -2,7 +2,7 @@ package badgegenerator
 
 const (
 	PortEnvironmentVariable               = "PORT"
-	MetricsPortEnvironmentVariable = "METRICS_PORT"
+	MetricsPortEnvironmentVariable        = "METRICS_PORT"
 	UserDataEnvironmentVariable           = "USER_DATA"
 	GrpcUrlEnvironmentVariable            = "GRPC_URL"
 	LavaChainIDEnvironmentVariable        = "CHAIN_ID"
