d029565388bded769b125d963bb3f1626b7b08fd
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-08-07 18:09:08 +0000

    pr

--- scripts/init_chain.sh
+++ scripts/init_chain.sh
@@ -14,37 +14,32 @@ config='config.toml'
 app='app.toml'
 
 # Edit genesis file
-data=$(cat "$path$genesis")
+data=$(cat "$path$genesis" \
+    | jq '.app_state.gov.deposit_params.min_deposit[0].denom = "ulava"' \
+    | jq '.app_state.gov.deposit_params.min_deposit[0].amount = "10000000"' \
+    | jq '.app_state.gov.voting_params.voting_period = "3s"' \
+    | jq '.app_state.mint.params.mint_denom = "ulava"' \
+    | jq '.app_state.staking.params.bond_denom = "ulava"' \
+    | jq '.app_state.crisis.constant_fee.denom = "ulava"' \
+    )
 
-data=$(echo "$data" | jq '.app_state.gov.deposit_params.min_deposit[0].denom = "ulava"')
-data=$(echo "$data" | jq '.app_state.gov.deposit_params.min_deposit[0].amount = "10000000"')
-data=$(echo "$data" | jq '.app_state.gov.voting_params.voting_period = "3s"')
-data=$(echo "$data" | jq '.app_state.mint.params.mint_denom = "ulava"')
-data=$(echo "$data" | jq '.app_state.staking.params.bond_denom = "ulava"')
-data=$(echo "$data" | jq '.app_state.crisis.constant_fee.denom = "ulava"')
-
-echo "$data" > "$path$genesis"
+echo -n "$data" > "$path$genesis"
 
 # Edit conflict.toml file
-data=$(cat "$path$config")
-
-data=$(echo "$data" | sed 's/timeout_propose = .*/timeout_propose = "1s"/')
-data=$(echo "$data" | sed 's/timeout_propose_delta = .*/timeout_propose_delta = "500ms"/')
-data=$(echo "$data" | sed 's/timeout_prevote = .*/timeout_prevote = "1s"/')
-data=$(echo "$data" | sed 's/timeout_prevote_delta = .*/timeout_prevote_delta = "500ms"/')
-data=$(echo "$data" | sed 's/timeout_precommit = .*/timeout_precommit = "500ms"/')
-data=$(echo "$data" | sed 's/timeout_precommit_delta = .*/timeout_precommit_delta = "1s"/')
-data=$(echo "$data" | sed 's/timeout_commit = .*/timeout_commit = "1s"/')
-data=$(echo "$data" | sed 's/skip_timeout_commit = .*/skip_timeout_commit = false/')
-
-echo "$data" > "$path$config"
+sed -i "$path$config" \
+    -e 's/timeout_propose = .*/timeout_propose = "1s"/' \
+    -e 's/timeout_propose_delta = .*/timeout_propose_delta = "500ms"/' \
+    -e 's/timeout_prevote = .*/timeout_prevote = "1s"/' \
+    -e 's/timeout_prevote_delta = .*/timeout_prevote_delta = "500ms"/' \
+    -e 's/timeout_precommit = .*/timeout_precommit = "500ms"/' \
+    -e 's/timeout_precommit_delta = .*/timeout_precommit_delta = "1s"/' \
+    -e 's/timeout_commit = .*/timeout_commit = "1s"/' \
+    -e 's/skip_timeout_commit = .*/skip_timeout_commit = false/' 
 
 # Edit app.toml file
-data=$(cat "$path$app")
+sed -i "$path$app" \
+    -e 's/enable = .*/enable = true/' 
 
-data=$(echo "$data" | sed 's/enable = .*/enable = true/')
-
-echo "$data" > "$path$app"
 
 # Add users
 users=("alice" "bob" "user1" "user2" "user3" "user4" "servicer1" "servicer2" "servicer3" "servicer4" "servicer5" "servicer6" "servicer7" "servicer8" "servicer9" "servicer10")
