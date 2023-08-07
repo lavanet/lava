0b1e4bda95881112197d11390da11828079b3079
Aleksao998 <aleksaopacic988@gmail.com>
2023-08-07 19:13:10 +0200

    Fix sdk e2e

--- testutil/e2e/sdkE2E.go
+++ testutil/e2e/sdkE2E.go
@@ -28,12 +28,12 @@ func (lt *lavaTest) startBadgeServer(ctx context.Context, privateKey string, pub
 		panic(err)
 	}
 
-	command := fmt.Sprintf("%s badgegenerator --grpc-url=127.0.0.1:9090 --log_level=debug --chain-id lava", lt.protocolPath)
+	command := fmt.Sprintf("%s badgegenerator --port=7070 --grpc-url=127.0.0.1:9090 --log_level=debug --chain-id lava", lt.protocolPath)
 	logName := "01_BadgeServer"
 	funcName := "startBadgeServer"
 	lt.execCommandWithRetry(ctx, funcName, logName, command)
 
-	lt.checkBadgeServerResponsive(ctx, "127.0.0.1:8080", time.Minute)
+	lt.checkBadgeServerResponsive(ctx, "127.0.0.1:7070", time.Minute)
 }
 
 // exportUserPublicKey exports public key from specific user
