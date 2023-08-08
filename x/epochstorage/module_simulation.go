c7c775fca062de1d449baec2c2f19e63a232cfbe
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-08-08 08:16:43 +0000

    some more lint

--- x/epochstorage/module_simulation.go
+++ x/epochstorage/module_simulation.go
@@ -42,7 +42,7 @@ func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 }
 
 // ProposalContents doesn't return any content functions for governance proposals
-func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
+func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalMsg {
 	return nil
 }
 
