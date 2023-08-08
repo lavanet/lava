4fc4dcd5e9e24692d624744d0f3b851adc20b029
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-08-08 12:42:50 +0300

    FetchAndBuildFromGithub added to version monitor

--- ecosystem/lavavisor/cmd/init.go
+++ ecosystem/lavavisor/cmd/init.go
@@ -1,11 +1,7 @@
 package lavavisor
 
 import (
-	"archive/zip"
 	"context"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,7 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	version_montior "github.com/lavanet/lava/ecosystem/lavavisor/pkg/monitor"
+	versionmontior "github.com/lavanet/lava/ecosystem/lavavisor/pkg/monitor"
 	lvstatetracker "github.com/lavanet/lava/ecosystem/lavavisor/pkg/state"
 	lvutil "github.com/lavanet/lava/ecosystem/lavavisor/pkg/util"
 	"github.com/lavanet/lava/protocol/chainlib"
@@ -62,45 +58,41 @@ var cmdLavavisorInit = &cobra.Command{
 			return utils.LavaFormatError("protcol version cannot be fetched from consensus", err)
 		}
 
-		utils.LavaFormatInfo("Initializing the environment", utils.Attribute{Key: "Version", Value: protocolConsensusVersion.ConsumerMin})
+		utils.LavaFormatInfo("Initializing the environment", utils.Attribute{Key: "Version", Value: protocolConsensusVersion.ProviderMin})
 
 		// 2- search extracted directory inside ./lavad/upgrades/<fetched_version>
 		// first check target version, then check min version
-		versionDir := filepath.Join(lavavisorPath, "upgrades", "v"+protocolConsensusVersion.ConsumerMin)
+		versionDir := filepath.Join(lavavisorPath, "upgrades", "v"+protocolConsensusVersion.ProviderMin)
 		binaryPath := filepath.Join(versionDir, "lava-protocol")
+
+		// check auto-download flag
+		autoDownload, err := cmd.Flags().GetBool("auto-download")
+		if err != nil {
+			return err
+		}
 
 		// check if version directory exists
 		if _, err := os.Stat(versionDir); os.IsNotExist(err) {
-			// directory doesn't exist, check auto-download flag
-			autoDownload, err := cmd.Flags().GetBool("auto-download")
-			if err != nil {
-				return err
-			}
 			if autoDownload {
 				utils.LavaFormatInfo("Version directory does not exist, but auto-download is enabled. Attempting to download binary from GitHub...")
 				os.MkdirAll(versionDir, os.ModePerm) // before downloading, ensure version directory exists
-				err = fetchAndBuildFromGithub(protocolConsensusVersion.ConsumerMin, versionDir)
+				err = versionmontior.FetchAndBuildFromGithub(protocolConsensusVersion.ProviderMin, versionDir)
 				if err != nil {
 					utils.LavaFormatError("Failed to auto-download binary from GitHub\n ", err)
 					os.Exit(1)
 				}
 			} else {
-				utils.LavaFormatError("Sub-directory for version not found in lavavisor", nil, utils.Attribute{Key: "version", Value: protocolConsensusVersion.ConsumerMin})
+				utils.LavaFormatError("Sub-directory for version not found in lavavisor", nil, utils.Attribute{Key: "version", Value: protocolConsensusVersion.ProviderMin})
 				os.Exit(1)
 			}
 			// ToDo: add checkLavaProtocolVersion after version flag is added to release
 			//
 		} else {
-			err = version_montior.ValidateProtocolBinaryVersion(protocolConsensusVersion, binaryPath)
+			err = versionmontior.ValidateProtocolBinaryVersion(protocolConsensusVersion, binaryPath)
 			if err != nil {
-				// binary not found or version mismatch, check auto-download flag
-				autoDownload, err := cmd.Flags().GetBool("auto-download")
-				if err != nil {
-					return err
-				}
 				if autoDownload {
 					utils.LavaFormatInfo("Version mismatch or binary not found, but auto-download is enabled. Attempting to download binary from GitHub...")
-					err = fetchAndBuildFromGithub(protocolConsensusVersion.ConsumerMin, versionDir)
+					err = versionmontior.FetchAndBuildFromGithub(protocolConsensusVersion.ProviderMin, versionDir)
 					if err != nil {
 						utils.LavaFormatError("Failed to auto-download binary from GitHub\n ", err)
 						os.Exit(1)
@@ -181,7 +173,7 @@ var cmdLavavisorInit = &cobra.Command{
 
 		utils.LavaFormatInfo("Symbolic link created successfully")
 
-		// if autodownload false: alert user that binary is not exist, monitor directory constantly!,
+		// ToDo: if autodownload false: alert user that binary is not exist, monitor directory constantly!,
 
 		return nil
 	},
@@ -206,155 +198,4 @@ func Copy(src, dest string) error {
 		return utils.LavaFormatError("couldn't write destination file", err)
 	}
 	return nil
-}
-
-func fetchAndBuildFromGithub(version, versionDir string) error {
-	// Clean up the binary directory if it exists
-	err := os.RemoveAll(versionDir)
-	if err != nil {
-		return utils.LavaFormatError("failed to clean up binary directory", err)
-	}
-	// URL might need to be updated based on the actual GitHub repository
-	url := fmt.Sprintf("https://github.com/lavanet/lava/archive/refs/tags/v%s.zip", version)
-	utils.LavaFormatInfo("Fetching the source from: ", utils.Attribute{Key: "URL", Value: url})
-
-	// Send the request
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Check server response
-	if resp.StatusCode != http.StatusOK {
-		return utils.LavaFormatError("bad HTTP status", nil, utils.Attribute{Key: "status", Value: resp.Status})
-	}
-
-	// Prepare the path for downloaded zip
-	zipPath := filepath.Join(versionDir, version+".zip")
-
-	// Make sure the directory exists
-	dir := filepath.Dir(zipPath)
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err = os.MkdirAll(dir, 0o755)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Write the body to file
-	out, err := os.Create(zipPath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-	// Unzip the source
-	_, err = unzip(zipPath, versionDir)
-	if err != nil {
-		return err
-	}
-	utils.LavaFormatInfo("Unzipping...")
-
-	// Build the binary
-	srcPath := versionDir + "/lava-" + version
-	protocolPath := srcPath + "/protocol"
-	utils.LavaFormatInfo("building protocol", utils.Attribute{Key: "protocol-path", Value: protocolPath})
-
-	cmd := exec.Command("go", "build", "-o", "lava-protocol")
-	cmd.Dir = protocolPath
-	err = cmd.Run()
-	if err != nil {
-		return err
-	}
-
-	// Move the binary to binaryPath
-	err = os.Rename(filepath.Join(protocolPath, "lava-protocol"), filepath.Join(versionDir, "lava-protocol"))
-	if err != nil {
-		return utils.LavaFormatError("failed to move compiled binary", err)
-	}
-
-	// Verify the compiled binary
-	versionDir += "/lava-protocol"
-	binaryInfo, err := os.Stat(versionDir)
-	if err != nil {
-		return utils.LavaFormatError("failed to verify compiled binary", err)
-	}
-	binaryMode := binaryInfo.Mode()
-	if binaryMode.Perm()&0o111 == 0 {
-		return utils.LavaFormatError("compiled binary is not executable", nil)
-	}
-	utils.LavaFormatInfo("lava-protocol binary is successfully verified!")
-
-	// Remove the source files and zip file
-	err = os.RemoveAll(srcPath)
-	if err != nil {
-		return utils.LavaFormatError("failed to remove source files", err)
-	}
-
-	err = os.Remove(zipPath)
-	if err != nil {
-		return utils.LavaFormatError("failed to remove zip file", err)
-	}
-	utils.LavaFormatInfo("Source and zip files removed from directory.")
-	utils.LavaFormatInfo("Auto-download successful.")
-
-	return nil
-}
-
-func unzip(src string, dest string) ([]string, error) {
-	var filenames []string
-
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return filenames, err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		// Store filename/path for returning and using later on
-		fpath := filepath.Join(dest, f.Name)
-
-		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
-		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
-			return filenames, utils.LavaFormatError("illegal file path", nil)
-		}
-
-		filenames = append(filenames, fpath)
-
-		if f.FileInfo().IsDir() {
-			// Make Folder
-			os.MkdirAll(fpath, os.ModePerm)
-			continue
-		}
-
-		// Make File
-		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-			return filenames, err
-		}
-
-		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-		if err != nil {
-			return filenames, err
-		}
-
-		rc, err := f.Open()
-		if err != nil {
-			return filenames, err
-		}
-
-		_, err = io.Copy(outFile, rc)
-
-		outFile.Close()
-		rc.Close()
-
-		if err != nil {
-			return filenames, err
-		}
-	}
-	return filenames, nil
 }
