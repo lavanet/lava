05dc14b869eadea70564fd224d959df4dc76e098
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-08-09 23:43:48 +0300

    processmanager pkg added

---
+++ ecosystem/lavavisor/pkg/monitor/process_manager.go
@@ -0,0 +1,47 @@
+package processmanager
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type ProviderProcess struct {
+	Name      string
+	ChainID   string
+	IsRunning bool
+}
+
+func StartProvider(providers *[]*ProviderProcess, provider string) {
+	// Extract the chain id from the provider string
+	chainID := strings.Split(provider, "-")[1]
+
+	// Create command list
+	cmds := []*exec.Cmd{
+		exec.Command("sudo", "systemctl", "daemon-reload"),
+		exec.Command("sudo", "systemctl", "enable", provider+".service"),
+		exec.Command("sudo", "systemctl", "restart", provider+".service"),
+		exec.Command("sudo", "systemctl", "status", provider+".service"),
+	}
+
+	// Run the commands and capture their output
+	for _, cmd := range cmds {
+		fmt.Printf("Running command: %s\n", strings.Join(cmd.Args, " "))
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Failed to run command: %s, Error: %s\n", cmd, err)
+			fmt.Printf("Command Output: \n%s\n", output)
+			return
+		} else {
+			fmt.Printf("Successfully run command: %s\n", cmd)
+			fmt.Printf("Command Output: \n%s\n", output)
+		}
+	}
+
+	// Add to the list of providers
+	*providers = append(*providers, &ProviderProcess{
+		Name:      provider,
+		ChainID:   chainID,
+		IsRunning: true,
+	})
+}
--- ecosystem/lavavisor/pkg/util/common.go
+++ ecosystem/lavavisor/pkg/util/common.go
@@ -1,20 +1,12 @@
 package lvutil
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/lavanet/lava/utils"
 )
-
-type ProviderProcess struct {
-	Name      string
-	ChainID   string
-	IsRunning bool
-}
 
 func ExpandTilde(path string) (string, error) {
 	if !strings.HasPrefix(path, "~") {
@@ -55,38 +47,4 @@ func Copy(src, dest string) error {
 		return utils.LavaFormatError("couldn't write destination file", err)
 	}
 	return nil
-}
-
-func StartProvider(providers *[]*ProviderProcess, provider string) {
-	// Extract the chain id from the provider string
-	chainID := strings.Split(provider, "-")[1]
-
-	// Create command list
-	cmds := []*exec.Cmd{
-		exec.Command("sudo", "systemctl", "daemon-reload"),
-		exec.Command("sudo", "systemctl", "enable", provider+".service"),
-		exec.Command("sudo", "systemctl", "restart", provider+".service"),
-		exec.Command("sudo", "systemctl", "status", provider+".service"),
-	}
-
-	// Run the commands and capture their output
-	for _, cmd := range cmds {
-		fmt.Printf("Running command: %s\n", strings.Join(cmd.Args, " "))
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Printf("Failed to run command: %s, Error: %s\n", cmd, err)
-			fmt.Printf("Command Output: \n%s\n", output)
-			return
-		} else {
-			fmt.Printf("Successfully run command: %s\n", cmd)
-			fmt.Printf("Command Output: \n%s\n", output)
-		}
-	}
-
-	// Add to the list of providers
-	*providers = append(*providers, &ProviderProcess{
-		Name:      provider,
-		ChainID:   chainID,
-		IsRunning: true,
-	})
 }
