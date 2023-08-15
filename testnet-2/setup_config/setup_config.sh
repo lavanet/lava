70f417290fbc3be45c214b28c2e9683b1ac7e749
David-Melamed <davidmelamed269@gmail.com>
2023-08-15 14:21:13 +0300

    update testnet2 seed URLs

--- testnet-2/setup_config/setup_config.sh
+++ testnet-2/setup_config/setup_config.sh
@@ -4,7 +4,7 @@
 deploy_env="testnet-2"
 
 # Blockchain
-seed_node="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-testnet2-seed-node2.lavanet.xyz:26656"
+seed_node="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"
 
 # Lavad service
 keyring_backend="test"
