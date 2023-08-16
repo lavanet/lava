af95ad3c177e7a80b60eab72c1ec4d09e11ee2db
oren-lava <oren@lavanet.xyz>
2023-08-16 15:57:10 +0300

    CNS-577: allow proposal to get multiple providers + chain IDs for each

--- x/pairing/types/unstake_proposal.pb.go
+++ x/pairing/types/unstake_proposal.pb.go
@@ -25,9 +25,9 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type UnstakeProposal struct {
-	Title       string `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
-	Description string `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
-	Provider    string `protobuf:"bytes,3,opt,name=provider,proto3" json:"provider,omitempty"`
+	Title         string                 `protobuf:"bytes,1,opt,name=title,proto3" json:"title,omitempty"`
+	Description   string                 `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
+	ProvidersInfo []*ProviderUnstakeInfo `protobuf:"bytes,3,rep,name=providers_info,json=providersInfo,proto3" json:"providers_info,omitempty"`
 }
 
 func (m *UnstakeProposal) Reset()         { *m = UnstakeProposal{} }
@@ -63,8 +63,61 @@ func (m *UnstakeProposal) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_UnstakeProposal proto.InternalMessageInfo
 
+type ProviderUnstakeInfo struct {
+	Provider string `protobuf:"bytes,1,opt,name=provider,proto3" json:"provider,omitempty"`
+	ChainId  string `protobuf:"bytes,2,opt,name=chain_id,json=chainId,proto3" json:"chain_id,omitempty"`
+}
+
+func (m *ProviderUnstakeInfo) Reset()         { *m = ProviderUnstakeInfo{} }
+func (m *ProviderUnstakeInfo) String() string { return proto.CompactTextString(m) }
+func (*ProviderUnstakeInfo) ProtoMessage()    {}
+func (*ProviderUnstakeInfo) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d35cf592c1bb0d91, []int{1}
+}
+func (m *ProviderUnstakeInfo) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ProviderUnstakeInfo) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ProviderUnstakeInfo.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ProviderUnstakeInfo) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ProviderUnstakeInfo.Merge(m, src)
+}
+func (m *ProviderUnstakeInfo) XXX_Size() int {
+	return m.Size()
+}
+func (m *ProviderUnstakeInfo) XXX_DiscardUnknown() {
+	xxx_messageInfo_ProviderUnstakeInfo.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ProviderUnstakeInfo proto.InternalMessageInfo
+
+func (m *ProviderUnstakeInfo) GetProvider() string {
+	if m != nil {
+		return m.Provider
+	}
+	return ""
+}
+
+func (m *ProviderUnstakeInfo) GetChainId() string {
+	if m != nil {
+		return m.ChainId
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*UnstakeProposal)(nil), "lavanet.lava.pairing.UnstakeProposal")
+	proto.RegisterType((*ProviderUnstakeInfo)(nil), "lavanet.lava.pairing.ProviderUnstakeInfo")
 }
 
 func init() {
@@ -72,23 +125,27 @@ func init() {
 }
 
 var fileDescriptor_d35cf592c1bb0d91 = []byte{
-	// 243 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0xce, 0x49, 0x2c, 0x4b,
-	0xcc, 0x4b, 0x2d, 0xd1, 0x07, 0xd1, 0xfa, 0x05, 0x89, 0x99, 0x45, 0x99, 0x79, 0xe9, 0xfa, 0xa5,
-	0x79, 0xc5, 0x25, 0x89, 0xd9, 0xa9, 0xf1, 0x05, 0x45, 0xf9, 0x05, 0xf9, 0xc5, 0x89, 0x39, 0x7a,
-	0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0x22, 0x50, 0xc5, 0x7a, 0x20, 0x5a, 0x0f, 0xaa, 0x58, 0x4a,
-	0x24, 0x3d, 0x3f, 0x3d, 0x1f, 0xac, 0x40, 0x1f, 0xc4, 0x82, 0xa8, 0x95, 0x92, 0x4b, 0xcf, 0xcf,
-	0x4f, 0xcf, 0x49, 0xd5, 0x07, 0xf3, 0x92, 0x4a, 0xd3, 0xf4, 0xcb, 0x8b, 0x12, 0x0b, 0x0a, 0x52,
-	0x8b, 0x8a, 0x21, 0xf2, 0x4a, 0xd9, 0x5c, 0xfc, 0xa1, 0x10, 0x5b, 0x02, 0xa0, 0x96, 0x08, 0x89,
-	0x70, 0xb1, 0x96, 0x64, 0x96, 0xe4, 0xa4, 0x4a, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x41, 0x38,
-	0x42, 0x0a, 0x5c, 0xdc, 0x29, 0xa9, 0xc5, 0xc9, 0x45, 0x99, 0x05, 0x25, 0x99, 0xf9, 0x79, 0x12,
-	0x4c, 0x60, 0x39, 0x64, 0x21, 0x21, 0x29, 0x2e, 0x8e, 0x82, 0xa2, 0xfc, 0xb2, 0xcc, 0x94, 0xd4,
-	0x22, 0x09, 0x66, 0xb0, 0x34, 0x9c, 0x6f, 0xc5, 0xd2, 0xb1, 0x40, 0x9e, 0xc1, 0xc9, 0xf1, 0xc4,
-	0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1,
-	0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0xd4, 0xd3, 0x33, 0x4b, 0x32, 0x4a, 0x93,
-	0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x51, 0x82, 0xa2, 0x02, 0x1e, 0x18, 0x25, 0x95, 0x05, 0xa9, 0xc5,
-	0x49, 0x6c, 0x60, 0x67, 0x1b, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xf6, 0x00, 0x9b, 0xaf, 0x31,
-	0x01, 0x00, 0x00,
+	// 306 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x91, 0xb1, 0x4a, 0x73, 0x31,
+	0x14, 0xc7, 0x6f, 0xbe, 0x7e, 0x6a, 0x4d, 0x51, 0x21, 0x76, 0xa8, 0x1d, 0x62, 0xe9, 0x62, 0x45,
+	0x48, 0x40, 0x37, 0x37, 0xdd, 0x0a, 0x0e, 0xa5, 0xe0, 0xe2, 0x52, 0xd2, 0xde, 0x34, 0x0d, 0x5e,
+	0x73, 0x42, 0x92, 0x56, 0x7d, 0x03, 0x47, 0x1f, 0x41, 0xdf, 0xc6, 0xb1, 0xa3, 0xa3, 0xb4, 0x2f,
+	0x22, 0xf7, 0xde, 0x78, 0x51, 0xe8, 0x74, 0xf2, 0xcb, 0xf9, 0x91, 0xf3, 0x27, 0x07, 0x9f, 0x65,
+	0x62, 0x21, 0x8c, 0x0c, 0x3c, 0xaf, 0xdc, 0x0a, 0xed, 0xb4, 0x51, 0x7c, 0x6e, 0x7c, 0x10, 0xf7,
+	0x72, 0x64, 0x1d, 0x58, 0xf0, 0x22, 0x63, 0xd6, 0x41, 0x00, 0xd2, 0x8c, 0x32, 0xcb, 0x2b, 0x8b,
+	0x72, 0xbb, 0xa9, 0x40, 0x41, 0x21, 0xf0, 0xfc, 0x54, 0xba, 0x6d, 0xaa, 0x00, 0x54, 0x26, 0x79,
+	0x41, 0xe3, 0xf9, 0x94, 0x3f, 0x3a, 0x61, 0xad, 0x74, 0xbe, 0xec, 0x77, 0xdf, 0x11, 0x3e, 0xb8,
+	0x2d, 0xc7, 0x0c, 0xe2, 0x14, 0xd2, 0xc4, 0x5b, 0x41, 0x87, 0x4c, 0xb6, 0x50, 0x07, 0xf5, 0x76,
+	0x87, 0x25, 0x90, 0x0e, 0x6e, 0xa4, 0xd2, 0x4f, 0x9c, 0xb6, 0x41, 0x83, 0x69, 0xfd, 0x2b, 0x7a,
+	0xbf, 0xaf, 0xc8, 0x00, 0xef, 0x5b, 0x07, 0x0b, 0x9d, 0x4a, 0xe7, 0x47, 0xda, 0x4c, 0xa1, 0x55,
+	0xeb, 0xd4, 0x7a, 0x8d, 0xf3, 0x53, 0xb6, 0x29, 0x30, 0x1b, 0x44, 0x37, 0x8e, 0xef, 0x9b, 0x29,
+	0x0c, 0xf7, 0xaa, 0x07, 0x72, 0xbc, 0xfc, 0xff, 0xf2, 0x76, 0x9c, 0x74, 0x6f, 0xf0, 0xe1, 0x06,
+	0x97, 0xb4, 0x71, 0xfd, 0xc7, 0x8e, 0x49, 0x2b, 0x26, 0x47, 0xb8, 0x3e, 0x99, 0x09, 0x6d, 0x46,
+	0x3a, 0x8d, 0x49, 0x77, 0x0a, 0xee, 0xa7, 0xd7, 0x57, 0x1f, 0x2b, 0x8a, 0x96, 0x2b, 0x8a, 0xbe,
+	0x56, 0x14, 0xbd, 0xae, 0x69, 0xb2, 0x5c, 0xd3, 0xe4, 0x73, 0x4d, 0x93, 0xbb, 0x13, 0xa5, 0xc3,
+	0x6c, 0x3e, 0x66, 0x13, 0x78, 0xe0, 0x7f, 0xf6, 0xf1, 0x54, 0x6d, 0x24, 0x3c, 0x5b, 0xe9, 0xc7,
+	0xdb, 0xc5, 0xdf, 0x5d, 0x7c, 0x07, 0x00, 0x00, 0xff, 0xff, 0x17, 0xa8, 0xbf, 0xff, 0xb6, 0x01,
+	0x00, 0x00,
 }
 
 func (m *UnstakeProposal) Marshal() (dAtA []byte, err error) {
@@ -111,12 +168,19 @@ func (m *UnstakeProposal) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.Provider) > 0 {
-		i -= len(m.Provider)
-		copy(dAtA[i:], m.Provider)
-		i = encodeVarintUnstakeProposal(dAtA, i, uint64(len(m.Provider)))
-		i--
-		dAtA[i] = 0x1a
+	if len(m.ProvidersInfo) > 0 {
+		for iNdEx := len(m.ProvidersInfo) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.ProvidersInfo[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintUnstakeProposal(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x1a
+		}
 	}
 	if len(m.Description) > 0 {
 		i -= len(m.Description)
@@ -129,6 +193,43 @@ func (m *UnstakeProposal) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.Title)
 		copy(dAtA[i:], m.Title)
 		i = encodeVarintUnstakeProposal(dAtA, i, uint64(len(m.Title)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ProviderUnstakeInfo) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ProviderUnstakeInfo) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ProviderUnstakeInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.ChainId) > 0 {
+		i -= len(m.ChainId)
+		copy(dAtA[i:], m.ChainId)
+		i = encodeVarintUnstakeProposal(dAtA, i, uint64(len(m.ChainId)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Provider) > 0 {
+		i -= len(m.Provider)
+		copy(dAtA[i:], m.Provider)
+		i = encodeVarintUnstakeProposal(dAtA, i, uint64(len(m.Provider)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -160,7 +261,26 @@ func (m *UnstakeProposal) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovUnstakeProposal(uint64(l))
 	}
+	if len(m.ProvidersInfo) > 0 {
+		for _, e := range m.ProvidersInfo {
+			l = e.Size()
+			n += 1 + l + sovUnstakeProposal(uint64(l))
+		}
+	}
+	return n
+}
+
+func (m *ProviderUnstakeInfo) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
 	l = len(m.Provider)
+	if l > 0 {
+		n += 1 + l + sovUnstakeProposal(uint64(l))
+	}
+	l = len(m.ChainId)
 	if l > 0 {
 		n += 1 + l + sovUnstakeProposal(uint64(l))
 	}
@@ -268,6 +388,90 @@ func (m *UnstakeProposal) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 3:
 			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ProvidersInfo", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowUnstakeProposal
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthUnstakeProposal
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthUnstakeProposal
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ProvidersInfo = append(m.ProvidersInfo, &ProviderUnstakeInfo{})
+			if err := m.ProvidersInfo[len(m.ProvidersInfo)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipUnstakeProposal(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthUnstakeProposal
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ProviderUnstakeInfo) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowUnstakeProposal
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ProviderUnstakeInfo: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ProviderUnstakeInfo: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Provider", wireType)
 			}
 			var stringLen uint64
@@ -297,6 +501,38 @@ func (m *UnstakeProposal) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.Provider = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ChainId", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowUnstakeProposal
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthUnstakeProposal
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthUnstakeProposal
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ChainId = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
