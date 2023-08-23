0e051322c313b14be538c8193df86681acb86c56
Ran Mishael <ran@lavanet.xyz>
2023-08-23 14:39:25 +0200

    update protocol version

--- protocol/upgrade/protocol_version.go
+++ protocol/upgrade/protocol_version.go
@@ -11,8 +11,8 @@ type ProtocolVersion struct {
 }
 
 var lavaProtocolVersion = ProtocolVersion{
-	ConsumerVersion: "0.21.0",
-	ProviderVersion: "0.21.0",
+	ConsumerVersion: "0.22.0",
+	ProviderVersion: "0.22.0",
 }
 
 func GetCurrentVersion() ProtocolVersion {
