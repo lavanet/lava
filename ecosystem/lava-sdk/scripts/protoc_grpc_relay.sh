d71e527dbdf69789021a40aa576e0912c8203e5e
Ran Mishael <ran@lavanet.xyz>
2023-08-27 17:09:10 +0200

    removing protobufs from lava-sdk

--- ecosystem/lava-sdk/scripts/protoc_grpc_relay.sh
+++ ecosystem/lava-sdk/scripts/protoc_grpc_relay.sh
@@ -1,8 +1,10 @@
-
 #!/bin/bash
+source ./scripts/prepare_protobufs.sh
+# preparing the env
+prepare
 
 ROOT_PROTO_DIR="./proto/cosmos/cosmos-sdk"
-COSMOS_PROTO_DIR="$ROOT_PROTO_DIR/proto"
+COSMOS_PROTO_DIR="$ROOT_PROTO_DIR"
 THIRD_PARTY_PROTO_DIR="../../proto"
 OUT_DIR="./src/grpc_web_services"
 
@@ -15,24 +17,30 @@ protoc --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
     --proto_path="$THIRD_PARTY_PROTO_DIR" \
     "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/relay.proto" \
     "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/badges.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/params.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/provider_payment_storage.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/unique_payment_storage_client_provider.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/subscription/subscription.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/projects/project.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/plans/policy.proto" \
+    "$THIRD_PARTY_PROTO_DIR/lavanet/lava/pairing/epoch_payments.proto" \
     "$THIRD_PARTY_PROTO_DIR/lavanet/lava/epochstorage/stake_entry.proto" \
     "$THIRD_PARTY_PROTO_DIR/lavanet/lava/epochstorage/endpoint.proto" \
     "$COSMOS_PROTO_DIR/gogoproto/gogo.proto" \
     "$COSMOS_PROTO_DIR/google/protobuf/descriptor.proto" \
     "$COSMOS_PROTO_DIR/google/protobuf/wrappers.proto" \
+    "$COSMOS_PROTO_DIR/google/api/annotations.proto" \
+    "$COSMOS_PROTO_DIR/google/api/http.proto" \
     "$COSMOS_PROTO_DIR/cosmos/base/v1beta1/coin.proto" \
+    "$COSMOS_PROTO_DIR/cosmos/base/query/v1beta1/pagination.proto" \
     "$COSMOS_PROTO_DIR/cosmos_proto/cosmos.proto" \
+
 # mv ./src/proto/test ./src/pairing/.
 # rm -rf ./src/proto
 # mv ./src/pairing ./src/proto
+
 
 echo "running fix_grpc_web_camel_case.py"
 python3 ./scripts/fix_grpc_web_camel_case.py
 
 cp -r $OUT_DIR ./bin/src/.
-
-echo "-------------------- CHANGE NEEDED --------------"
-echo "We need to change camel case to snake case in relay_pb.js"
-echo "Also copy the compiled files to the bin"
-echo "read ./scripts/README.md for more information"
-echo "-------------------------------------------------"
