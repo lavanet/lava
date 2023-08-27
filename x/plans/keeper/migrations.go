291fe9ac06221e1309b8f7fd459b86c9b2821a66
oren-lava <oren@lavanet.xyz>
2023-08-27 16:49:33 +0300

    CNS-491: implemented plans migrations

--- x/plans/keeper/migrations.go
+++ x/plans/keeper/migrations.go
@@ -5,7 +5,9 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v2 "github.com/lavanet/lava/x/plans/migrations/v2"
-	plansv3 "github.com/lavanet/lava/x/plans/migrations/v3"
+	v3 "github.com/lavanet/lava/x/plans/migrations/v3"
+	v7 "github.com/lavanet/lava/x/plans/migrations/v7"
+	v8 "github.com/lavanet/lava/x/plans/migrations/v8"
 	projectsv3 "github.com/lavanet/lava/x/projects/migrations/v3"
 )
 
@@ -42,7 +44,7 @@ func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 			}
 
 			// convert plan from type v2.Plan to types.Plan
-			plan_v3 := plansv3.Plan{
+			plan_v3 := v3.Plan{
 				Index:                    plan_v2.Index,
 				Block:                    plan_v2.Block,
 				Price:                    plan_v2.Price,
@@ -97,4 +99,59 @@ func (m Migrator) Migrate5to6(ctx sdk.Context) error {
 // -- trigger fixation migration (v4->v5), initialize IsLatest field
 func (m Migrator) Migrate6to7(ctx sdk.Context) error {
 	return m.keeper.plansFS.MigrateVersion(ctx)
+}
+
+func (m Migrator) Migrate7to8(ctx sdk.Context) error {
+	plansInds := m.keeper.plansFS.GetAllEntryIndices(ctx)
+	for _, ind := range plansInds {
+		blocks := m.keeper.plansFS.GetAllEntryVersions(ctx, ind)
+		for _, block := range blocks {
+			var p7 v7.Plan
+			m.keeper.plansFS.ReadEntry(ctx, ind, block, &p7)
+
+			var cp8arr []v8.ChainPolicy
+			for _, cp7 := range p7.PlanPolicy.ChainPolicies {
+				cp8 := v8.ChainPolicy{
+					ChainId: cp7.ChainId,
+					Apis:    cp7.Apis,
+				}
+
+				var req8arr []v8.ChainRequirement
+				for _, req7 := range cp7.Requirements {
+					req8 := v8.ChainRequirement{
+						Extensions: req7.Extensions,
+						Collection: req7.Collection,
+					}
+					req8arr = append(req8arr, req8)
+				}
+
+				cp8.Requirements = req8arr
+				cp8arr = append(cp8arr, cp8)
+			}
+
+			p8 := v8.Plan{
+				Index:                    p7.Index,
+				Block:                    p7.Block,
+				Price:                    p7.Price,
+				AllowOveruse:             p7.AllowOveruse,
+				OveruseRate:              p7.OveruseRate,
+				Description:              p7.Description,
+				Type:                     p7.Type,
+				AnnualDiscountPercentage: p7.AnnualDiscountPercentage,
+				PlanPolicy: v8.Policy{
+					ChainPolicies:         cp8arr,
+					GeolocationProfile:    int32(p7.PlanPolicy.GeolocationProfile),
+					TotalCuLimit:          p7.PlanPolicy.TotalCuLimit,
+					EpochCuLimit:          p7.PlanPolicy.EpochCuLimit,
+					MaxProvidersToPair:    p7.PlanPolicy.MaxProvidersToPair,
+					SelectedProvidersMode: v8.SELECTED_PROVIDERS_MODE(p7.PlanPolicy.SelectedProvidersMode),
+					SelectedProviders:     p7.PlanPolicy.SelectedProviders,
+				},
+			}
+
+			m.keeper.plansFS.ModifyEntry(ctx, ind, block, &p8)
+		}
+	}
+
+	return nil
 }
