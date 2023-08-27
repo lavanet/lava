934f13dc33233818fbc797a1599cd76c0179541f
oren-lava <oren@lavanet.xyz>
2023-08-27 17:33:08 +0300

    CNS-491: implemented spec params migration

--- x/spec/keeper/migrations.go
+++ x/spec/keeper/migrations.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v3 "github.com/lavanet/lava/x/spec/types/migrations/v3"
+	v4 "github.com/lavanet/lava/x/spec/types/migrations/v4"
 )
 
 type Migrator struct {
@@ -20,5 +22,18 @@ func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 		spec.Name = strings.ToLower(spec.Name)
 		m.keeper.SetSpec(ctx, spec)
 	}
+	return nil
+}
+
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	var paramsV3 v3.Params
+	m.keeper.paramstore.GetParamSet(ctx, &paramsV3)
+
+	var paramsV4 v4.Params
+	paramsV4.GeolocationCount = int32(paramsV3.GeolocationCount)
+	paramsV4.MaxCU = paramsV3.MaxCU
+
+	m.keeper.paramstore.SetParamSet(ctx, &paramsV4)
+
 	return nil
 }
---
+++ x/spec/types/migrations/v4/params.go
@@ -0,0 +1,86 @@
+package v4
+
+import (
+	fmt "fmt"
+
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	KeyGeolocationCount           = []byte("GeolocationCount")
+	DefaultGeolocationCount int32 = 8
+)
+
+var (
+	KeyMaxCU            = []byte("MaxCU")
+	DefaultMaxCU uint64 = 10000
+)
+
+var _ paramtypes.ParamSet = (*Params)(nil)
+
+// ParamKeyTable the param key table for launch module
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
+}
+
+// NewParams creates a new Params instance
+func NewParams(geolocationCount int32, maxCU uint64) Params {
+	return Params{GeolocationCount: geolocationCount, MaxCU: maxCU}
+}
+
+// DefaultParams returns a default set of parameters
+func DefaultParams() Params {
+	return NewParams(DefaultGeolocationCount, DefaultMaxCU)
+}
+
+// ParamSetPairs get the params.ParamSet
+func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyGeolocationCount, &p.GeolocationCount, validateGeolocationCount),
+		paramtypes.NewParamSetPair(KeyMaxCU, &p.MaxCU, validateMaxCU),
+	}
+}
+
+// Validate validates the set of params
+func (p Params) Validate() error {
+	if err := validateGeolocationCount(p.GeolocationCount); err != nil {
+		return err
+	}
+
+	if err := validateMaxCU(p.MaxCU); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// String implements the Stringer interface.
+func (p Params) String() string {
+	out, _ := yaml.Marshal(p)
+	return string(out)
+}
+
+func validateGeolocationCount(v interface{}) error {
+	geolocationCount, ok := v.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	// TODO implement validation
+	_ = geolocationCount
+
+	return nil
+}
+
+func validateMaxCU(v interface{}) error {
+	maxCU, ok := v.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	// TODO implement validation
+	_ = maxCU
+
+	return nil
+}
