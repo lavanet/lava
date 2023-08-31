4ce607a724fbcb60d902ef506e57f858e6636db5
oren-lava <oren@lavanet.xyz>
2023-08-31 18:40:43 +0300

    CNS-524: scaffolded and implented providerDelegators query

--- x/dualstaking/client/cli/query.go
+++ x/dualstaking/client/cli/query.go
@@ -26,6 +26,7 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	cmd.AddCommand(CmdQueryParams())
 	cmd.AddCommand(CmdQueryDelegatorProviders())
+	cmd.AddCommand(CmdQueryProviderDelegators())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
