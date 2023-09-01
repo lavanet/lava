99f3b8c93c77deb8e74d2870743d6010977b3b04
Oren Laadan <orenl@lavanet.xyz>
2023-09-02 00:34:38 +0800

    CNS-570: delegate: simplfy tests of transactions

--- x/dualstaking/keeper/delegate_test.go
+++ x/dualstaking/keeper/delegate_test.go
@@ -1,11 +1,11 @@
 package keeper_test
 
 import (
+"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lavanet/lava/testutil/common"
-	"github.com/lavanet/lava/x/dualstaking/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -84,14 +84,7 @@ func TestDelegateFail(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			amount := sdk.NewCoin("ulava", sdk.ZeroInt())
 			amount.Amount = amount.Amount.Add(sdk.NewInt(tt.amount))
-			msg := types.MsgDelegate{
-				Creator:  tt.delegator,
-				Provider: tt.provider,
-				ChainID:  tt.chainID,
-				Amount:   amount,
-			}
-
-			_, err := ts.TxDualstakingDelegate(msg.Creator, msg.Provider, msg.ChainID, msg.Amount)
+			_, err := ts.TxDualstakingDelegate(tt.delegator, tt.provider, tt.chainID, amount)
 			require.Error(t, err, tt.name)
 		})
 	}
@@ -106,47 +99,41 @@ func TestDelegate(t *testing.T) {
 	_, client1Addr := ts.GetAccount(common.CONSUMER, 0)
 	provider1Acct, provider1Addr := ts.GetAccount(common.PROVIDER, 0)
 
-	msg := types.MsgDelegate{
-		Creator:  client1Addr,
-		Provider: provider1Addr,
-		ChainID:  ts.spec.Name,
-		Amount:   sdk.NewCoin("ulava", sdk.NewInt(10000)),
-	}
-
 	delegated := zeroCoin
 	bonded := zeroCoin
 
 	// delegate once
-	_, err := ts.TxDualstakingDelegate(msg.Creator, msg.Provider, msg.ChainID, msg.Amount)
+	amount := sdk.NewCoin("ulava", sdk.NewInt(10000))
+	_, err := ts.TxDualstakingDelegate(client1Addr, provider1Addr, ts.spec.Name, amount)
 	require.NoError(t, err)
 	// not yet in effect
-	bonded = bonded.Add(msg.Amount)
-	stakeEntry := ts.getStakeEntry(provider1Acct.Addr, msg.ChainID)
+	bonded = bonded.Add(amount)
+	stakeEntry := ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
 	require.True(t, delegated.IsEqual(stakeEntry.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 	// advance epoch to digest the delegate
 	ts.AdvanceEpoch()
 	// now in effect
-	delegated = delegated.Add(msg.Amount)
-	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, msg.ChainID)
+	delegated = delegated.Add(amount)
+	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
 	require.True(t, delegated.IsEqual(stakeEntry.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 
 	// delegate twice same block (fail)
-	_, err = ts.TxDualstakingDelegate(msg.Creator, msg.Provider, msg.ChainID, msg.Amount)
+	_, err = ts.TxDualstakingDelegate(client1Addr, provider1Addr, ts.spec.Name, amount)
 	require.NoError(t, err)
-	bonded = bonded.Add(msg.Amount)
-	_, err = ts.TxDualstakingDelegate(msg.Creator, msg.Provider, msg.ChainID, msg.Amount)
+	bonded = bonded.Add(amount)
+	_, err = ts.TxDualstakingDelegate(client1Addr, provider1Addr, ts.spec.Name, amount)
 	require.NoError(t, err)
-	bonded = bonded.Add(msg.Amount)
-	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, msg.ChainID)
+	bonded = bonded.Add(amount)
+	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
 	require.True(t, delegated.IsEqual(stakeEntry.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 	// advance epoch to digest the delegate
 	ts.AdvanceEpoch()
 	// now in effect
-	delegated = delegated.Add(msg.Amount).Add(msg.Amount) // two delegations
-	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, msg.ChainID)
+	delegated = delegated.Add(amount).Add(amount) // two delegations
+	stakeEntry = ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
 	require.True(t, delegated.IsEqual(stakeEntry.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 }
@@ -154,7 +141,7 @@ func TestDelegate(t *testing.T) {
 func TestRedelegateFail(t *testing.T) {
 	ts := newTester(t)
 
-	// 1 delegator, 1 provider staked, 1 provider unstaked, 1 provider unstaking
+	// 1 delegator, 2 provider staked, 1 provider unstaked, 1 provider unstaking
 	ts.setupForDelegation(1, 2, 1, 1)
 
 	_, client1Addr := ts.GetAccount(common.CONSUMER, 0)
@@ -163,15 +150,9 @@ func TestRedelegateFail(t *testing.T) {
 	_, provider3Addr := ts.GetAccount(common.PROVIDER, 2)
 	_, provider4Addr := ts.GetAccount(common.PROVIDER, 3)
 
-	msg1 := types.MsgDelegate{
-		Creator:  client1Addr,
-		Provider: provider1Addr,
-		ChainID:  ts.spec.Name,
-		Amount:   sdk.NewCoin("ulava", sdk.NewInt(10000)),
-	}
-
 	// delegate once for setup
-	_, err := ts.TxDualstakingDelegate(msg1.Creator, msg1.Provider, msg1.ChainID, msg1.Amount)
+	amount := sdk.NewCoin("ulava", sdk.NewInt(10000))
+	_, err := ts.TxDualstakingDelegate(client1Addr, provider1Addr, ts.spec.Name, amount)
 	require.NoError(t, err)
 
 	template := []struct {
@@ -252,23 +233,8 @@ func TestRedelegateFail(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			amount := sdk.NewCoin("ulava", sdk.ZeroInt())
 			amount.Amount = amount.Amount.Add(sdk.NewInt(tt.amount))
-			msg := types.MsgRedelegate{
-				Creator:      tt.delegator,
-				FromProvider: tt.provider1,
-				ToProvider:   tt.provider2,
-				FromChainID:  tt.chainID,
-				ToChainID:    tt.chainID,
-				Amount:       amount,
-			}
-
 			_, err := ts.TxDualstakingRedelegate(
-				msg.Creator,
-				msg.FromProvider,
-				msg.ToProvider,
-				msg.FromChainID,
-				msg.ToChainID,
-				msg.Amount,
-			)
+				tt.delegator, tt.provider1, tt.provider2, tt.chainID, tt.chainID, amount)
 			require.Error(t, err, tt.name)
 		})
 	}
@@ -284,63 +250,44 @@ func TestRedelegate(t *testing.T) {
 	provider1Acct, provider1Addr := ts.GetAccount(common.PROVIDER, 0)
 	provider2Acct, provider2Addr := ts.GetAccount(common.PROVIDER, 1)
 
-	msg1 := types.MsgDelegate{
-		Creator:  client1Addr,
-		Provider: provider1Addr,
-		ChainID:  ts.spec.Name,
-		Amount:   sdk.NewCoin("ulava", sdk.NewInt(10000)),
-	}
-
-	msg2 := types.MsgRedelegate{
-		Creator:      client1Addr,
-		FromProvider: provider1Addr,
-		ToProvider:   provider2Addr,
-		FromChainID:  ts.spec.Name,
-		ToChainID:    ts.spec.Name,
-		Amount:       sdk.NewCoin("ulava", sdk.NewInt(5000)),
-	}
-
 	delegated1 := zeroCoin
 	delegated2 := zeroCoin
 	bonded := zeroCoin
 
 	// delegate once
-	_, err := ts.TxDualstakingDelegate(msg1.Creator, msg1.Provider, msg1.ChainID, msg1.Amount)
+	amount := sdk.NewCoin("ulava", sdk.NewInt(10000))
+	_, err := ts.TxDualstakingDelegate(
+		client1Addr, provider1Addr, ts.spec.Name, amount)
 	require.NoError(t, err)
-	bonded = bonded.Add(msg1.Amount)
+	bonded = bonded.Add(amount)
 	// advance epoch to digest the delegate
 	ts.AdvanceEpoch()
 	// now in effect
-	delegated1 = delegated1.Add(msg1.Amount)
-	stakeEntry1 := ts.getStakeEntry(provider1Acct.Addr, msg1.ChainID)
-	stakeEntry2 := ts.getStakeEntry(provider2Acct.Addr, msg1.ChainID)
+	delegated1 = delegated1.Add(amount)
+	stakeEntry1 := ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
+	stakeEntry2 := ts.getStakeEntry(provider2Acct.Addr, ts.spec.Name)
 	require.True(t, delegated1.IsEqual(stakeEntry1.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 	require.True(t, delegated2.IsEqual(stakeEntry2.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 
 	// redelegate once
+	amount = sdk.NewCoin("ulava", sdk.NewInt(5000))
 	_, err = ts.TxDualstakingRedelegate(
-		msg2.Creator,
-		msg2.FromProvider,
-		msg2.ToProvider,
-		msg2.FromChainID,
-		msg2.ToChainID,
-		msg2.Amount,
-	)
+		client1Addr, provider1Addr, provider2Addr, ts.spec.Name, ts.spec.Name, amount)
 	require.NoError(t, err)
-	stakeEntry1 = ts.getStakeEntry(provider1Acct.Addr, msg1.ChainID)
-	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, msg2.ToChainID)
+	stakeEntry1 = ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
+	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, ts.spec.Name)
 	require.True(t, delegated1.IsEqual(stakeEntry1.DelegateTotal))
 	require.True(t, delegated2.IsEqual(stakeEntry2.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 	// advance epoch to digest the delegate
 	ts.AdvanceEpoch()
 	// now in effect
-	delegated1 = delegated1.Sub(msg2.Amount)
-	delegated2 = delegated2.Add(msg2.Amount)
-	stakeEntry1 = ts.getStakeEntry(provider1Acct.Addr, msg1.ChainID)
-	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, msg2.ToChainID)
+	delegated1 = delegated1.Sub(amount)
+	delegated2 = delegated2.Add(amount)
+	stakeEntry1 = ts.getStakeEntry(provider1Acct.Addr, ts.spec.Name)
+	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, ts.spec.Name)
 	require.True(t, delegated1.IsEqual(stakeEntry1.DelegateTotal))
 	require.True(t, delegated2.IsEqual(stakeEntry2.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
@@ -349,20 +296,15 @@ func TestRedelegate(t *testing.T) {
 	require.NoError(t, err)
 
 	// redelegate from unstaking provider
+	amount = sdk.NewCoin("ulava", sdk.NewInt(5000))
 	_, err = ts.TxDualstakingRedelegate(
-		msg2.Creator,
-		msg2.FromProvider,
-		msg2.ToProvider,
-		msg2.FromChainID,
-		msg2.ToChainID,
-		msg2.Amount,
-	)
+		client1Addr, provider1Addr, provider2Addr, ts.spec.Name, ts.spec.Name, amount)
 	require.NoError(t, err)
 	// advance epoch to digest the delegate
 	ts.AdvanceEpoch()
 	// now in effect
-	delegated2 = delegated2.Add(msg2.Amount)
-	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, msg2.ToChainID)
+	delegated2 = delegated2.Add(amount)
+	stakeEntry2 = ts.getStakeEntry(provider2Acct.Addr, ts.spec.Name)
 	require.True(t, delegated2.IsEqual(stakeEntry2.DelegateTotal))
 	require.True(t, bonded.Amount.Equal(ts.Keepers.Dualstaking.TotalBondedTokens(ts.Ctx)))
 
