e9eef1f1d68db0dde02eea5b726a63d611e45b01
Oren Laadan <orenl@lavanet.xyz>
2023-09-04 02:02:38 +0800

    CNS-570: upgrade migration for epochstorage (due to dualstaking)
    Having added `DelegateTotal` and `DelegateLimit` both of type sdk.Coin, we
    must initialize them so that they use the expected Denom field. Also need
    to initialize the commission to 100(%).
    
    NOTE: for the eventual upgrade, also need to add the new module dualstaking
    to the upgrade.

--- x/epochstorage/keeper/migrations.go
+++ x/epochstorage/keeper/migrations.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/utils"
+	"github.com/lavanet/lava/x/epochstorage/types"
 )
 
 type Migrator struct {
@@ -30,6 +32,39 @@ func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 				m.keeper.SetStakeStorage(ctx, storage)
 			}
 		}
+	}
+	return nil
+}
+
+// Migrate3to4 implements store migration from v3 to v4:
+// - initialize DelegateTotal, DelegateLimit, DelegateCommission
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	const ProviderKey = "provider"
+
+	utils.LavaFormatDebug("migrate: epochstorage")
+
+	storage := m.keeper.GetAllStakeStorage(ctx)
+	for _, storage := range storage {
+		if storage.Index[:len(ProviderKey)] != ProviderKey {
+			utils.LavaFormatDebug("migrate: skip storage with key",
+				utils.Attribute{Key: "index", Value: storage.Index})
+			continue
+		}
+		if len(storage.Index) <= len(ProviderKey) {
+			utils.LavaFormatDebug("migrate: skip storage with short key",
+				utils.Attribute{Key: "index", Value: storage.Index})
+			continue
+		}
+		utils.LavaFormatDebug("migrate: handle storage with key",
+			utils.Attribute{Key: "index", Value: storage.Index})
+		for i := range storage.StakeEntries {
+			utils.LavaFormatDebug("  StakeEntry",
+				utils.Attribute{Key: "address", Value: storage.StakeEntries[i].Address})
+			storage.StakeEntries[i].DelegateTotal = sdk.NewCoin(types.TokenDenom, sdk.ZeroInt())
+			storage.StakeEntries[i].DelegateLimit = sdk.NewCoin(types.TokenDenom, sdk.ZeroInt())
+			storage.StakeEntries[i].DelegateCommission = 100
+		}
+		m.keeper.SetStakeStorage(ctx, storage)
 	}
 	return nil
 }
