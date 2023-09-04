2d74a1afa1708c010af91fa1ea2dfce7463d8ef8
oren-lava <oren@lavanet.xyz>
2023-09-04 15:21:30 +0300

    CNS-531: scaffold map delegator reward

---
+++ x/dualstaking/keeper/delegator_reward.go
@@ -0,0 +1,61 @@
+package keeper
+
+import (
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/x/dualstaking/types"
+)
+
+// SetDelegatorReward set a specific DelegatorReward in the store from its index
+func (k Keeper) SetDelegatorReward(ctx sdk.Context, delegatorReward types.DelegatorReward) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.DelegatorRewardKeyPrefix))
+	b := k.cdc.MustMarshal(&delegatorReward)
+	store.Set(types.DelegatorRewardKey(
+		delegatorReward.Index,
+	), b)
+}
+
+// GetDelegatorReward returns a DelegatorReward from its index
+func (k Keeper) GetDelegatorReward(
+	ctx sdk.Context,
+	index string,
+) (val types.DelegatorReward, found bool) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.DelegatorRewardKeyPrefix))
+
+	b := store.Get(types.DelegatorRewardKey(
+		index,
+	))
+	if b == nil {
+		return val, false
+	}
+
+	k.cdc.MustUnmarshal(b, &val)
+	return val, true
+}
+
+// RemoveDelegatorReward removes a DelegatorReward from the store
+func (k Keeper) RemoveDelegatorReward(
+	ctx sdk.Context,
+	index string,
+) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.DelegatorRewardKeyPrefix))
+	store.Delete(types.DelegatorRewardKey(
+		index,
+	))
+}
+
+// GetAllDelegatorReward returns all DelegatorReward
+func (k Keeper) GetAllDelegatorReward(ctx sdk.Context) (list []types.DelegatorReward) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.DelegatorRewardKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.DelegatorReward
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
+}
