224857921ca6ae40ff1fa2c857d5ba7283ca1415
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-09-13 10:04:24 +0300

    version updated to v0.22.3

--- protocol/upgrade/protocol_version.go
+++ protocol/upgrade/protocol_version.go
@@ -14,8 +14,8 @@ type ProtocolVersion struct {
 }
 
 var lavaProtocolVersion = ProtocolVersion{
-	ConsumerVersion: "0.22.1",
-	ProviderVersion: "0.22.1",
+	ConsumerVersion: "0.22.3",
+	ProviderVersion: "0.22.3",
 }
 
 func GetCurrentVersion() ProtocolVersion {
