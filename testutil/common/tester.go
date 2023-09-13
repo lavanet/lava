8044a603407c045ca50168647d8cf4d05f282b81
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-09-13 07:48:00 +0000

    add default values to stake provider

--- testutil/common/tester.go
+++ testutil/common/tester.go
@@ -427,12 +427,14 @@ func (ts *Tester) TxPairingStakeProvider(
 	moniker string,
 ) (*pairingtypes.MsgStakeProviderResponse, error) {
 	msg := &pairingtypes.MsgStakeProvider{
-		Creator:     addr,
-		ChainID:     chainID,
-		Amount:      amount,
-		Geolocation: geoloc,
-		Endpoints:   endpoints,
-		Moniker:     moniker,
+		Creator:            addr,
+		ChainID:            chainID,
+		Amount:             amount,
+		Geolocation:        geoloc,
+		Endpoints:          endpoints,
+		Moniker:            moniker,
+		DelegateLimit:      sdk.NewCoin(epochstoragetypes.TokenDenom, sdk.ZeroInt()),
+		DelegateCommission: 100,
 	}
 	return ts.Servers.PairingServer.StakeProvider(ts.GoCtx, msg)
 }
