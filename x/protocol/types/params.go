c3b211859dbf30c713b4e29b5d9edaced9769ce5
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-09-19 16:39:46 +0300

    protocol version change v0.23.4

--- x/protocol/types/params.go
+++ x/protocol/types/params.go
@@ -14,9 +14,9 @@ var _ paramtypes.ParamSet = (*Params)(nil)
 var (
 	KeyVersion     = []byte("Version")
 	DefaultVersion = Version{
-		ProviderTarget: "0.23.3",
+		ProviderTarget: "0.23.4",
 		ProviderMin:    "0.21.0",
-		ConsumerTarget: "0.23.3",
+		ConsumerTarget: "0.23.4",
 		ConsumerMin:    "0.21.0",
 	}
 )
