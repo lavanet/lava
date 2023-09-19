f0efbbb88bb6f843487bb729a10018d5e3f9ad12
Elad Gildnur <elad.gildnur@gmail.com>
2023-09-19 12:36:53 +0300

    Add a log for payment handler

--- protocol/rpcprovider/rewardserver/reward_server.go
+++ protocol/rpcprovider/rewardserver/reward_server.go
@@ -357,6 +357,8 @@ func (rws *RewardServer) PaymentHandler(payment *PaymentRequest) {
 			utils.LavaFormatWarning("tried removing payment that wasn't expected", nil, utils.Attribute{Key: "payment", Value: payment})
 		}
 
+		utils.LavaFormatDebug("deleting claimed rewards", utils.Attribute{Key: "payment uid", Value: payment.UniqueIdentifier})
+
 		err = rws.rewardDB.DeleteClaimedRewards(payment.PaymentEpoch, payment.Client.String(), payment.UniqueIdentifier, payment.ConsumerRewardsKey)
 		if err != nil {
 			utils.LavaFormatWarning("failed deleting claimed rewards", err)
