da5b0089bd9a4d9c41db9f0bac54047346c592ea
Elad Gildnur <elad.gildnur@gmail.com>
2023-09-27 19:26:30 +0300

    Add block parsing to rest

--- ecosystem/lava-sdk/src/chainlib/rest.ts
+++ ecosystem/lava-sdk/src/chainlib/rest.ts
@@ -7,7 +7,11 @@ import {
   HeadersPassSend,
 } from "../chainlib/base_chain_parser";
 import { Logger } from "../logger/logger";
-import { NOT_APPLICABLE } from "../common/common";
+import { HttpMethod, NOT_APPLICABLE } from "../common/common";
+import { Parser } from "../parser/parser";
+import { FUNCTION_TAG } from "../grpc_web_services/lavanet/lava/spec/api_collection_pb";
+import { RestMessage } from "./chainproxy/rpcInterfaceMessages/rest_message";
+import { ParsedMessage } from "./chain_message";
 export class RestChainParser extends BaseChainParser {
   constructor() {
     super();
@@ -44,7 +48,58 @@ export class RestChainParser extends BaseChainParser {
       apiCollection,
       HeadersPassSend
     );
-    // TODO: implement apip.GetParsingByTag to support headers
+
+    const [settingHeaderDirective] = this.getParsingByTag(
+      FUNCTION_TAG.SET_LATEST_IN_METADATA
+    );
+
+    let restMessage = new RestMessage(
+      options.data,
+      options.url,
+      apiCont.api.getName(),
+      {
+        headers: headerHandler.filteredHeaders,
+        latestBlockHeaderSetter: undefined,
+      }
+    );
+
+    if (options.connectionType === HttpMethod.GET) {
+      restMessage = new RestMessage(
+        undefined,
+        options.url + String(options.data),
+        apiCont.api.getName(),
+        {
+          headers: headerHandler.filteredHeaders,
+          latestBlockHeaderSetter: settingHeaderDirective,
+        }
+      );
+    }
+
+    let requestedBlock: number | Error | null;
+
+    const overwriteRequestedBlock = headerHandler.overwriteRequestedBlock;
+    if (overwriteRequestedBlock == "") {
+      const blockParser = apiCont.api.getBlockParsing();
+      if (!blockParser) {
+        throw Logger.fatal("BlockParsing is missing");
+        overwriteRequestedBlock;
+      }
+      requestedBlock = Parser.ParseBlockFromParams(restMessage, blockParser);
+
+      if (!requestedBlock) {
+        throw Logger.fatal(
+          `ParseBlockFromParams failed parsing block for chain: ${this.spec?.getName()}, blockParsing: ${blockParser}`
+        );
+      }
+    } else {
+      requestedBlock = restMessage.parseBlock(overwriteRequestedBlock);
+      if (requestedBlock instanceof Error) {
+        throw Logger.fatal(
+          `Failed parsing block from an overwrite header for chain: ${this.spec?.getName()}, overwriteRequestedBlock: ${overwriteRequestedBlock}`
+        );
+      }
+    }
+
     let data = "";
     if (options.data) {
       data = "?";
@@ -52,15 +107,26 @@ export class RestChainParser extends BaseChainParser {
         data = data + key + "=" + options.data[key] + "&";
       }
     }
-    // TODO: add block parsing and use header overwrite.
-    const chainMessage = new ChainMessage(
+
+    const parsedMessage = new ParsedMessage(
+      apiCont.api,
+      requestedBlock,
+      undefined,
+      restMessage,
+      apiCollection,
+      undefined
+    );
+
+    // TODO: add extension parsing.
+
+    // TODO: Change to parsedMessage when ready
+    // return parsedMessage;
+    return new ChainMessage(
       NOT_APPLICABLE,
       apiCont.api,
       apiCollection,
       data,
       options.url
     );
-    // TODO: add extension parsing.
-    return chainMessage;
   }
 }
