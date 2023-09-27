b9d0772de821cdf85195c029cf4825f6027358b4
Elad Gildnur <elad.gildnur@gmail.com>
2023-09-28 02:13:46 +0300

    Use jsonrpcmessage in jsonrpc

--- ecosystem/lava-sdk/src/chainlib/jsonrpc.ts
+++ ecosystem/lava-sdk/src/chainlib/jsonrpc.ts
@@ -7,10 +7,14 @@ import {
   HeadersPassSend,
 } from "../chainlib/base_chain_parser";
 import { Logger } from "../logger/logger";
-import { generateRPCData } from "../util/common";
-import { NOT_APPLICABLE } from "../common/common";
+import { encodeUtf8, generateRPCData } from "../util/common";
+import { HttpMethod, NOT_APPLICABLE } from "../common/common";
+import { FUNCTION_TAG } from "../grpc_web_services/lavanet/lava/spec/api_collection_pb";
+import { JsonrpcMessage } from "./chainproxy/rpcInterfaceMessages/json_rpc_message";
+import { Parser } from "../parser/parser";
+import { ParsedMessage } from "./chain_message";
 
-const MethodPost = "POST";
+const jsonrpcVersion = "2.0";
 export class JsonRpcChainParser extends BaseChainParser {
   constructor() {
     super();
@@ -22,18 +26,77 @@ export class JsonRpcChainParser extends BaseChainParser {
         "Wrong relay options provided, expected SendRestRelayOptions got SendRelayOptions"
       );
     }
-    const apiCont = this.getSupportedApi(options.method, MethodPost);
+
+    const apiCont = this.getSupportedApi(options.method, HttpMethod.POST);
     const apiCollection = this.getApiCollection({
       addon: apiCont.collectionKey.addon,
-      connectionType: MethodPost,
+      connectionType: HttpMethod.POST,
       internalPath: apiCont.collectionKey.internalPath,
     });
+
     const headerHandler = this.handleHeaders(
       options.metadata,
       apiCollection,
       HeadersPassSend
     );
-    // TODO: implement apip.GetParsingByTag to support headers
+
+    const [settingHeaderDirective] = this.getParsingByTag(
+      FUNCTION_TAG.SET_LATEST_IN_METADATA
+    );
+
+    const jsonrpcMessage = new JsonrpcMessage();
+    jsonrpcMessage.initJsonrpcMessage(
+      jsonrpcVersion,
+      encodeUtf8(
+        String(
+          options.id ?? Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+        )
+      ),
+      options.method,
+      options.params
+    );
+    jsonrpcMessage.initBaseMessage({
+      headers: headerHandler.filteredHeaders,
+      latestBlockHeaderSetter: settingHeaderDirective,
+    });
+
+    const blockParser = apiCont.api.getBlockParsing();
+    if (!blockParser) {
+      throw Logger.fatal("BlockParsing is missing");
+    }
+
+    let requestedBlock: number | Error | null;
+
+    const overwriteRequestedBlock = headerHandler.overwriteRequestedBlock;
+    if (overwriteRequestedBlock === "") {
+      requestedBlock = Parser.ParseBlockFromParams(jsonrpcMessage, blockParser);
+      if (!requestedBlock) {
+        throw Logger.fatal(
+          `ParseBlockFromParams failed parsing block for chain: ${this.spec?.getName()}, blockParsing: ${blockParser}`
+        );
+      }
+    } else {
+      requestedBlock = jsonrpcMessage.parseBlock(overwriteRequestedBlock);
+      if (requestedBlock instanceof Error) {
+        throw Logger.fatal(
+          `Failed parsing block from an overwrite header for chain: ${this.spec?.getName()}, overwriteRequestedBlock: ${overwriteRequestedBlock}`
+        );
+      }
+    }
+
+    const parsedMessage = new ParsedMessage(
+      apiCont.api,
+      requestedBlock,
+      undefined,
+      jsonrpcMessage,
+      apiCollection,
+      undefined
+    );
+
+    // TODO: add extension parsing.
+
+    // TODO: Change to parsedMessage when ready
+    // return parsedMessage;
     const chainMessage = new ChainMessage(
       NOT_APPLICABLE,
       apiCont.api,
