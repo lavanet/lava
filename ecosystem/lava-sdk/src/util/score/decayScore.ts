c0134255ec7ce2d17d2f03e185cb4ecd4485e802
bunea <andrei@bunea.me>
2023-09-29 19:24:25 +0300

    Add comments to score store

--- ecosystem/lava-sdk/src/util/score/decayScore.ts
+++ ecosystem/lava-sdk/src/util/score/decayScore.ts
@@ -7,6 +7,22 @@ export class ScoreStore {
     public readonly time: number
   ) {}
 
+  /**
+   * Calculates the time decayed score update between two `ScoreStore` entries.
+   * It uses a decay function with a half life of `halfLife` to factor in the time
+   * elapsed since the `oldScore` was recorded. Both the numerator and the denominator
+   * are decayed by this function.
+   *
+   * Additionally, the `newScore` is factored by a weight of `updateWeight`.
+   * The function returns a new `ScoreStore` entry with the updated numerator, denominator, and current time.
+   *
+   * The mathematical equation used to calculate the update is:
+   *
+   * updatedNum   = oldScore.Num * exp(-(sampleTime - oldScore.time) / halfLife) + newScore.Num * (-(sampleTime - newScore.time) / halfLife) * updateWeight
+   * updatedDenom = oldScore.Denom * exp(-(sampleTime - oldScore.time) / halfLife) + newScore.Denom * (-(sampleTime - newScore.time) / halfLife) * updateWeight
+   *
+   * Note that the returned `ScoreStore` has a new time field set to the sample time.
+   */
   public static calculateTimeDecayFunctionUpdate(
     oldScore: ScoreStore,
     newScore: ScoreStore,
