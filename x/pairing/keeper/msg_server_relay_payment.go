a7fdd9ca276b2e8b56574114964d79ece36ff4fc
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-10-04 09:41:25 +0000

    now cu will be the floor of the limit and the cu

--- x/pairing/keeper/msg_server_relay_payment.go
+++ x/pairing/keeper/msg_server_relay_payment.go
@@ -186,7 +186,7 @@ func (k msgServer) RelayPayment(goCtx context.Context, msg *types.MsgRelayPaymen
 			)
 		}
 
-		err = k.Keeper.EnforceClientCUsUsageInEpoch(ctx, allowedCU, totalCUInEpochForUserProvider, clientAddr, relay.SpecId, uint64(relay.Epoch))
+		relay.CuSum, err = k.Keeper.EnforceClientCUsUsageInEpoch(ctx, allowedCU, totalCUInEpochForUserProvider, clientAddr, relay.SpecId, uint64(relay.Epoch))
 		if err != nil {
 			// TODO: maybe give provider money but burn user, colluding?
 			// TODO: display correct totalCU and usedCU for provider
