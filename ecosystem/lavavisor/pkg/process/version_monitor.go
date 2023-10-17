42dd8941bcb5748d837ded246aa1e349e4b1e651
Ran Mishael <ran@lavanet.xyz>
2023-10-17 14:57:48 +0200

    linker verbosity checks

--- ecosystem/lavavisor/pkg/process/version_monitor.go
+++ ecosystem/lavavisor/pkg/process/version_monitor.go
@@ -42,11 +42,9 @@ func NewVersionMonitor(initVersion string, lavavisorPath string, processes []str
 	}
 }
 
-func (vm *VersionMonitor) handleUpdateTrigger(incoming *protocoltypes.Version) error {
+func (vm *VersionMonitor) handleUpdateTrigger() error {
 	// set latest known version to incoming.
 	utils.LavaFormatInfo("Update detected. Lavavisor starting the auto-upgrade...")
-	vm.lastKnownVersion = incoming
-
 	// 1. check lavavisor directory first and attempt to fetch new binary from there
 	versionDir := filepath.Join(vm.LavavisorPath, "upgrades", "v"+vm.lastKnownVersion.ProviderTarget)
 	binaryPath := filepath.Join(versionDir, "lavap")
@@ -57,9 +55,13 @@ func (vm *VersionMonitor) handleUpdateTrigger(incoming *protocoltypes.Version) e
 	if err != nil {
 		return utils.LavaFormatError("Lavavisor was not able to fetch updated version. Skipping.", err, utils.Attribute{Key: "Version", Value: vm.lastKnownVersion.ProviderTarget})
 	}
+	return vm.createLinkAndRestartServices()
+}
 
+// create a link for lavap from the binary path and restart the services
+func (vm *VersionMonitor) createLinkAndRestartServices() error {
 	// linker
-	err = vm.protocolBinaryLinker.CreateLink(binaryPath)
+	err := vm.protocolBinaryLinker.CreateLink(vm.BinaryPath)
 	if err != nil {
 		return utils.LavaFormatError("Lavavisor was not able to create link to the binaries. Skipping.", err, utils.Attribute{Key: "Version", Value: vm.lastKnownVersion.ProviderTarget})
 	}
@@ -96,6 +98,41 @@ func (vm *VersionMonitor) handleUpdateTrigger(incoming *protocoltypes.Version) e
 	return nil
 }
 
+func (vm *VersionMonitor) validateLinkPointsToTheRightTarget() error {
+	lavapPath, err := vm.protocolBinaryLinker.FindLavaProtocolPath(vm.BinaryPath)
+	if err != nil {
+		return utils.LavaFormatError("Failed searching for lavap path, failed to validate link exists for lavap latest version", err)
+	}
+	var createLink bool
+	_, err = os.Stat(lavapPath)
+	if err != nil {
+		// failed to validate lavap path.
+		utils.LavaFormatDebug("lavap link path was not found attempting to create link", utils.Attribute{Key: "lavap_path", Value: lavapPath})
+		createLink = true
+	} else {
+		utils.LavaFormatDebug("lavap link path found, validating linked binary is the right one", utils.Attribute{Key: "lavap_path", Value: lavapPath})
+		// read link
+		targetPath, err := os.Readlink(lavapPath)
+		if err != nil {
+			utils.LavaFormatInfo("failed reading link from lavap path", utils.Attribute{Key: "error", Value: err})
+			createLink = true
+		} else {
+			// utils.LavaFormatDebug("target validation", utils.Attribute{Key: "targetPath", Value: targetPath})
+			if targetPath != vm.BinaryPath {
+				utils.LavaFormatInfo("lavap link was pointing to the wrong binary. removing and creating a new link")
+				createLink = true
+			} else {
+				// utils.LavaFormatDebug("targets match", utils.Attribute{Key: "targetPath", Value: targetPath}, utils.Attribute{Key: " vm.BinaryPath", Value: vm.BinaryPath})
+			}
+		}
+	}
+	if createLink {
+		utils.LavaFormatInfo("Attempting new link creation for lavap path", utils.Attribute{Key: "lavap", Value: lavapPath}, utils.Attribute{Key: "binary path", Value: vm.BinaryPath})
+		err = vm.createLinkAndRestartServices()
+	}
+	return err
+}
+
 func (vm *VersionMonitor) ValidateProtocolVersion(incoming *statetracker.ProtocolVersionResponse) error {
 	if !vm.lock.TryLock() { // if an upgrade is currently ongoing we don't need to check versions. just wait for the flow to end.
 		utils.LavaFormatDebug("ValidateProtocolVersion is locked, assuming upgrade is ongoing")
@@ -103,15 +140,23 @@ func (vm *VersionMonitor) ValidateProtocolVersion(incoming *statetracker.Protoco
 	}
 	defer vm.lock.Unlock()
 	currentBinaryVersion, _ := GetBinaryVersion(vm.BinaryPath)
+	vm.lastKnownVersion = incoming.Version
 
 	if currentBinaryVersion == "" || ValidateMismatch(incoming.Version, currentBinaryVersion) {
 		utils.LavaFormatInfo("New version detected", utils.Attribute{Key: "incoming", Value: incoming})
 		utils.LavaFormatInfo("Started Version Upgrade flow")
-		err := vm.handleUpdateTrigger(incoming.Version)
+		err := vm.handleUpdateTrigger()
 		if err != nil {
 			utils.LavaFormatInfo("protocol update failed, lavavisor will continue trying to upgrade version every block until it succeeds")
 		}
 		return err
+
+	} else if currentBinaryVersion != "" {
+		// in case we have the latest version already installed we need to validate a few things.
+		err := vm.validateLinkPointsToTheRightTarget()
+		if err != nil {
+			return utils.LavaFormatError("Failed to validateLinkPointsToTheRightTarget", err)
+		}
 	}
 
 	// version is ok.
