b805ec3c5dd642dc77236d09d34d2e93cfc8d6d3
Elad Gildnur <elad.gildnur@gmail.com>
2023-11-13 20:46:49 +0200

    Fix SDK setup scripts

--- ecosystem/lava-sdk/scripts/setup_sdk_with_local_badge.sh
+++ ecosystem/lava-sdk/scripts/setup_sdk_with_local_badge.sh
@@ -18,6 +18,8 @@ pairing=$(lavad q pairing providers LAV1)
 addresses=$(echo "$pairing" | grep 'iPPORT:' | awk '{print $2}' | sort -u)
 public_addresses=$(echo "$pairing" | grep 'address:' | awk '{print $3}')
 
+GEOLOCATION=1
+
 # Associative array to store the JSON structure
 declare -A json_data
 index=0;
@@ -38,22 +40,22 @@ for rpc_address in $addresses; do
             break
         fi
         echo "Adding Provider: $rpc_address, $public_address"
-        json_data["1"]+="{
+        json_data["$GEOLOCATION"]+="{
         \"rpcAddress\": \"$rpc_address\",
         \"publicAddress\": \"$public_address\"
         }"
         if ((index < addresses_length-1)); then
-            json_data["1"]+=","
+            json_data["$GEOLOCATION"]+=","
         fi
     done
     ((index++))
 done
 
 # Construct the final JSON content
-json_content='{
-  "testnet": {
-    "1": [
-'
+json_content="{
+  \"testnet\": {
+    \"$GEOLOCATION\": [
+"
 
 for index in "${!json_data[@]}"; do
     json_content+="  ${json_data["$index"]}"
@@ -71,21 +73,30 @@ echo "$json_content" > pairingList.json
 
 # Run the badge server
 signer=$(lavad keys show user1 -a)
-echo "signer address: $signer"
+echo "Signer address: $signer"
 PROJECT_ID="sampleProjectId"
-GEOLOCATION="1"
+BADGE_PORT=8080
+BADGE_URL=http://localhost:$BADGE_PORT
 
 cp examples/jsonRPC_badge.ts examples/jsonRPC_badge_test.ts
+cp examples/restAPI_badge.ts examples/restAPI_badge_test.ts
+cp examples/tendermintRPC_badge.ts examples/tendermintRPC_badge_test.ts
 
-sed -i 's/geolocation: "2",/geolocation: "1",\n\n    pairingListConfig: "pairingList.json",\n\n    lavaChainId: "lava",/g' examples/jsonRPC_badge_test.ts
+sed -i "s|geolocation:.*,|geolocation: \"$GEOLOCATION\",\n\n    pairingListConfig: \"pairingList.json\",\n\n    lavaChainId: \"lava\",\n\n    logLevel: \"debug\",\n\n    allowInsecureTransport: true|g" examples/jsonRPC_badge_test.ts
+sed -i "s|geolocation:.*,|geolocation: \"$GEOLOCATION\",\n\n    pairingListConfig: \"pairingList.json\",\n\n    lavaChainId: \"lava\",\n\n    logLevel: \"debug\",\n\n    allowInsecureTransport: true|g" examples/restAPI_badge_test.ts
+sed -i "s|geolocation:.*,|geolocation: \"$GEOLOCATION\",\n\n    pairingListConfig: \"pairingList.json\",\n\n    lavaChainId: \"lava\",\n\n    logLevel: \"debug\",\n\n    allowInsecureTransport: true|g" examples/tendermintRPC_badge_test.ts
 
-sed -i 's|badgeServerAddress:.*|badgeServerAddress: "http://localhost:8080",|g' examples/jsonRPC_badge_test.ts
+sed -i "s|badgeServerAddress:.*|badgeServerAddress: \"$BADGE_URL\",|g" examples/jsonRPC_badge_test.ts
+sed -i "s|badgeServerAddress:.*|badgeServerAddress: \"$BADGE_URL\",|g" examples/restAPI_badge_test.ts
+sed -i "s|badgeServerAddress:.*|badgeServerAddress: \"$BADGE_URL\",|g" examples/tendermintRPC_badge_test.ts
+
 sed -i "s|projectId:.*|projectId: \"$PROJECT_ID\",|g" examples/jsonRPC_badge_test.ts
+sed -i "s|projectId:.*|projectId: \"$PROJECT_ID\",|g" examples/restAPI_badge_test.ts
+sed -i "s|projectId:.*|projectId: \"$PROJECT_ID\",|g" examples/tendermintRPC_badge_test.ts
 
+BADGE_DEFAULT_GEOLOCATION="$GEOLOCATION" BADGE_USER_DATA="{\"$GEOLOCATION\":{\"$PROJECT_ID\":{\"project_public_key\":\"$signer\",\"private_key\":\"$privateKey\",\"epochs_max_cu\":2233333333}}}" lavad badgegenerator --grpc-url=127.0.0.1:9090 --log_level=debug --chain-id lava --port $BADGE_PORT
 
-BADGE_USER_DATA="{\"$GEOLOCATION\":{\"$PROJECT_ID\":{\"project_public_key\":\"$signer\",\"private_key\":\"$privateKey\",\"epochs_max_cu\":2233333333}}}" lavad badgegenerator --grpc-url=127.0.0.1:9090 --log_level=debug --chain-id lava
-
-badgeResponse=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"badge_address\": \"user1\", \"project_id\": \"$PROJECT_ID\"}" http://localhost:8080/lavanet.lava.pairing.BadgeGenerator/GenerateBadge)
+badgeResponse=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"badge_address\": \"user1\", \"project_id\": \"$PROJECT_ID\"}" $BADGE_URL/lavanet.lava.pairing.BadgeGenerator/GenerateBadge)
 
 if [[ -z "$badgeResponse" ]]; then
   echo "Failed to generate the badge. Please check if the badge server is running and accessible."
