bcf0e11e549450b084910a167e7de85560314371
slandymani <slandyman9@gmail.com>
2023-11-14 15:08:44 +0200

    make consts from emergency tracker tests private

--- protocol/statetracker/emergency_tracker_test.go
+++ protocol/statetracker/emergency_tracker_test.go
@@ -9,52 +9,52 @@ import (
 )
 
 const (
-	DowntimeDuration  = 10 * time.Millisecond
-	EpochDuration     = 2 * DowntimeDuration
-	FirstEpoch        = 10
-	SecondEpoch       = 20
-	ThirdEpoch        = 30
-	ZeroVirtualEpoch  = uint64(0)
-	FirstVirtualEpoch = uint64(1)
+	downtimeDuration  = 10 * time.Millisecond
+	epochDuration     = 2 * downtimeDuration
+	firstEpoch        = 10
+	secondEpoch       = 20
+	thirdEpoch        = 30
+	zeroVirtualEpoch  = uint64(0)
+	firstVirtualEpoch = uint64(1)
 )
 
 func TestEmergencyTrackerVirtualEpoch(t *testing.T) {
 	emergencyTracker, emergencyCallback := NewEmergencyTracker(nil)
 	emergencyTracker.SetDowntimeParams(downtimev1.Params{
-		DowntimeDuration: DowntimeDuration,
-		EpochDuration:    EpochDuration,
+		DowntimeDuration: downtimeDuration,
+		EpochDuration:    epochDuration,
 	})
 
-	emergencyTracker.UpdateEpoch(FirstEpoch)
+	emergencyTracker.UpdateEpoch(firstEpoch)
 
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(FirstEpoch))
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(firstEpoch))
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
 
-	time.Sleep(EpochDuration)
-	emergencyTracker.UpdateEpoch(SecondEpoch)
+	time.Sleep(epochDuration)
+	emergencyTracker.UpdateEpoch(secondEpoch)
 	latestBlockTime := time.Now()
 
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(FirstEpoch))
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(SecondEpoch))
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(firstEpoch))
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(secondEpoch))
 
 	// wait until downtime starts, virtual_epoch = 0
-	time.Sleep(DowntimeDuration)
+	time.Sleep(downtimeDuration)
 	emergencyCallback(latestBlockTime)
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
 
 	// wait until the end of the normal epoch, virtual_epoch = 1
-	time.Sleep(DowntimeDuration)
+	time.Sleep(downtimeDuration)
 	emergencyCallback(latestBlockTime)
-	require.Equal(t, FirstVirtualEpoch, emergencyTracker.GetVirtualEpoch(SecondEpoch))
-	require.Equal(t, FirstVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
+	require.Equal(t, firstVirtualEpoch, emergencyTracker.GetVirtualEpoch(secondEpoch))
+	require.Equal(t, firstVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
 
-	emergencyTracker.UpdateEpoch(ThirdEpoch)
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
-	require.Equal(t, FirstVirtualEpoch, emergencyTracker.GetVirtualEpoch(SecondEpoch))
+	emergencyTracker.UpdateEpoch(thirdEpoch)
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetLatestVirtualEpoch())
+	require.Equal(t, firstVirtualEpoch, emergencyTracker.GetVirtualEpoch(secondEpoch))
 
 	// test clear old virtual epoch
-	emergencyTracker.UpdateEpoch(ThirdEpoch + FirstEpoch)
-	require.Equal(t, FirstVirtualEpoch, emergencyTracker.GetVirtualEpoch(SecondEpoch))
-	emergencyTracker.UpdateEpoch(ThirdEpoch + SecondEpoch)
-	require.Equal(t, ZeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(SecondEpoch))
+	emergencyTracker.UpdateEpoch(thirdEpoch + firstEpoch)
+	require.Equal(t, firstVirtualEpoch, emergencyTracker.GetVirtualEpoch(secondEpoch))
+	emergencyTracker.UpdateEpoch(thirdEpoch + secondEpoch)
+	require.Equal(t, zeroVirtualEpoch, emergencyTracker.GetVirtualEpoch(secondEpoch))
 }
