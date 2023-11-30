320043b9959c6bbf43c49efb9c4f70402709b753
oren-lava <oren@lavanet.xyz>
2023-11-30 16:28:47 +0200

    CNS-737: added reward module params

--- x/rewards/keeper/params.go
+++ x/rewards/keeper/params.go
@@ -7,10 +7,39 @@ import (
 
 // GetParams get all parameters as types.Params
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams()
+	return types.NewParams(
+		k.MinBondedTarget(ctx),
+		k.MaxBondedTarget(ctx),
+		k.LowFactor(ctx),
+		k.LeftoverBurnRate(ctx),
+	)
 }
 
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
+}
+
+// MinBondedTarget returns the MinBondedTarget param
+func (k Keeper) MinBondedTarget(ctx sdk.Context) (res sdk.Dec) {
+	k.paramstore.Get(ctx, types.KeyMinBondedTarget, &res)
+	return
+}
+
+// MaxBondedTarget returns the MaxBondedTarget param
+func (k Keeper) MaxBondedTarget(ctx sdk.Context) (res sdk.Dec) {
+	k.paramstore.Get(ctx, types.KeyMaxBondedTarget, &res)
+	return
+}
+
+// LowFactor returns the LowFactor param
+func (k Keeper) LowFactor(ctx sdk.Context) (res sdk.Dec) {
+	k.paramstore.Get(ctx, types.KeyLowFactor, &res)
+	return
+}
+
+// LeftoverBurnRate returns the LeftoverBurnRate param
+func (k Keeper) LeftoverBurnRate(ctx sdk.Context) (res sdk.Dec) {
+	k.paramstore.Get(ctx, types.KeyLeftoverBurnRate, &res)
+	return
 }
--- x/rewards/types/params.go
+++ x/rewards/types/params.go
@@ -1,11 +1,34 @@
 package types
 
 import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
+
+var (
+	KeyMinBondedTarget             = []byte("MinBondedTarget")
+	DefaultMinBondedTarget sdk.Dec = sdk.NewDecWithPrec(6, 1) // 0.6
+)
+
+var (
+	KeyMaxBondedTarget             = []byte("MaxBondedTarget")
+	DefaultMaxBondedTarget sdk.Dec = sdk.NewDecWithPrec(8, 1) // 0.8
+)
+
+var (
+	KeyLowFactor             = []byte("LowFactor")
+	DefaultLowFactor sdk.Dec = sdk.NewDecWithPrec(5, 1) // 0.5
+)
+
+var (
+	KeyLeftoverBurnRate             = []byte("LeftoverBurnRate")
+	DefaultLeftOverBurnRate sdk.Dec = sdk.OneDec()
+)
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -13,22 +36,62 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(
+	minBondedTarget sdk.Dec,
+	maxBondedTarget sdk.Dec,
+	lowFactor sdk.Dec,
+	leftoverBurnRate sdk.Dec,
+) Params {
+	return Params{
+		MinBondedTarget:  minBondedTarget,
+		MaxBondedTarget:  maxBondedTarget,
+		LowFactor:        lowFactor,
+		LeftoverBurnRate: leftoverBurnRate,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(
+		DefaultMinBondedTarget,
+		DefaultMaxBondedTarget,
+		DefaultLowFactor,
+		DefaultLeftOverBurnRate,
+	)
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyMinBondedTarget, &p.MinBondedTarget, validateDec),
+		paramtypes.NewParamSetPair(KeyMaxBondedTarget, &p.MaxBondedTarget, validateDec),
+		paramtypes.NewParamSetPair(KeyLowFactor, &p.LowFactor, validateDec),
+		paramtypes.NewParamSetPair(KeyLeftoverBurnRate, &p.LeftoverBurnRate, validateDec),
+	}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+	if err := validateDec(p.MinBondedTarget); err != nil {
+		return err
+	}
+
+	if err := validateDec(p.MaxBondedTarget); err != nil {
+		return err
+	}
+
+	if p.MinBondedTarget.GT(p.MaxBondedTarget) {
+		return fmt.Errorf("min_bonded_target cannot be greater than max_bonded_target")
+	}
+
+	if err := validateDec(p.LowFactor); err != nil {
+		return err
+	}
+
+	if err := validateDec(p.LeftoverBurnRate); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -36,4 +99,18 @@ func (p Params) Validate() error {
 func (p Params) String() string {
 	out, _ := yaml.Marshal(p)
 	return string(out)
+}
+
+// validateDec validates the Dec param is between 0 and 1
+func validateDec(v interface{}) error {
+	param, ok := v.(sdk.Dec)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	if param.GT(sdk.OneDec()) || param.LT(sdk.ZeroDec()) {
+		return fmt.Errorf("invalid parameter minBondedTarget")
+	}
+
+	return nil
 }
