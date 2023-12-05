64c17fad28a610f1a102cdfd1b9ec1f821a81753
Omer <100387053+omerlavanet@users.noreply.github.com>
2023-12-05 12:42:24 +0200

    CNS-improve the verification for subsquid (#1021)
    * improve the verification
    
    * bump target version

--- protocol/chainlib/rest.go
+++ protocol/chainlib/rest.go
@@ -44,8 +44,15 @@ func (bcp *RestChainParser) GetUniqueName() string {
 
 func (apip *RestChainParser) CraftMessage(parsing *spectypes.ParseDirective, connectionType string, craftData *CraftData, metadata []pairingtypes.Metadata) (ChainMessageForSend, error) {
 	if craftData != nil {
+		var data []byte = nil
+		urlPath := string(craftData.Data)
+		if craftData.ConnectionType == http.MethodPost {
+			// on post we need to send the data provided in the templace with the api as method
+			data = craftData.Data
+			urlPath = craftData.Path
+		}
 		// chain fetcher sends the replaced request inside data
-		chainMessage, err := apip.ParseMsg(string(craftData.Data), nil, craftData.ConnectionType, metadata, 0)
+		chainMessage, err := apip.ParseMsg(urlPath, data, craftData.ConnectionType, metadata, 0)
 		if err == nil {
 			chainMessage.AppendHeader(metadata)
 		}
