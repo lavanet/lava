ef2a38f71a13af803dbba46d7e457eb8861f9cfc
oren-lava <oren@lavanet.xyz>
2023-12-05 17:17:20 +0200

    CNS-737: added readme

--- app/app.go
+++ app/app.go
@@ -663,7 +663,7 @@ func New(
 		capabilitytypes.ModuleName,
 		authtypes.ModuleName,
 		banktypes.ModuleName,
-		rewardsmoduletypes.ModuleName,
+		rewardsmoduletypes.ModuleName, // rewards needs to run before distribution to fill the validator rewards pool
 		distrtypes.ModuleName,
 		stakingtypes.ModuleName,
 		slashingtypes.ModuleName,
