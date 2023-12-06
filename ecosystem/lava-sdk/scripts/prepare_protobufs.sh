74bd7427ae0747930b793772cd70089eafc7c288
unknown unknown <unknown@unknown>
2023-12-06 17:20:00 +0100

    try fix sdk codegen

--- ecosystem/lava-sdk/scripts/prepare_protobufs.sh
+++ ecosystem/lava-sdk/scripts/prepare_protobufs.sh
@@ -48,7 +48,7 @@ function prepare() {
         exit 1
     fi
 
-    specific_dir="$GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v0.47.3"
+    specific_dir="$GOPATH/pkg/mod/github.com/lavanet/cosmos-sdk@v0.47.7-0.20231206103345-2d840a1d27f9"
 
     if [[ ! -d "$specific_dir" ]]; then
         echo "Error: The cosmos-sdk directory ('$specific_dir') does not exist under '$GOPATH/pkg/mod'." >&2
