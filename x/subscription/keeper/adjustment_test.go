3cff3fedb10dfbc15281ed60bf59f433baf9285e
omerlavanet <omer@lavanet.xyz>
2023-12-20 12:03:41 +0200

    added unitest for edge cases

--- x/subscription/keeper/adjustment_test.go
+++ x/subscription/keeper/adjustment_test.go
@@ -135,3 +135,104 @@ func TestAdjustment(t *testing.T) {
 	// 120/300, 141/201 = 261/501
 	require.True(t, providersFactors2[provider2].Equal(math.LegacyMustNewDecFromStr("261").QuoInt64(501)), providersFactors2[provider2].String())
 }
+
+func TestAdjustmentEdgeValues(t *testing.T) {
+	playbook := []struct {
+		name                  string
+		firstProviderCU       uint64
+		expectedAdjustmentDec string
+	}{
+		{
+			name:                  "all-equal",
+			firstProviderCU:       1000,
+			expectedAdjustmentDec: "1",
+		},
+		{
+			name:                  "very little",
+			firstProviderCU:       1,
+			expectedAdjustmentDec: "1",
+		},
+		{
+			name:                  "top",
+			firstProviderCU:       1000000000000000000,
+			expectedAdjustmentDec: "0.2",
+		},
+	}
+	for _, play := range playbook {
+		t.Run(play.name, func(t *testing.T) {
+			ts := common.NewTester(t)
+			err := ts.SetupForTests("../../../", "LAV1", 1, 1, 1, 5)
+			require.NoError(t, err)
+			_, consumer1Addr := ts.GetAccount(common.CONSUMER, 0)
+			projects := ts.Keepers.Projects.GetAllProjectsForSubscription(ts.Ctx, consumer1Addr)
+			require.Len(t, projects, 2)
+			providers := ts.Accounts(common.PROVIDER)
+
+			usage := []struct {
+				providerUsage []uint64
+			}{
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+				{providerUsage: []uint64{
+					play.firstProviderCU, 1000, 1000, 1000, 1000,
+				}},
+			}
+
+			totalUsage := func(cus []uint64) uint64 {
+				res := uint64(0)
+				for _, cu := range cus {
+					res += cu
+				}
+				return res
+			}
+
+			for _, usageProviders := range usage {
+				totalUsageThisIndex := totalUsage(usageProviders.providerUsage)
+				for providerIdx, cu := range usageProviders.providerUsage {
+					ts.Keepers.Subscription.AppendAdjustment(ts.Ctx, consumer1Addr, providers[providerIdx].Addr.String(), totalUsageThisIndex, cu)
+				}
+			}
+
+			consumerAdjustments := ts.Keepers.Subscription.GetConsumerAdjustments(ts.Ctx, consumer1Addr)
+			require.NotEmpty(t, consumerAdjustments)
+			seenProviders := map[string]struct{}{}
+			for _, adjustment := range consumerAdjustments {
+				provider, err := ts.Keepers.Subscription.GetProviderFromAdjustment(&adjustment)
+				require.NoError(t, err)
+				seenProviders[provider] = struct{}{}
+			}
+			require.Len(t, seenProviders, len(providers))
+
+			providersFactors := ts.Keepers.Subscription.GetAdjustmentFactorProvider(ts.Ctx, consumerAdjustments)
+			require.Len(t, providersFactors, len(providers))
+			prevFactor := providersFactors[providers[1].Addr.String()]
+			for idx, factor := range providersFactors {
+				if idx == providers[0].Addr.String() {
+					require.True(t, factor.Equal(math.LegacyMustNewDecFromStr(play.expectedAdjustmentDec)), factor.String())
+				} else {
+					require.Equal(t, prevFactor, factor)
+				}
+			}
+		})
+	}
+
+}
