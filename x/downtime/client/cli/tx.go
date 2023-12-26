e78d68c3b61de5bddf92c2faa84ff182a0130514
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2023-12-26 09:10:03 +0000

    fix

--- x/downtime/client/cli/tx.go
+++ x/downtime/client/cli/tx.go
@@ -9,7 +9,10 @@ import (
 
 func NewTxCmd() *cobra.Command {
 	return &cobra.Command{
-		Use: types.ModuleName + "tx commands",
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			return fmt.Errorf("the downtime module does not have any tx commands")
 		},
