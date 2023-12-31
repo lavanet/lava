64dedf1e8a046c44a89d754ee36e242733081c31
unknown unknown <unknown@unknown.com>
2023-12-31 19:35:55 +0100

    add blacklisted message inclussion

--- x/spec/ante/ante.go
+++ x/spec/ante/ante.go
@@ -1,8 +1,10 @@
 package ante
 
 import (
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/types"
 	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	"github.com/cosmos/gogoproto/proto"
 	"github.com/lavanet/lava/x/spec/keeper"
 )
 
@@ -22,13 +24,36 @@ func (e ExpeditedProposalFilterAnteDecorator) AnteHandle(
 ) (newCtx types.Context, err error) {
 	msgs := tx.GetMsgs()
 
+	blacklist := e.k.BlacklistedExpeditedMsgs(ctx)
+
 	for _, msg := range msgs {
 		switch m := msg.(type) {
 		case *v1.MsgSubmitProposal:
 			if m.GetExpedited() {
+				expeditedMsgs, err := m.GetMsgs()
+				if err != nil {
+					return ctx, err
+				}
+
+				for _, expeditedMsg := range expeditedMsgs {
+					if blacklistContainsMsg(blacklist, expeditedMsg) {
+						return ctx, fmt.Errorf("expedited proposal contains blacklisted message %s", proto.MessageName(expeditedMsg))
+					}
+				}
 			}
 		}
 	}
 
 	return next(ctx, tx, simulate)
+}
+
+func blacklistContainsMsg(blacklist []string, msg types.Msg) bool {
+	msgName := proto.MessageName(msg)
+	for _, blacklistedMsg := range blacklist {
+		if blacklistedMsg == msgName {
+			return true
+		}
+	}
+
+	return false
 }
--- x/spec/ante/ante_test.go
+++ x/spec/ante/ante_test.go
@@ -1,24 +1,64 @@
 package ante
 
 import (
-	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	"github.com/cosmos/gogoproto/proto"
+	"github.com/lavanet/lava/app"
 	testkeeper "github.com/lavanet/lava/testutil/keeper"
 	types2 "github.com/lavanet/lava/x/spec/types"
+	types3 "github.com/lavanet/lava/x/subscription/types"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestNewExpeditedProposalFilterAnteDecorator(t *testing.T) {
 	tests := []struct {
 		name       string
-		txMsgs     []types.Msg
+		theMsg     func() types.Msg
 		shouldFail bool
 	}{
 		{
-			name:       "should fail if any of the messages are in the blacklist",
-			txMsgs:     []types.Msg{},
+			name: "should fail if any of the messages are in the blacklist, expedited",
+			theMsg: func() types.Msg {
+				proposal, err := v1.NewMsgSubmitProposal(
+					[]types.Msg{
+						&banktypes.MsgSend{},
+					},
+					types.NewCoins(types.NewCoin("lava", types.NewInt(100))),
+					"cosmos1qypqxpq9qcrsszgjx3ysxf7j8xq9q9qyq9q9q9",
+					"metadata",
+					"title",
+					"summary",
+					true,
+				)
+				require.NoError(t, err)
+
+				return proposal
+			},
 			shouldFail: true,
+		},
+		{
+			name: "should not fail if none of the messages are in the blacklist, expedited",
+			theMsg: func() types.Msg {
+				proposal, err := v1.NewMsgSubmitProposal(
+					[]types.Msg{
+						&types3.MsgAutoRenewal{},
+					},
+					types.NewCoins(types.NewCoin("lava", types.NewInt(100))),
+					"cosmos1qypqxpq9qcrsszgjx3ysxf7j8xq9q9qyq9q9q9",
+					"metadata",
+					"title",
+					"summary",
+					true,
+				)
+				require.NoError(t, err)
+
+				return proposal
+			},
+			shouldFail: false,
 		},
 	}
 
@@ -29,12 +69,31 @@ func TestNewExpeditedProposalFilterAnteDecorator(t *testing.T) {
 			k, ctx := testkeeper.SpecKeeper(t)
 			params := types2.DefaultParams()
 			params.BlacklistedExpeditedMsgs = []string{
-				proto.MessageName(&bankv1beta1.MsgSend{}),
+				proto.MessageName(&banktypes.MsgSend{}),
 			} // we whitelist MsgSend proposal
 
 			k.SetParams(ctx, params)
 
+			encodingConfig := app.MakeEncodingConfig()
+			clientCtx := client.Context{}.
+				WithTxConfig(encodingConfig.TxConfig)
+
+			txBuilder := clientCtx.TxConfig.NewTxBuilder()
+			err := txBuilder.SetMsgs(tt.theMsg())
+			require.NoError(t, err)
+
+			tx := txBuilder.GetTx()
 			anteHandler := NewExpeditedProposalFilterAnteDecorator(k)
+
+			_, err = anteHandler.AnteHandle(ctx, tx, false, func(ctx types.Context, tx types.Tx, simulate bool) (newCtx types.Context, err error) {
+				return ctx, nil
+			})
+			if tt.shouldFail {
+				require.Error(t, err)
+				require.ErrorContainsf(t, err, "proposal contains blacklisted message", "expected error to contain %s", "proposal contains blacklisted messages")
+			} else {
+				require.NoError(t, err)
+			}
 		})
 	}
 }
