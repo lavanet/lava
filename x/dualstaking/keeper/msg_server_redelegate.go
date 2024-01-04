419ccb46259cf9dd9fc7611fa8cc9767db978ae8
Elad Gildnur <elad.gildnur@gmail.com>
2024-01-04 05:56:17 -0500

    Add denom check to msg_server_redelegate

--- x/dualstaking/keeper/msg_server_redelegate.go
+++ x/dualstaking/keeper/msg_server_redelegate.go
@@ -3,13 +3,22 @@ package keeper
 import (
 	"context"
 
+	sdkerror "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/lavanet/lava/utils"
 	"github.com/lavanet/lava/x/dualstaking/types"
 )
 
 func (k msgServer) Redelegate(goCtx context.Context, msg *types.MsgRedelegate) (*types.MsgRedelegateResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	bondDenom := k.stakingKeeper.BondDenom(ctx)
+	if msg.Amount.Denom != bondDenom {
+		return &types.MsgRedelegateResponse{}, sdkerror.Wrapf(
+			sdkerrors.ErrInvalidRequest, "invalid coin denomination: got %s, expected %s", msg.Amount.Denom, bondDenom,
+		)
+	}
 
 	err := k.Keeper.Redelegate(
 		ctx,
