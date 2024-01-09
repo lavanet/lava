c430cfa38e7d3751728892183923649626cd8b51
Elad Gildnur <elad.gildnur@gmail.com>
2024-01-09 15:05:37 -0500

    Add a test for RelaysMonitor

---
+++ protocol/metrics/relays_monitor_test.go
@@ -0,0 +1,61 @@
+package metrics
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHappyFlow(t *testing.T) {
+	isHealthy := true
+	relaySender := func() (bool, error) {
+		return isHealthy, nil
+	}
+
+	interval := time.Second * 3
+	extraTimeToWait := time.Second
+	timeToSleep := interval + extraTimeToWait
+
+	relaysMonitor := NewRelaysMonitor(interval, "test_chain", "rest", relaySender)
+	relaysMonitor.Start(context.Background())
+
+	t.Run("HappyFlow", func(t *testing.T) {
+		// Log a relay
+		relaysMonitor.LogRelay()
+
+		// Check if the relays monitor is healthy
+		require.True(t, relaysMonitor.IsHealthy())
+
+		// Sleep for the interval
+		time.Sleep(timeToSleep)
+
+		// Check if the relays monitor is still healthy
+		require.True(t, relaysMonitor.IsHealthy())
+
+		// Set to false
+		isHealthy = false
+
+		// Sleep for the interval
+		time.Sleep(timeToSleep)
+
+		// Check if the relays monitor is still healthy
+		require.False(t, relaysMonitor.IsHealthy())
+
+		// Sleep for the interval
+		time.Sleep(timeToSleep)
+
+		// Log a relay
+		relaysMonitor.LogRelay()
+
+		// Now should be healthy again
+		require.True(t, relaysMonitor.IsHealthy())
+
+		// Sleep for the interval
+		time.Sleep(timeToSleep)
+
+		// Now should be not healthy
+		require.False(t, relaysMonitor.IsHealthy())
+	})
+}
