74329ebc938eb822da692c688679c16a614c0de2
oren-lava <oren@lavanet.xyz>
2024-01-10 14:19:19 +0200

    CNS-795: show addon in show-chain-info

--- x/spec/keeper/grpc_query_show_chain_info.go
+++ x/spec/keeper/grpc_query_show_chain_info.go
@@ -2,8 +2,8 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
-	sdkerrors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	epochstoragetypes "github.com/lavanet/lava/x/epochstorage/types"
 	"github.com/lavanet/lava/x/spec/types"
@@ -20,57 +20,50 @@ func (k Keeper) ShowChainInfo(goCtx context.Context, req *types.QueryShowChainIn
 
 	var apiInterfacesStructList []*types.ApiList
 	var optionalInterfaceList []string
-	var chainID string
 
-	allSpec := k.GetAllSpec(ctx)
-	for _, spec := range allSpec {
-		// get info by chain name
-		if spec.GetName() == req.GetChainName() || spec.GetIndex() == req.GetChainName() {
-			// get chain ID
-			chainID = spec.GetIndex()
-
-			fullspec, err := k.ExpandSpec(ctx, spec)
-			if err != nil {
-				return nil, err
-			}
-			// get the spec's expected interfaces
-			expectedInterfaces := k.GetExpectedServicesForExpandedSpec(fullspec, true)
-
-			mandatoryInterfaceList := getInterfacesNamesFromMap(expectedInterfaces)
-
-			// get API methods (includes their interfaces)
-			apisCollections := fullspec.GetApiCollections()
-			for _, apiCollection := range apisCollections {
-				if !apiCollection.Enabled {
-					continue
-				}
-				apiInterface := apiCollection.CollectionData.ApiInterface
-
-				apiMethods := []string{}
-				// iterate over APIs
-				if _, ok := expectedInterfaces[epochstoragetypes.EndpointService{
-					ApiInterface: apiInterface,
-					Addon:        "",
-					Extension:    "",
-				}]; !ok {
-					optionalInterfaceList = append(optionalInterfaceList, apiInterface)
-				}
-				for _, api := range apiCollection.Apis {
-					if !api.Enabled {
-						continue
-					}
-					apiMethods = append(apiMethods, api.GetName())
-				}
-				apiInterfacesStructList = append(apiInterfacesStructList, &types.ApiList{
-					Interface:     apiInterface,
-					SupportedApis: apiMethods,
-				})
-			}
-
-			// found the chain, there is no need to further iterate
-			return &types.QueryShowChainInfoResponse{ChainID: chainID, Interfaces: mandatoryInterfaceList, SupportedApisInterfaceList: apiInterfacesStructList, OptionalInterfaces: optionalInterfaceList}, nil
-		}
+	spec, found := k.GetSpec(ctx, req.ChainName)
+	if !found {
+		return nil, fmt.Errorf("spec not found")
 	}
-	// Didn't find the chain
-	return nil, sdkerrors.Wrapf(types.ErrChainNameNotFound, "%s", req.GetChainName())
+
+	fullspec, err := k.ExpandSpec(ctx, spec)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the spec's expected interfaces (mandatory)
+	expectedInterfaces := k.GetExpectedServicesForExpandedSpec(fullspec, true)
+	mandatoryInterfaceList := getInterfacesNamesFromMap(expectedInterfaces)
+
+	// get API methods (includes their interfaces)
+	apisCollections := fullspec.GetApiCollections()
+	for _, apiCollection := range apisCollections {
+		if !apiCollection.Enabled {
+			continue
+		}
+		apiInterface := apiCollection.CollectionData.ApiInterface
+
+		apiMethods := []string{}
+		// iterate over APIs
+		if _, ok := expectedInterfaces[epochstoragetypes.EndpointService{
+			ApiInterface: apiInterface,
+			Addon:        "",
+			Extension:    "",
+		}]; !ok {
+			optionalInterfaceList = append(optionalInterfaceList, apiInterface)
+		}
+		for _, api := range apiCollection.Apis {
+			if !api.Enabled {
+				continue
+			}
+			apiMethods = append(apiMethods, api.GetName())
+		}
+		apiInterfacesStructList = append(apiInterfacesStructList, &types.ApiList{
+			Interface:     apiInterface,
+			SupportedApis: apiMethods,
+			Addon:         apiCollection.CollectionData.AddOn,
+		})
+	}
+
+	return &types.QueryShowChainInfoResponse{ChainID: spec.Index, Interfaces: mandatoryInterfaceList, SupportedApisInterfaceList: apiInterfacesStructList, OptionalInterfaces: optionalInterfaceList}, nil
 }
