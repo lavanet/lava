060b526a11606a72387d5a0230f53d3a38e99f4b
Elad Gildnur <6321801+shleikes@users.noreply.github.com>
2024-01-18 05:47:49 -0500

    PRT-1065: RPCConsumer relays health check (#1136)
    * Add the relays health checks
    
    * Fix lint
    
    * Fix error
    
    * Return success boolean on init relays
    
    * Add interval argument
    
    * Remove unused arg
    
    * Add a test for RelaysMonitor
    
    * Rename lint test workflow
    
    * RelaysMonitor isHealthy is now thread safe
    
    * Send initial relays on RPConsumerServer using the RelaysMonitor
    
    * Add the RelaysMonitorAggregator
    
    * CR Fix: Merge duplicated code
    
    * CR Fix: Check name
    
    * CR Fix: Comment
    
    * CR Fix: Conventions
    
    * CR Fix: Send initial relays when RelayMonitor is off
    
    * CR Fix: Remove checks for flags, and instead check for nil inside func
    
    * CR Fix: Command line args improvments
    
    * Add debug log when sending crafted relays
    
    * CR Fix: Narrow return args

--- protocol/chainlib/chainlib.go
+++ protocol/chainlib/chainlib.go
@@ -33,16 +33,17 @@ func NewChainListener(
 	ctx context.Context,
 	listenEndpoint *lavasession.RPCEndpoint,
 	relaySender RelaySender,
+	healthReporter HealthReporter,
 	rpcConsumerLogs *metrics.RPCConsumerLogs,
 	chainParser ChainParser,
 ) (ChainListener, error) {
 	switch listenEndpoint.ApiInterface {
 	case spectypes.APIInterfaceJsonRPC:
-		return NewJrpcChainListener(ctx, listenEndpoint, relaySender, rpcConsumerLogs), nil
+		return NewJrpcChainListener(ctx, listenEndpoint, relaySender, healthReporter, rpcConsumerLogs), nil
 	case spectypes.APIInterfaceTendermintRPC:
-		return NewTendermintRpcChainListener(ctx, listenEndpoint, relaySender, rpcConsumerLogs), nil
+		return NewTendermintRpcChainListener(ctx, listenEndpoint, relaySender, healthReporter, rpcConsumerLogs), nil
 	case spectypes.APIInterfaceRest:
-		return NewRestChainListener(ctx, listenEndpoint, relaySender, rpcConsumerLogs), nil
+		return NewRestChainListener(ctx, listenEndpoint, relaySender, healthReporter, rpcConsumerLogs), nil
 	case spectypes.APIInterfaceGrpc:
 		return NewGrpcChainListener(ctx, listenEndpoint, relaySender, rpcConsumerLogs, chainParser), nil
 	}
@@ -82,6 +83,10 @@ type ChainMessageForSend interface {
 	GetApi() *spectypes.Api
 	GetRPCMessage() rpcInterfaceMessages.GenericMessage
 	GetApiCollection() *spectypes.ApiCollection
+}
+
+type HealthReporter interface {
+	IsHealthy() bool
 }
 
 type RelaySender interface {
