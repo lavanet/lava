b23ff163f4552096c6fa81a84352a482347f37f1
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2024-01-21 09:28:57 +0000

    rewrite

--- x/plans/module.go
+++ x/plans/module.go
@@ -179,7 +179,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 9 }
+func (AppModule) ConsensusVersion() uint64 { return 8 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
