e70a8c2537d34b819630d195f16c1b20446b21ee
Ran Mishael <ran@lavanet.xyz>
2024-01-21 19:30:12 +0100

    adding ip addresses filtering

--- utils/ips/protected_ips.go
+++ utils/ips/protected_ips.go
@@ -5,6 +5,8 @@ import (
 	"regexp"
 	"strings"
 
+	"golang.org/x/exp/slices"
+
 	"github.com/lavanet/lava/utils"
 )
 
@@ -17,65 +19,71 @@ func isValidDomainName(domain string) bool {
 	return domainRegex.MatchString(domain)
 }
 
-// getResolvedIPs returns the IP addresses that the given domain is pointing to.
-func getResolvedIPs(domain string) ([]string, error) {
-	ips, err := net.LookupHost(domain)
-	if err != nil {
-		return nil, err
+func validateIp(host string) bool {
+	ip := net.ParseIP(host)
+	if ip != nil {
+		// If the host is an IP address, check if it is not a loopback or reserved address
+		return !ip.IsLoopback() && !ip.IsGlobalUnicast() && !ip.IsPrivate() && !ip.IsLinkLocalUnicast()
 	}
-	return ips, nil
+	return false
+}
+
+func isValidIPv4(address string) bool {
+	ipv4Regex := regexp.MustCompile(`^(\d{1,3}\.){3}\d{1,3}$`)
+	return ipv4Regex.MatchString(address)
 }
 
 // isValidAddress checks if the given address is a valid and not a reserved or local address.
 func IsValidAddress(address string) bool {
 	// Split the address into host and port
+	if strings.HasPrefix(address, "http://") {
+		address = strings.TrimPrefix(address, "http://")
+	} else if strings.HasPrefix(address, "https://") {
+		address = strings.TrimPrefix(address, "https://")
+	}
+
 	host, _, err := net.SplitHostPort(address)
-	if err == nil {
-		// Check if the host is an IP address
-		ip := net.ParseIP(host)
-		if ip != nil {
-			// If the host is an IP address, check if it is not a loopback or reserved address
-			return !ip.IsLoopback() && !ip.IsGlobalUnicast() && !ip.IsPrivate() && !ip.IsLinkLocalUnicast()
+	if err == nil && host != "" {
+		if host == "::" || host == "localhost" || host == "0.0.0.0" {
+			return false
+		}
+
+		if isValidIPv4(host) {
+			return validateIp(host)
+		}
+
+		// probably a domain name
+		if !isValidDomainName(host) {
+			utils.LavaFormatError("Failed parsing domain name, and ip address", nil)
+			return false
 		}
 	}
 
-	if !isValidDomainName(host) {
-		utils.LavaFormatError("Failed parsing domain name, and ip address", nil)
-		return false
-	}
-
-	resolvedIps, err := getResolvedIPs(address)
-	if err != nil {
-		utils.LavaFormatError("Failed resolving IP address for domain", err, utils.LogAttr("address", address))
-	}
-	for _, resolvedIp := range resolvedIps {
-		ip := net.ParseIP(resolvedIp)
-		// validate all ips this domain is pointing to
-		if ip != nil {
-			// If the host is an IP address, check if it is not a loopback or reserved address
-			valid := !ip.IsLoopback() && !ip.IsGlobalUnicast() && !ip.IsPrivate() && !ip.IsLinkLocalUnicast()
-			if !valid {
-				return false
-			}
-		}
-	}
-	return true
+	return false
 }
 
 func ValidateProviderAddressConsensus(address string) (valid bool) {
+	invalidOrProtectedIps := []string{
+		"0.0.0.0", "::", ":", "localhost",
+	}
+	if strings.HasPrefix(address, "http://") {
+		address = strings.TrimPrefix(address, "http://")
+	} else if strings.HasPrefix(address, "https://") {
+		address = strings.TrimPrefix(address, "https://")
+	}
+	if slices.Contains(invalidOrProtectedIps, address) {
+		return false
+	}
 	host, _, err := net.SplitHostPort(address)
 	if err != nil {
-		// domain name and not ip. we cant validate so we return true.
-		return true
+		return false
 	}
-	if host == "0.0.0.0" {
+	if slices.Contains(invalidOrProtectedIps, host) {
 		return false
 	}
 	if strings.HasPrefix(host, "127.0.0.") {
 		return false
 	}
-	if host == "localhost" {
-		return false
-	}
+
 	return true
 }
---
+++ utils/ips/protected_ips_test.go
@@ -0,0 +1,38 @@
+package ips
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidAddressesProtocol(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		value string
+		valid bool
+	}{
+		{valid: false, value: ":"},
+		{valid: false, value: "http://::"},
+		{valid: false, value: "::"},
+		{valid: false, value: "http://localhost:1211"},
+		{valid: false, value: "http://127.0.0.1:1211"},
+		{valid: false, value: "127.0.0.1:3123"},
+		{valid: false, value: "127.0.0.126:3123"},
+		{valid: false, value: "http://127.0.0.126:3123"},
+		{valid: false, value: "localhost:1211"},
+		{valid: false, value: "0.0.0.0:1211"},
+		{valid: false, value: "http://0.0.0.0:1211"},
+		{valid: false, value: "http://0.0.0.0:1211"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.value, func(t *testing.T) {
+			value := IsValidAddress(tt.value)
+			require.Equal(t, tt.valid, value)
+
+			value = ValidateProviderAddressConsensus(tt.value)
+			require.Equal(t, tt.valid, value)
+		})
+	}
+}
