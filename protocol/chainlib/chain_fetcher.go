ec711df93c29b63c34a4dd64fda56d84eb336c3e
oren-lava <oren@lavanet.xyz>
2024-01-28 11:05:13 +0200

    CNS-823: severity is per extension and not per verification

--- protocol/chainlib/chain_fetcher.go
+++ protocol/chainlib/chain_fetcher.go
@@ -79,7 +79,7 @@ func (cf *ChainFetcher) Validate(ctx context.Context) error {
 			}
 			if err != nil {
 				err := utils.LavaFormatError("invalid Verification on provider startup", err, utils.Attribute{Key: "Addons", Value: addons}, utils.Attribute{Key: "verification", Value: verification.Name})
-				if verification.Severity == spectypes.Verification_Fail {
+				if verification.Severity == spectypes.ParseValue_Fail {
 					return err
 				}
 			}
