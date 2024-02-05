50d4e41afeb7ed36f7fdae566eb72f8815882132
Elad Gildnur <6321801+shleikes@users.noreply.github.com>
2024-02-05 16:52:14 +0200

    Add deprecation warning for the commands in lavad (#1172)

---
+++ cmd/common/warning_log_command_wrapper.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	"github.com/lavanet/lava/utils"
+	"github.com/spf13/cobra"
+)
+
+func CreateWarningLogCommandWrapper(innerCommand *cobra.Command, warningMessage string) *cobra.Command {
+	return &cobra.Command{
+		Use:   innerCommand.Use,
+		Short: innerCommand.Short,
+		Run: func(cmd *cobra.Command, args []string) {
+			utils.LavaFormatWarning(warningMessage, nil)
+			_ = innerCommand.Execute()
+		},
+	}
+}
