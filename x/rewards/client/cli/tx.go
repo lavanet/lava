6e2a2ec3f5f4c98eb2c4462c312ecb07f3f2d813
oren-lava <oren@lavanet.xyz>
2024-02-08 19:00:31 +0200

    CNS-868: tx proposal cli to automatically make proposal

--- x/rewards/client/cli/tx.go
+++ x/rewards/client/cli/tx.go
@@ -1,21 +1,36 @@
 package cli
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	"github.com/lavanet/lava/utils/slices"
 	"github.com/lavanet/lava/x/rewards/types"
 )
 
 var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
 
 const (
-	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-	listSeparator              = ","
+	flagPacketTimeoutTimestamp       = "packet-timeout-timestamp"
+	listSeparator                    = ","
+	expeditedFlagName                = "expedited"
+	minIprpcCostFlagName             = "min-cost"
+	addIprpcSubscriptionsFlagName    = "add-subscriptions"
+	removeIprpcSubscriptionsFlagName = "remove-subscriptions"
 )
 
 // GetTxCmd returns the transaction commands for this module
@@ -30,5 +45,108 @@ func GetTxCmd() *cobra.Command {
 
 	// this line is used by starport scaffolding # 1
 
+	return cmd
+}
+
+// SetIprpcDataProposalHandler is the param change proposal handler.
+var SetIprpcDataProposalHandler = govclient.NewProposalHandler(NewSubmitSetIprpcDataProposalTxCmd)
+
+// NewSubmitSetIprpcDataProposalTxCmd returns a CLI command handler for creating
+// a set-iprpc-data proposal governance transaction.
+func NewSubmitSetIprpcDataProposalTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-iprpc-data <deposit> --min-cost 0ulava --add-subscriptions addr1,addr2 --remove-subscriptions addr3,addr4",
+		Args:  cobra.ExactArgs(1),
+		Short: "Submit a set IPRPC data proposal",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a set IPRPC data proposal along with an initial deposit.
+The proposal details (IPRPC data to set) are supplied via optional flags. To set the minimum IPRPC cost, use the --min-cost flag.
+To add IPRPC eligible subscriptions use the --add-subscriptions flag. To remove IPRPC eligible subscriptions use the --remove-subscriptions flag.
+Optionally, you can make the proposal expedited using the --expedited flag.
+Example:
+$ %s tx gov submit-legacy-proposal set-iprpc-data --min-cost 0ulava --add-subscriptions addr1,addr2 --remove-subscriptions addr3,addr4
+`,
+				version.AppName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			from := clientCtx.GetFromAddress()
+
+			isExpedited, err := cmd.Flags().GetBool(expeditedFlagName)
+			if err != nil {
+				return err
+			}
+
+			// get min cost
+			costStr, err := cmd.Flags().GetString(minIprpcCostFlagName)
+			if err != nil {
+				return err
+			}
+			cost, err := sdk.ParseCoinNormalized(costStr)
+			if err != nil {
+				return err
+			}
+
+			// get current iprpc subscriptions
+			q := types.NewQueryClient(clientCtx)
+			res, err := q.ShowIprpcData(context.Background(), &types.QueryShowIprpcDataRequest{})
+			if err != nil {
+				return err
+			}
+			subs := res.IprpcSubscriptions
+
+			// add from msg
+			subsToAdd, err := cmd.Flags().GetStringSlice(addIprpcSubscriptionsFlagName)
+			if err != nil {
+				return err
+			}
+			subs = append(subs, subsToAdd...)
+
+			// remove duplicates
+			iprpcSubs := []string{}
+			unique := map[string]bool{}
+			for _, sub := range subs {
+				if !unique[sub] {
+					unique[sub] = true
+					iprpcSubs = append(iprpcSubs, sub)
+				}
+			}
+
+			// remove from msg
+			subsToRemove, err := cmd.Flags().GetStringSlice(removeIprpcSubscriptionsFlagName)
+			if err != nil {
+				return err
+			}
+			for _, sub := range subsToRemove {
+				iprpcSubs, _ = slices.Remove(iprpcSubs, sub)
+			}
+
+			deposit, err := sdk.ParseCoinsNormalized(args[0])
+			if err != nil {
+				return err
+			}
+
+			msg := types.MsgSetIprpcData{
+				Authority:          authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+				IprpcSubscriptions: iprpcSubs,
+				MinIprpcCost:       cost,
+			}
+
+			submitPropMsg, err := govv1.NewMsgSubmitProposal([]sdk.Msg{&msg}, deposit, from.String(), "", "Set IPRPC data", "Set IPRPC data", isExpedited)
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), submitPropMsg)
+		},
+	}
+	cmd.Flags().String(minIprpcCostFlagName, "0ulava", "set minimum iprpc cost")
+	cmd.Flags().StringSlice(addIprpcSubscriptionsFlagName, []string{}, "add iprpc eligible subscriptions")
+	cmd.Flags().StringSlice(removeIprpcSubscriptionsFlagName, []string{}, "remove iprpc eligible subscriptions")
+	cmd.Flags().Bool(expeditedFlagName, false, "set to true to make the spec proposal expedited")
 	return cmd
 }
