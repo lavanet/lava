baa570a084049d815bc6185a6203a3f3bce17a6b
oren-lava <oren@lavanet.xyz>
2024-02-11 16:34:50 +0200

    CNS-850: added min self delegation param

--- x/dualstaking/keeper/params.go
+++ x/dualstaking/keeper/params.go
@@ -7,10 +7,18 @@ import (
 
 // GetParams get all parameters as types.Params
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams()
+	return types.NewParams(
+		k.MinSelfDelegation(ctx),
+	)
 }
 
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
+}
+
+// MinSelfDelegation returns the MinSelfDelegation param
+func (k Keeper) MinSelfDelegation(ctx sdk.Context) (res sdk.Coin) {
+	k.paramstore.Get(ctx, types.KeyMinSelfDelegation, &res)
+	return
 }
