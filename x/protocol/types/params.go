0a5e9c3eaae24d83446a8392cf16646e2cd627b8
Ran Mishael <ran@lavanet.xyz>
2024-02-12 14:38:09 +0100

    upgrade protocol version 35-1

--- x/protocol/types/params.go
+++ x/protocol/types/params.go
@@ -12,8 +12,8 @@ import (
 var _ paramtypes.ParamSet = (*Params)(nil)
 
 const (
-	TARGET_VERSION = "0.34.3"
-	MIN_VERSION    = "0.33.2"
+	TARGET_VERSION = "0.35.1"
+	MIN_VERSION    = "0.34.3"
 )
 
 var (
