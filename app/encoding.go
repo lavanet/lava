d454bdae8c67844a1da2e78182b450c19defa0be
Nguyen Quoc Duong <duongnq27.98@gmail.com>
2024-02-16 00:13:45 +0700

    Code Refactor and Error Handling Improvement

--- app/encoding.go
+++ app/encoding.go
@@ -5,13 +5,16 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
-
 	"github.com/lavanet/lava/app/params"
 )
 
-// makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
-func makeEncodingConfig() params.EncodingConfig {
+// makeEncodingConfig creates an EncodingConfig for an amino-based test configuration.
+func makeEncodingConfig() (params.EncodingConfig, error) {
 	amino := codec.NewLegacyAmino()
+	if amino == nil {
+		return params.EncodingConfig{}, errors.New("failed to create LegacyAmino codec")
+	}
+
 	interfaceRegistry := types.NewInterfaceRegistry()
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
@@ -21,15 +24,21 @@ func makeEncodingConfig() params.EncodingConfig {
 		Marshaler:         marshaler,
 		TxConfig:          txCfg,
 		Amino:             amino,
-	}
+	}, nil
 }
 
-// MakeEncodingConfig creates an EncodingConfig for testing
-func MakeEncodingConfig() params.EncodingConfig {
-	encodingConfig := makeEncodingConfig()
+// MakeEncodingConfig creates an EncodingConfig for testing.
+func MakeEncodingConfig() (params.EncodingConfig, error) {
+	encodingConfig, err := makeEncodingConfig()
+
+	if err != nil {
+		return params.EncodingConfig{}, err
+	}
+
 	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-	return encodingConfig
+
+	return encodingConfig, nil
 }
