027cf8434abe867fd5420174e915490be09df7db
oren-lava <oren@lavanet.xyz>
2024-02-21 17:54:02 +0200

    CNS-877: migrator

--- x/dualstaking/keeper/migrations.go
+++ x/dualstaking/keeper/migrations.go
@@ -6,8 +6,10 @@ import (
 
 	_ "embed"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/lavanet/lava/utils"
+	dualstakingv4 "github.com/lavanet/lava/x/dualstaking/migrations/v4"
 	dualstakingtypes "github.com/lavanet/lava/x/dualstaking/types"
 	epochstoragetypes "github.com/lavanet/lava/x/epochstorage/types"
 	"github.com/lavanet/lava/x/pairing/types"
@@ -374,4 +376,35 @@ func GetStakeStorages(lavaChainID string) map[string]epochstoragetypes.StakeStor
 func (m Migrator) MigrateVersion3To4(ctx sdk.Context) error {
 	m.keeper.SetDisableDualstakingHook(ctx, false)
 	return nil
+}
+
+// MigrateVersion4To5 change the DelegatorReward object amount's field from sdk.Coin to sdk.Coins
+func (m Migrator) MigrateVersion4To5(ctx sdk.Context) error {
+	delegatorRewards := m.GetAllDelegatorRewardV4(ctx)
+	for _, dr := range delegatorRewards {
+		delegatorReward := dualstakingtypes.DelegatorReward{
+			Delegator: dr.Delegator,
+			Provider:  dr.Provider,
+			ChainId:   dr.ChainId,
+			Amount:    sdk.NewCoins(dr.Amount),
+		}
+		m.keeper.RemoveDelegatorReward(ctx, dualstakingtypes.DelegationKey(dr.Provider, dr.Delegator, dr.ChainId))
+		m.keeper.SetDelegatorReward(ctx, delegatorReward)
+	}
+	return nil
+}
+
+func (m Migrator) GetAllDelegatorRewardV4(ctx sdk.Context) (list []dualstakingv4.DelegatorRewardv4) {
+	store := prefix.NewStore(ctx.KVStore(m.keeper.storeKey), types.KeyPrefix(dualstakingtypes.DelegatorRewardKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val dualstakingv4.DelegatorRewardv4
+		m.keeper.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
 }
