d0327da5695fb7457d46ec019f1f08be492125d7
Ran Mishael <106548467+ranlavanet@users.noreply.github.com>
2024-03-03 15:55:14 +0100

    fixing issues with reflection requests using the newst client, which require a specific error returned to work properly (#1271)

--- scripts/pre_setups/init_axelar_only_with_node.sh
+++ scripts/pre_setups/init_axelar_only_with_node.sh
@@ -42,18 +42,18 @@ PROVIDER1_LISTENER="127.0.0.1:2220"
 
 lavad tx subscription buy DefaultPlan $(lavad keys show user1 -a) -y --from user1 --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 wait_next_block
-lavad tx pairing stake-provider "LAV1" $PROVIDERSTAKE "$PROVIDER1_LISTENER,1" 1 $(operator_address) -y --from servicer1 --delegate-limit 1000ulava --provider-moniker "dummyMoniker" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+lavad tx pairing stake-provider "AXELAR" $PROVIDERSTAKE "$PROVIDER1_LISTENER,1" 1 $(operator_address) -y --from servicer1 --provider-moniker "dummyMoniker" --delegate-limit 1000ulava --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
 sleep_until_next_epoch
 
 screen -d -m -S provider1 bash -c "source ~/.bashrc; lavap rpcprovider \
-$PROVIDER1_LISTENER LAV1 rest '$LAVA_REST' \
-$PROVIDER1_LISTENER LAV1 tendermintrpc '$LAVA_RPC,$LAVA_RPC' \
-$PROVIDER1_LISTENER LAV1 grpc '$LAVA_GRPC' \
+$PROVIDER1_LISTENER AXELAR rest '$AXELAR_REST' \
+$PROVIDER1_LISTENER AXELAR tendermintrpc '$AXELAR_RPC,$AXELAR_RPC' \
+$PROVIDER1_LISTENER AXELAR grpc '$AXELAR_GRPC' \
 $EXTRA_PROVIDER_FLAGS --geolocation 1 --log_level debug --from servicer1 --chain-id lava --metrics-listen-address ":7776" 2>&1 | tee $LOGS_DIR/PROVIDER1.log" && sleep 0.25
 
 screen -d -m -S consumers bash -c "source ~/.bashrc; lavap rpcconsumer \
-127.0.0.1:3360 LAV1 rest 127.0.0.1:3361 LAV1 tendermintrpc 127.0.0.1:3362 LAV1 grpc \
+127.0.0.1:3360 AXELAR rest 127.0.0.1:3361 AXELAR tendermintrpc 127.0.0.1:3362 AXELAR grpc \
 $EXTRA_PORTAL_FLAGS --geolocation 1 --log_level debug --from user1 --chain-id lava --allow-insecure-provider-dialing --metrics-listen-address ":7779" 2>&1 | tee $LOGS_DIR/CONSUMERS.log" && sleep 0.25
 
 echo "--- setting up screens done ---"
