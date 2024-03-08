81c447df726ba0cc3d35dc26d17658367550e64a
oren-lava <111131399+oren-lava@users.noreply.github.com>
2024-03-08 08:02:48 +0200

    CNS-877: IPRPC part 6 - migrator (#1243)
    * CNS-877: migrator
    
    * CNS-874: updated README
    
    ---------
    
    Co-authored-by: Yaroms <103432884+Yaroms@users.noreply.github.com>
    Co-authored-by: Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>

--- x/rewards/module.go
+++ x/rewards/module.go
@@ -127,6 +127,14 @@ func (am AppModule) Name() string {
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+
+	migrator := keeper.NewMigrator(am.keeper)
+
+	// register v1 -> v2 migration
+	if err := cfg.RegisterMigration(types.ModuleName, 1, migrator.MigrateVersion1To2); err != nil {
+		// panic:ok: at start up, migration cannot proceed anyhow
+		panic(fmt.Errorf("%s: failed to register migration to v2: %w", types.ModuleName, err))
+	}
 }
 
 // RegisterInvariants registers the capability module's invariants.
@@ -151,7 +159,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
