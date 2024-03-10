0e046da34da5bc46ca0e1ed24dd465105aa86bd5
Ran Mishael <ran@lavanet.xyz>
2024-03-10 20:45:02 +0100

    remove test body

--- protocol/test/test_test.go
+++ protocol/test/test_test.go
@@ -1,22 +1,11 @@
 package test
 
 import (
-	"encoding/base64"
-	"fmt"
-	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 // used as a testing wrapper to just test fun stuff quickly.
 
 func TestFlow(t *testing.T) {
-	// Decoding base64 string
-	bodyString := "CiEyNTAwMDAwMC4wMDAwMDAwMDAwMDAwMDAwMDBhZXZtb3M="
-	decodedBody, err := base64.StdEncoding.DecodeString(bodyString)
-	require.Nil(t, err)
-	fmt.Println("Decoded body:", string(decodedBody)) // Output the decoded body
-	resultString := strings.ReplaceAll(string(decodedBody), "!", "")
-	fmt.Println("Result:", resultString) // Output the decoded body
+
 }
