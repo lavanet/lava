22e9e88b359e7d2e654cefb48312e14861fc3620
Elad Gildnur <elad.gildnur@gmail.com>
2024-03-18 16:56:04 +0200

    Add Stringify to RelayFinalization

--- x/conflict/types/relay_finalization.go
+++ x/conflict/types/relay_finalization.go
@@ -1,7 +1,11 @@
 package types
 
 import (
+	"encoding/json"
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/utils"
 	"github.com/lavanet/lava/utils/sigs"
 	pairingtypes "github.com/lavanet/lava/x/pairing/types"
 )
@@ -39,4 +43,19 @@ func (rf RelayFinalization) DataToSign() []byte {
 
 func (rf RelayFinalization) HashRounds() int {
 	return 1
+}
+
+func (rf RelayFinalization) Stringify() string {
+	consumerAddr := sdk.AccAddress{}
+	consumerAddr.Unmarshal([]byte(rf.ConsumerAddress))
+	consumerAddrStr := consumerAddr.String()
+
+	finalizedBlocks := map[int64]string{}
+	json.Unmarshal(rf.FinalizedBlocksHashes, &finalizedBlocks)
+
+	return fmt.Sprintf("{latestBlock: %v consumerAddress: %v blockDistanceToFinalization: %v specId: %v epoch: %v finalizedBlocksHashes: %v}",
+		utils.StrValue(rf.LatestBlock), consumerAddrStr,
+		utils.StrValue(rf.BlockDistanceToFinalization), utils.StrValue(rf.SpecId),
+		utils.StrValue(rf.Epoch), utils.StrValue(finalizedBlocks),
+	)
 }
