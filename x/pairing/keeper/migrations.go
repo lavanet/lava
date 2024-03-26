7a652219d862ce7d5e235326bd78cd1df0af6e67
oren-lava <oren@lavanet.xyz>
2024-03-26 18:43:21 +0200

    CNS-930: migrator

---
+++ x/pairing/keeper/migrations.go
@@ -0,0 +1,22 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v2 "github.com/lavanet/lava/x/pairing/migrations/v2"
+)
+
+type Migrator struct {
+	keeper Keeper
+}
+
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// MigrateVersion2To3 removes all the old payment objects (to get a fresh start for the new ones)
+func (m Migrator) MigrateVersion2To3(ctx sdk.Context) error {
+	v2.RemoveAllUniquePaymentStorageClientProvider(ctx, m.keeper.storeKey)
+	v2.RemoveAllProviderPaymentStorage(ctx, m.keeper.storeKey)
+	v2.RemoveAllEpochPayments(ctx, m.keeper.storeKey)
+	return nil
+}
