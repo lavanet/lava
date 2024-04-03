a2db5007dc532a8df172cf145a1af3a93ffdd260
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2024-04-03 16:37:43 +0300

    fix print

--- x/spec/module.go
+++ x/spec/module.go
@@ -139,7 +139,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	// register v3 -> v4 migration
 	if err := cfg.RegisterMigration(types.ModuleName, 3, migrator.Migrate3to4); err != nil {
 		// panic:ok: at start up, migration cannot proceed anyhow
-		panic(fmt.Errorf("%s: failed to register migration to v3: %w", types.ModuleName, err))
+		panic(fmt.Errorf("%s: failed to register migration to v4: %w", types.ModuleName, err))
 	}
 }
 
