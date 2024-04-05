e74e60f12205222d6ea1d67632176bfa55191a47
Yaroms <yaroms@lavanet.xyz>
2024-04-05 09:04:00 +0300

    fix the key decoding

--- x/pairing/types/epoch_cu.go
+++ x/pairing/types/epoch_cu.go
@@ -37,11 +37,15 @@ func ProviderConsumerEpochCuKey(epoch uint64, provider string, project string, c
 }
 
 func DecodeUniqueEpochSessionKey(key string) (epoch uint64, provider string, chainID string, project string, sessionID uint64, err error) {
-	split := strings.Split(key, " ")
-	if len(split) != 5 {
+	if len(key) < 9 {
 		return 0, "", "", "", 0, fmt.Errorf("invalid UniqueEpochSession key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(split[0]))
+
+	split := strings.Split(key[9:], " ")
+	if len(split) != 4 {
+		return 0, "", "", "", 0, fmt.Errorf("invalid UniqueEpochSession key: bad structure. key: %s", key)
+	}
+	epoch = DecodeBlock([]byte(key[0:8]))
 	sessionID, err = strconv.ParseUint(split[4], 10, 64)
 	if err != nil {
 		return 0, "", "", "", 0, fmt.Errorf("invalid UniqueEpochSession key: bad session ID. key: %s", key)
@@ -50,20 +54,26 @@ func DecodeUniqueEpochSessionKey(key string) (epoch uint64, provider string, cha
 }
 
 func DecodeProviderEpochCuKey(key string) (epoch uint64, provider string, chainID string, err error) {
-	split := strings.Split(key, " ")
-	if len(split) != 3 {
+	if len(key) < 9 {
 		return 0, "", "", fmt.Errorf("invalid ProviderEpochCu key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(split[0]))
+	split := strings.Split(key[9:], " ")
+	if len(split) != 2 {
+		return 0, "", "", fmt.Errorf("invalid ProviderEpochCu key: bad structure. key: %s", key)
+	}
+	epoch = DecodeBlock([]byte(key[0:8]))
 	return epoch, split[1], split[2], nil
 }
 
 func DecodeProviderConsumerEpochCuKey(key string) (epoch uint64, provider string, project string, chainID string, err error) {
-	split := strings.Split(key, " ")
-	if len(split) != 4 {
+	if len(key) < 9 {
 		return 0, "", "", "", fmt.Errorf("invalid ProviderConsumerEpochCu key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(split[0]))
+	split := strings.Split(key[9:], " ")
+	if len(split) != 3 {
+		return 0, "", "", "", fmt.Errorf("invalid ProviderConsumerEpochCu key: bad structure. key: %s", key)
+	}
+	epoch = DecodeBlock([]byte(key[0:8]))
 	return epoch, split[1], split[2], split[3], nil
 }
 
