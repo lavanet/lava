4fa5ce3866ccedb05445c6ff3361d80636d718cc
Ran Mishael <ran@lavanet.xyz>
2024-04-30 15:56:14 +0200

    checking nil 6

--- protocol/rpcprovider/reliabilitymanager/reliability_manager.go
+++ protocol/rpcprovider/reliabilitymanager/reliability_manager.go
@@ -112,11 +112,14 @@ func (rm *ReliabilityManager) VoteHandler(voteParams *VoteParams, nodeHeight uin
 		}
 		// TODO: get extensions and addons from the request
 		replyWrapper, _, _, _, _, err := rm.chainRouter.SendNodeMsg(ctx, nil, chainMessage, nil)
-		reply := replyWrapper.RelayReply
 		if err != nil {
 			return utils.LavaFormatError("vote relay send has failed", err,
 				utils.Attribute{Key: "ApiURL", Value: voteParams.ApiURL}, utils.Attribute{Key: "RequestData", Value: voteParams.RequestData})
 		}
+		if replyWrapper == nil || replyWrapper.RelayReply == nil {
+			return utils.LavaFormatError("vote relay send has failed, relayWrapper is nil", nil, utils.Attribute{Key: "ApiURL", Value: voteParams.ApiURL}, utils.Attribute{Key: "RequestData", Value: voteParams.RequestData})
+		}
+		reply := replyWrapper.RelayReply
 		reply.Metadata, _, _ = rm.chainParser.HandleHeaders(reply.Metadata, chainMessage.GetApiCollection(), spectypes.Header_pass_reply)
 		nonce := rand.Int63()
 		relayData := BuildRelayDataFromVoteParams(voteParams)
