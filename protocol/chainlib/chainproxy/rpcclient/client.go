c5fdecb180d46f2a0d9ae52f56e607950025cf2f
Ran Mishael <ran@lavanet.xyz>
2024-05-12 18:03:20 +0200

    fix case

--- protocol/chainlib/chainproxy/rpcclient/client.go
+++ protocol/chainlib/chainproxy/rpcclient/client.go
@@ -292,9 +292,7 @@ func (c *Client) CallContext(ctx context.Context, id json.RawMessage, method str
 	var msg *JsonrpcMessage
 	var err error
 	switch p := params.(type) {
-	case []interface{}:
-		msg, err = c.newMessageArrayWithID(method, id, p)
-	case string:
+	case []interface{}, string:
 		msg, err = c.newMessageArrayWithID(method, id, p)
 	case map[string]interface{}:
 		msg, err = c.newMessageMapWithID(method, id, p)
