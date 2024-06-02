2d9d6ac25e0b7c5312424d2138326c130a0b0680
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2024-06-02 16:43:30 +0300

    add tendermint spec to cli commands

--- scripts/init_payment_e2e.sh
+++ scripts/init_payment_e2e.sh
@@ -8,7 +8,7 @@ source $__dir/useful_commands.sh
 GASPRICE="0.000000001ulava"
 
 # Specs proposal
-lavad tx gov submit-legacy-proposal spec-add ./cookbook/specs/cosmoswasm.json,./cookbook/specs/ibc.json,./cookbook/specs/tendermint.json,./cookbook/specs/cosmossdk.json,./cookbook/specs/cosmossdk_full.json,./cookbook/specs/lava.json --lava-dev-test -y --from alice --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+lavad tx gov submit-legacy-proposal spec-add ./cookbook/specs/cosmoswasm.json,./cookbook/specs/ibc.json,./cookbook/specs/tendermint.json,./cookbook/specs/tendermint.json,./cookbook/specs/cosmossdk.json,./cookbook/specs/cosmossdk_full.json,./cookbook/specs/lava.json --lava-dev-test -y --from alice --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 wait_next_block
 lavad tx gov deposit 1 100ulava -y --from alice --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 wait_next_block
