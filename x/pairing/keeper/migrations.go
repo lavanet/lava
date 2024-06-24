ffde7c1eca8cf263261d5524337903ca8a1e1201
Oren <oren@lavanet.xyz>
2024-06-24 17:10:21 +0300

    CNS-980: migrator

--- x/pairing/keeper/migrations.go
+++ x/pairing/keeper/migrations.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v2 "github.com/lavanet/lava/x/pairing/migrations/v2"
 )
@@ -18,5 +19,19 @@ func (m Migrator) MigrateVersion2To3(ctx sdk.Context) error {
 	v2.RemoveAllUniquePaymentStorageClientProvider(ctx, m.keeper.storeKey)
 	v2.RemoveAllProviderPaymentStorage(ctx, m.keeper.storeKey)
 	v2.RemoveAllEpochPayments(ctx, m.keeper.storeKey)
+	return nil
+}
+
+// MigrateVersion2To3 removes all the old payment objects (to get a fresh start for the new ones)
+func (m Migrator) MigrateVersion3To4(ctx sdk.Context) error {
+	pcecs := m.keeper.GetAllProviderConsumerEpochCuStore(ctx)
+	for i := range pcecs {
+		m.keeper.RemoveProviderConsumerEpochCu(ctx, pcecs[i].Epoch, pcecs[i].Provider, pcecs[i].Project, pcecs[i].ChainId)
+		pcec := pcecs[i].ProviderConsumerEpochCu
+		pcec.QosSum = math.LegacyZeroDec()
+		pcec.QosAmount = 0
+		m.keeper.SetProviderConsumerEpochCu(ctx, pcecs[i].Epoch, pcecs[i].Provider, pcecs[i].Project, pcecs[i].ChainId, pcec)
+	}
+
 	return nil
 }
