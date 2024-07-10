a908d599986d66a0c526da0aca430d5b34e85a6c
Oren <oren@lavanet.xyz>
2024-07-10 14:45:20 +0300

    CNS-994: use utils to encode/decode epoch

--- x/pairing/types/epoch_cu.go
+++ x/pairing/types/epoch_cu.go
@@ -1,10 +1,11 @@
 package types
 
 import (
-	"encoding/binary"
 	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/lavanet/lava/utils"
 )
 
 const (
@@ -14,26 +15,16 @@ const (
 	ProviderConsumerEpochCuPrefix   = "ProviderConsumerEpochCu/"
 )
 
-func EncodeBlock(block uint64) []byte {
-	encodedKey := make([]byte, 8)
-	binary.BigEndian.PutUint64(encodedKey[0:8], block)
-	return encodedKey
-}
-
-func DecodeBlock(encodedKey []byte) uint64 {
-	return binary.BigEndian.Uint64(encodedKey[0:8])
-}
-
 func UniqueEpochSessionKey(epoch uint64, provider string, chainID string, project string, sessionID uint64) []byte {
-	return append(EncodeBlock(epoch), []byte(strings.Join([]string{provider, chainID, project, strconv.FormatUint(sessionID, 10)}, " "))...)
+	return append(utils.Serialize(epoch), []byte(strings.Join([]string{provider, chainID, project, strconv.FormatUint(sessionID, 10)}, " "))...)
 }
 
 func ProviderEpochCuKey(epoch uint64, provider string, chainID string) []byte {
-	return append(EncodeBlock(epoch), []byte(strings.Join([]string{provider, chainID}, " "))...)
+	return append(utils.Serialize(epoch), []byte(strings.Join([]string{provider, chainID}, " "))...)
 }
 
 func ProviderConsumerEpochCuKey(epoch uint64, provider string, project string, chainID string) []byte {
-	return append(EncodeBlock(epoch), []byte(strings.Join([]string{provider, project, chainID}, " "))...)
+	return append(utils.Serialize(epoch), []byte(strings.Join([]string{provider, project, chainID}, " "))...)
 }
 
 func DecodeUniqueEpochSessionKey(key string) (epoch uint64, provider string, chainID string, project string, sessionID uint64, err error) {
@@ -45,7 +36,7 @@ func DecodeUniqueEpochSessionKey(key string) (epoch uint64, provider string, cha
 	if len(split) != 4 {
 		return 0, "", "", "", 0, fmt.Errorf("invalid UniqueEpochSession key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(key[:8]))
+	utils.Deserialize([]byte(key[:8]), &epoch)
 	sessionID, err = strconv.ParseUint(split[3], 10, 64)
 	if err != nil {
 		return 0, "", "", "", 0, fmt.Errorf("invalid UniqueEpochSession key: bad session ID. key: %s", key)
@@ -61,7 +52,7 @@ func DecodeProviderEpochCuKey(key string) (epoch uint64, provider string, chainI
 	if len(split) != 2 {
 		return 0, "", "", fmt.Errorf("invalid ProviderEpochCu key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(key[:8]))
+	utils.Deserialize([]byte(key[:8]), &epoch)
 	return epoch, split[0], split[1], nil
 }
 
@@ -73,7 +64,8 @@ func DecodeProviderConsumerEpochCuKey(key string) (epoch uint64, provider string
 	if len(split) != 3 {
 		return 0, "", "", "", fmt.Errorf("invalid ProviderConsumerEpochCu key: bad structure. key: %s", key)
 	}
-	epoch = DecodeBlock([]byte(key[:8]))
+
+	utils.Deserialize([]byte(key[:8]), &epoch)
 	return epoch, split[0], split[1], split[2], nil
 }
 
