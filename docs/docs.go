57c29df45239b32f20eff2dc6ae904319c8c2a86
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-07-19 15:13:20 +0300

    Update docs.go

--- docs/docs.go
+++ docs/docs.go
@@ -19,25 +19,50 @@ var staticFiles embed.FS
 //go:embed template
 var templateFiles embed.FS
 
-func RegisterOpenAPIService(appName string, rtr *mux.Router) {
-	rtr.Handle(apiFilePath, http.FileServer(http.FS(staticFiles)))
-	rtr.HandleFunc("/", handler(appName))
+func RegisterOpenAPIService(appName string, rtr *mux.Router) error {
+	if err := registerStaticFiles(rtr); err != nil {
+		return err
+	}
+	if err := registerTemplateHandler(appName, rtr); err != nil {
+		return err
+	}
+	return nil
 }
 
-// openapi handler returns a handler that serves the openapi documentation
-func handler(title string) http.HandlerFunc {
+func registerStaticFiles(rtr *mux.Router) error {
+	rtr.Handle(apiFilePath, http.FileServer(http.FS(staticFiles)))
+	return nil
+}
+
+func registerTemplateHandler(appName string, rtr *mux.Router) error {
+	t, err := parseTemplates()
+	if err != nil {
+		return err
+	}
+	rtr.HandleFunc("/", handler(t, appName))
+	return nil
+}
+
+func parseTemplates() (*httptemplate.Template, error) {
 	t, err := httptemplate.ParseFS(templateFiles, indexTemplatePath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	return t, nil
+}
 
+func handler(t *httptemplate.Template, title string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		t.Execute(w, struct {
+		data := struct {
 			Title string
 			URL   string
 		}{
-			title,
-			apiFilePath,
-		})
+			Title: title,
+			URL:   apiFilePath,
+		}
+		if err := t.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
+
