d8ef59d6447de6f4078d8320482716291e3e2fb0
Elad Gildnur <elad.gildnur@gmail.com>
2024-07-21 13:04:27 +0200

    Test fix for lint

--- protocol/chainlib/consumer_ws_subscription_manager_test.go
+++ protocol/chainlib/consumer_ws_subscription_manager_test.go
@@ -266,8 +266,8 @@ func TestConsumerWSSubscriptionManager(t *testing.T) {
 			relaySender.
 				EXPECT().
 				SendParsedRelay(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Cond(func(x any) bool {
-					relayPrivateData := x.(*pairingtypes.RelayPrivateData)
-					if relayPrivateData == nil {
+					relayPrivateData, ok := x.(*pairingtypes.RelayPrivateData)
+					if !ok || relayPrivateData == nil {
 						return false
 					}
 
@@ -296,7 +296,8 @@ func TestConsumerWSSubscriptionManager(t *testing.T) {
 			relaySender.
 				EXPECT().
 				ParseRelay(gomock.Any(), gomock.Any(), gomock.Cond(func(x any) bool {
-					reqData := x.(string)
+					reqData, ok := x.(string)
+					require.True(t, ok)
 					areEqual := reqData == string(play.unsubscribeMessage1)
 					return areEqual
 				}), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
@@ -308,7 +309,8 @@ func TestConsumerWSSubscriptionManager(t *testing.T) {
 			relaySender.
 				EXPECT().
 				ParseRelay(gomock.Any(), gomock.Any(), gomock.Cond(func(x any) bool {
-					reqData := x.(string)
+					reqData, ok := x.(string)
+					require.True(t, ok)
 					areEqual := reqData == string(play.subscriptionRequestData1)
 					return areEqual
 				}), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
@@ -428,7 +430,8 @@ func TestConsumerWSSubscriptionManager(t *testing.T) {
 			relaySender.
 				EXPECT().
 				ParseRelay(gomock.Any(), gomock.Any(), gomock.Cond(func(x any) bool {
-					reqData := x.(string)
+					reqData, ok := x.(string)
+					require.True(t, ok)
 					areEqual := reqData == string(play.unsubscribeMessage2)
 					return areEqual
 				}), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
@@ -440,7 +443,8 @@ func TestConsumerWSSubscriptionManager(t *testing.T) {
 			relaySender.
 				EXPECT().
 				ParseRelay(gomock.Any(), gomock.Any(), gomock.Cond(func(x any) bool {
-					reqData := x.(string)
+					reqData, ok := x.(string)
+					require.True(t, ok)
 					areEqual := reqData == string(play.subscriptionRequestData2)
 					return areEqual
 				}), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
