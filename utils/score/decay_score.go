75b90d1d86297bc2e1ab25d771e63a5c7300375a
Oren <oren@lavanet.xyz>
2024-07-31 14:01:46 +0300

    CNS-1002: collect raw qos data

--- utils/score/decay_score.go
+++ utils/score/decay_score.go
@@ -29,12 +29,19 @@ func NewScoreStore(num, denom float64, inpTime time.Time) ScoreStore {
 // where now is the current time.
 //
 // Note that the returned ScoreStore has a new Time field set to the current time.
-func CalculateTimeDecayFunctionUpdate(oldScore, newScore ScoreStore, halfLife time.Duration, updateWeight float64, sampleTime time.Time) ScoreStore {
+func CalculateTimeDecayFunctionUpdate(oldScore, newScore ScoreStore, halfLife time.Duration, updateWeight float64, sampleTime time.Time, isHanging bool) (normalizedScoreStore ScoreStore, rawScoreStore ScoreStore) {
 	oldDecayExponent := math.Ln2 * sampleTime.Sub(oldScore.Time).Seconds() / halfLife.Seconds()
 	oldDecayFactor := math.Exp(-oldDecayExponent)
 	newDecayExponent := math.Ln2 * sampleTime.Sub(newScore.Time).Seconds() / halfLife.Seconds()
 	newDecayFactor := math.Exp(-newDecayExponent)
 	updatedNum := oldScore.Num*oldDecayFactor + newScore.Num*newDecayFactor*updateWeight
 	updatedDenom := oldScore.Denom*oldDecayFactor + newScore.Denom*newDecayFactor*updateWeight
-	return NewScoreStore(updatedNum, updatedDenom, sampleTime)
+
+	// calculate raw denom for reputation for non-hanging API.
+	// Raw denom = denom not divided by benchmark value (=denom of a new ScoreStore)
+	updatedRawDenom := oldDecayFactor
+	if !isHanging {
+		updatedRawDenom += newDecayFactor * updateWeight // removed newScore.Denom from update to get raw data
+	}
+	return NewScoreStore(updatedNum, updatedDenom, sampleTime), NewScoreStore(updatedNum, updatedRawDenom, sampleTime)
 }
