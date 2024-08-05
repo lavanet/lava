68ab303d6733d09378c500421ffc71b2ce59cf42
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2024-08-05 16:48:23 +0300

    added chain id

--- x/subscription/keeper/cu_tracker.go
+++ x/subscription/keeper/cu_tracker.go
@@ -202,7 +202,7 @@ func (k Keeper) RewardAndResetCuTracker(ctx sdk.Context, cuTrackerTimerKeyBytes 
 		// Note: if the reward function doesn't reward the provider
 		// because he was unstaked, we only print an error and not returning
 
-		providerReward, _, err := k.dualstakingKeeper.RewardProvidersAndDelegators(ctx, provider, chainID, sdk.NewCoins(creditToSub), types.ModuleName, false, false, false)
+		_, _, err := k.dualstakingKeeper.RewardProvidersAndDelegators(ctx, provider, chainID, sdk.NewCoins(creditToSub), types.ModuleName, false, false, false)
 		if errors.Is(err, epochstoragetypes.ErrProviderNotStaked) || errors.Is(err, epochstoragetypes.ErrStakeStorageNotFound) {
 			utils.LavaFormatWarning("sending provider reward with delegations failed", err,
 				utils.Attribute{Key: "provider", Value: provider},
@@ -219,7 +219,7 @@ func (k Keeper) RewardAndResetCuTracker(ctx sdk.Context, cuTrackerTimerKeyBytes 
 				utils.Attribute{Key: "block", Value: ctx.BlockHeight()},
 			)
 		} else {
-			details[provider] = fmt.Sprintf("cu: %d reward: %s", trackedCu, providerReward.String())
+			details[provider+" "+chainID] = fmt.Sprintf("cu: %d reward: %s", trackedCu, creditToSub.String())
 		}
 	}
 
