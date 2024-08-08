679ac015de3fa3c2d12fbf1eae032303e9ee2c39
Oren <oren@lavanet.xyz>
2024-08-08 12:13:41 +0300

    CNS-994: import fixes

--- x/epochstorage/keeper/migrations.go
+++ x/epochstorage/keeper/migrations.go
@@ -9,9 +9,9 @@ import (
 	"cosmossdk.io/collections"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/lavanet/lava/utils"
-	"github.com/lavanet/lava/x/epochstorage/types"
-	v3 "github.com/lavanet/lava/x/epochstorage/types/migrations/v3"
+	"github.com/lavanet/lava/v2/utils"
+	"github.com/lavanet/lava/v2/x/epochstorage/types"
+	v3 "github.com/lavanet/lava/v2/x/epochstorage/types/migrations/v3"
 )
 
 type Migrator struct {
--- x/epochstorage/keeper/stake_entries.go
+++ x/epochstorage/keeper/stake_entries.go
@@ -7,8 +7,8 @@ import (
 
 	"cosmossdk.io/collections"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/lavanet/lava/utils"
-	"github.com/lavanet/lava/x/epochstorage/types"
+	"github.com/lavanet/lava/v2/utils"
+	"github.com/lavanet/lava/v2/x/epochstorage/types"
 )
 
 /* ########## StakeEntry ############ */
