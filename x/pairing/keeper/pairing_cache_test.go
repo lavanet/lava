d127c233fdaea6f7dd255642bd33840ba64fd9dd
Oren <oren@lavanet.xyz>
2024-08-13 15:10:04 +0300

    CNS-999: added unit test

--- x/pairing/keeper/pairing_cache_test.go
+++ x/pairing/keeper/pairing_cache_test.go
@@ -77,3 +77,45 @@ func TestPairingRelayCache(t *testing.T) {
 	emptyCacheAgainGas := getRelayPairingGas(ts)
 	require.InEpsilon(t, emptyCacheGas, emptyCacheAgainGas, 0.05)
 }
+
+// TestPairingRelayCacheReset tests that the pairing relay cache reset truly returns the state to its previous
+// empty cache state by verifying the gas is exactly the same when running the same test with different testers
+func TestPairingRelayCacheReset(t *testing.T) {
+	firstEmptyCache, firstNonEmptyCache, secondEmptyCache, secondNonEmptyCache := uint64(0), uint64(0), uint64(0), uint64(0)
+	for i := 0; i < 2; i++ {
+		ts := newTester(t)
+		ts.setupForPayments(1, 1, 0) // 1 provider, 1 client, default providers-to-pair
+
+		consumer, _ := ts.GetAccount(common.CONSUMER, 0)
+		provider, _ := ts.GetAccount(common.PROVIDER, 0)
+
+		getRelayPairingGas := func(ts *tester) uint64 {
+			gm := ts.Ctx.GasMeter()
+			before := gm.GasConsumed()
+			relayPayment := sendRelay(ts, provider.Addr.String(), consumer, []string{ts.spec.Index})
+			_, err := ts.TxPairingRelayPayment(relayPayment.Creator, relayPayment.Relays[0])
+			require.NoError(t, err)
+			return gm.GasConsumed() - before
+		}
+
+		// query for pairing for the first time - empty cache
+		emptyCacheGas := getRelayPairingGas(ts)
+
+		// query for pairing for the second time - non-empty cache
+		filledCacheGas := getRelayPairingGas(ts)
+
+		// second time gas should be smaller than first time
+		require.Less(t, filledCacheGas, emptyCacheGas)
+
+		if i == 0 {
+			firstEmptyCache = emptyCacheGas
+			firstNonEmptyCache = filledCacheGas
+		} else {
+			secondEmptyCache = emptyCacheGas
+			secondNonEmptyCache = filledCacheGas
+		}
+	}
+
+	require.Equal(t, firstEmptyCache, secondEmptyCache)
+	require.Equal(t, firstNonEmptyCache, secondNonEmptyCache)
+}
