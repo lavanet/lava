75bc608adbc857743fa10dfe4119059767aef310
amitza <9674751+amitza@users.noreply.github.com>
2024-09-12 01:44:41 +0300

    testify

--- protocol/rpcprovider/provider_listener_test.go
+++ protocol/rpcprovider/provider_listener_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/lavanet/lava/v3/protocol/lavasession"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -18,22 +19,18 @@ func TestHealthCheck(t *testing.T) {
 		DisableTLS: true,
 	}
 
-	providerListener := NewProviderListener(context.Background(), networkAddressData, "/lava/health")
+	healthcheckPath := "/lava/health"
+
+	providerListener := NewProviderListener(context.Background(), networkAddressData, healthcheckPath)
 
 	defer providerListener.Shutdown(context.Background())
 
 	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		t.Fatalf("GRPC new client failed: %v", err)
-	}
+	require.NoError(t, err)
 
 	client := grpc_health_v1.NewHealthClient(conn)
 	resp, err := client.Check(context.Background(), &grpc_health_v1.HealthCheckRequest{})
-	if err != nil {
-		t.Fatalf("Health check failed: %v", err)
-	}
+	require.NoError(t, err)
 
-	if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
-		t.Errorf("Unexpected health status: got %v, want %v", resp.Status, grpc_health_v1.HealthCheckResponse_SERVING)
-	}
+	require.Equal(t, grpc_health_v1.HealthCheckResponse_SERVING, resp.Status, "Unexpected health status")
 }
