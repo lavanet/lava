09efb19471f9d1491dd4c5e6b5814c3284e8ea8c
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2024-09-19 12:45:02 +0300

    added dualstaking migrator

--- x/dualstaking/keeper/migrations.go
+++ x/dualstaking/keeper/migrations.go
@@ -2,6 +2,11 @@ package keeper
 
 import (
 	_ "embed"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/lavanet/lava/v3/x/dualstaking/types"
+	fixationtypes "github.com/lavanet/lava/v3/x/fixationstore/types"
 )
 
 type Migrator struct {
@@ -10,4 +15,70 @@ type Migrator struct {
 
 func NewMigrator(keeper Keeper) Migrator {
 	return Migrator{keeper: keeper}
+}
+
+func (m Migrator) MigrateVersion5To6(ctx sdk.Context) error {
+	nextEpoch := m.keeper.epochstorageKeeper.GetCurrentNextEpoch(ctx)
+
+	// prefix for the delegations fixation store
+	const (
+		DelegationPrefix         = "delegation-fs"
+		DelegatorPrefix          = "delegator-fs"
+		UnbondingPrefix          = "unbonding-ts"
+		DelegatorRewardKeyPrefix = "DelegatorReward/value/"
+	)
+
+	// set delegations
+	delegationFS := fixationtypes.NewFixationStore(m.keeper.storeKey, m.keeper.cdc, DelegationPrefix, nil, nil)
+	incisec := delegationFS.GetAllEntryIndices(ctx)
+	for _, index := range incisec {
+		var oldDelegation types.Delegation
+		found := delegationFS.FindEntry(ctx, index, nextEpoch, &oldDelegation)
+		if found {
+			delegation, found := m.keeper.GetDelegation(ctx, oldDelegation.Provider, oldDelegation.Delegator)
+			if found {
+				delegation.Amount = delegation.Amount.Add(oldDelegation.Amount)
+			} else {
+				delegation = oldDelegation
+			}
+			m.keeper.SetDelegation(ctx, delegation)
+		}
+	}
+
+	// set rewards
+	store := prefix.NewStore(ctx.KVStore(m.keeper.storeKey), types.KeyPrefix(DelegatorRewardKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.DelegatorReward
+		m.keeper.cdc.MustUnmarshal(iterator.Value(), &val)
+		reward, found := m.keeper.GetDelegatorReward(ctx, val.Delegator, val.Delegator)
+		if found {
+			reward.Amount = reward.Amount.Add(val.Amount...)
+		} else {
+			reward = val
+		}
+		m.keeper.SetDelegatorReward(ctx, reward)
+	}
+
+	// now delete the stores
+	deleteStore := func(prefixString string) {
+		store := prefix.NewStore(ctx.KVStore(m.keeper.storeKey), types.KeyPrefix(prefixString))
+		iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+		defer iterator.Close()
+
+		for ; iterator.Valid(); iterator.Next() {
+			store.Delete(iterator.Key())
+		}
+	}
+
+	deleteStore(DelegationPrefix)
+	deleteStore(DelegatorPrefix)
+	deleteStore(UnbondingPrefix)
+	deleteStore(DelegatorRewardKeyPrefix)
+
+	return nil
 }
--- x/dualstaking/module.go
+++ x/dualstaking/module.go
@@ -116,13 +116,13 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 
-	// migrator := keeper.NewMigrator(am.keeper)
+	migrator := keeper.NewMigrator(am.keeper)
 
-	// // register v4 -> v5 migration
-	// if err := cfg.RegisterMigration(types.ModuleName, 4, migrator.MigrateVersion4To5); err != nil {
-	// 	// panic:ok: at start up, migration cannot proceed anyhow
-	// 	panic(fmt.Errorf("%s: failed to register migration to v5: %w", types.ModuleName, err))
-	// }
+	// register v5 -> v6 migration
+	if err := cfg.RegisterMigration(types.ModuleName, 5, migrator.MigrateVersion5To6); err != nil {
+		// panic:ok: at start up, migration cannot proceed anyhow
+		panic(fmt.Errorf("%s: failed to register migration to v5: %w", types.ModuleName, err))
+	}
 }
 
 // RegisterInvariants registers the invariants of the module. If an invariant deviates from its predicted value, the InvariantRegistry triggers appropriate logic (most often the chain will be halted)
@@ -146,7 +146,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion is a sequence number for state-breaking change of the module. It should be incremented on each consensus-breaking change introduced by the module. To avoid wrong/empty versions, the initial version should be set to 1
-func (AppModule) ConsensusVersion() uint64 { return 5 }
+func (AppModule) ConsensusVersion() uint64 { return 6 }
 
 // BeginBlock contains the logic that is automatically triggered at the beginning of each block
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
