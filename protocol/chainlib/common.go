9458d6c71c4809897bbaecf7b11742151c064e2a
leon mandel <lmandel@leons-MacBook-Pro.local>
2024-10-06 13:17:45 +0200

    fixed chainlib common formatting

--- protocol/chainlib/common.go
+++ protocol/chainlib/common.go
@@ -30,7 +30,7 @@ const (
 	relayMsgLogMaxChars        = 200
 	RPCProviderNodeAddressHash = "Lava-Provider-Node-Address-Hash"
 	RPCProviderNodeExtension   = "Lava-Provider-Node-Extension"
-	RpcProviderLoadRateHeader  = "Lava-provider-load-rate"
+	RpcProviderLoadRateHeader  = "Lava-Provider-load-rate"
 	RpcProviderUniqueIdHeader  = "Lava-Provider-Unique-Id"
 	WebSocketExtension         = "websocket"
 )
