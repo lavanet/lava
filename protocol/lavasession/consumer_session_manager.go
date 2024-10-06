c601166e323595125b54b208de605f019ccace4c
Elad Gildnur <elad.gildnur@gmail.com>
2024-10-06 14:01:26 +0300

    Fix lint

--- protocol/lavasession/consumer_session_manager.go
+++ protocol/lavasession/consumer_session_manager.go
@@ -74,6 +74,7 @@ func (csm *ConsumerSessionManager) GetNumberOfValidProviders() int {
 func (csm *ConsumerSessionManager) RPCEndpoint() RPCEndpoint {
 	return *csm.rpcEndpoint
 }
+
 func (csm *ConsumerSessionManager) UpdateAllProviders(epoch uint64, pairingList map[uint64]*ConsumerSessionsWithProvider) error {
 	pairingListLength := len(pairingList)
 	// TODO: we can block updating until some of the probing is done, this can prevent failed attempts on epoch change when we have no information on the providers,
