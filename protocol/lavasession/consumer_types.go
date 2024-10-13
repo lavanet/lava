cd6854a9afefbd455486a8f3e3cee1227fae9605
Oren <oren@lavanet.xyz>
2024-10-13 14:58:05 +0300

    CNS-1008: revert raw qos report code

--- protocol/lavasession/consumer_types.go
+++ protocol/lavasession/consumer_types.go
@@ -72,9 +72,9 @@ type ConsumerSessionsMap map[string]*SessionInfo
 type ProviderOptimizer interface {
 	AppendProbeRelayData(providerAddress string, latency time.Duration, success bool)
 	AppendRelayFailure(providerAddress string)
-	AppendRelayData(providerAddress string, latency time.Duration, cu, syncBlock uint64)
+	AppendRelayData(providerAddress string, latency time.Duration, isHangingApi bool, cu, syncBlock uint64)
 	ChooseProvider(allAddresses []string, ignoredProviders map[string]struct{}, cu uint64, requestedBlock int64) (addresses []string, tier int)
-	GetExcellenceQoSReportForProvider(string) *pairingtypes.QualityOfServiceReport
+	GetExcellenceQoSReportForProvider(string) (*pairingtypes.QualityOfServiceReport, *pairingtypes.QualityOfServiceReport)
 	Strategy() provideroptimizer.Strategy
 	UpdateWeights(map[string]int64)
 }
@@ -85,13 +85,14 @@ type ignoredProviders struct {
 }
 
 type QoSReport struct {
-	LastQoSReport           *pairingtypes.QualityOfServiceReport
-	LastExcellenceQoSReport *pairingtypes.QualityOfServiceReport
-	LatencyScoreList        []sdk.Dec
-	SyncScoreSum            int64
-	TotalSyncScore          int64
-	TotalRelays             uint64
-	AnsweredRelays          uint64
+	LastQoSReport              *pairingtypes.QualityOfServiceReport
+	LastExcellenceQoSReport    *pairingtypes.QualityOfServiceReport
+	LastExcellenceQoSReportRaw *pairingtypes.QualityOfServiceReport
+	LatencyScoreList           []sdk.Dec
+	SyncScoreSum               int64
+	TotalSyncScore             int64
+	TotalRelays                uint64
+	AnsweredRelays             uint64
 }
 
 type DataReliabilitySession struct {
