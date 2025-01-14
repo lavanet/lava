1121718612ab9cc6e5d126e111f6be080cf452bf
Elad Gildnur <elad.gildnur@gmail.com>
2025-01-14 16:22:09 +0200

    Switch one spam log to debug

--- protocol/rpcconsumer/rpcconsumer_server.go
+++ protocol/rpcconsumer/rpcconsumer_server.go
@@ -309,7 +309,7 @@ func (rpccs *RPCConsumerServer) getLatestBlock() uint64 {
 	if numProviders > 0 && latestKnownBlock > 0 {
 		return uint64(latestKnownBlock)
 	}
-	utils.LavaFormatWarning("no information on latest block", nil, utils.Attribute{Key: "latest block", Value: 0})
+	utils.LavaFormatDebug("no information on latest block", utils.Attribute{Key: "latest block", Value: 0})
 	return 0
 }
 
