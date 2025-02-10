898f16ab07404f5c7f4679007743f4a3f7ed6f7f
Yarom Swisa <yarom@lavanet.xyz git config --global user.name Yarom>
2025-02-10 16:07:40 +0200

    lint

--- protocol/statetracker/hybridclient/responses.go
+++ protocol/statetracker/hybridclient/responses.go
@@ -64,9 +64,7 @@ type ResultBlockResults struct {
 
 // NewResultCommit is a helper to initialize the ResultCommit with
 // the embedded struct
-func NewResultCommit(header *types.Header, commit *types.Commit,
-	canonical bool) *ResultCommit {
-
+func NewResultCommit(header *types.Header, commit *types.Commit, canonical bool) *ResultCommit {
 	return &ResultCommit{
 		SignedHeader: types.SignedHeader{
 			Header: header,
