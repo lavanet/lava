e1946db2d8948c754f74e214a4ff11c19935ba58
Ran Mishael <ran@lavanet.xyz>
2025-03-23 16:40:24 +0100

    fix test

--- protocol/chainlib/node_error_handler_test.go
+++ protocol/chainlib/node_error_handler_test.go
@@ -110,73 +110,11 @@ func TestNodeErrorJQ(t *testing.T) {
       "inner": "0xabed5d742e4de942f5b1b4e419ff8b51a5361c5ab0cbbe59824a5c4249e426f"
     },
     {
-      "inner": "0x78f3de4897720dbf03ada5c018c35a5ea41f9351bb5a80405091d18bb4fe7d1a"
-    },
-    {
-      "inner": "0xf9b3b3e0f56612d0066a64f512f6c109df471806ab199c60dfaf258450ca85cd"
-    },
-    {
-      "inner": "0x2cd81d6cab1f3bc4750389aa62107f7507beeadb9090b902e6c9f212eba2f26a"
-    },
-    {
-      "inner": "0x90a5a36890cb9fd19ebe19b5c966bef698f51f09e05936b84e9cf11105f127da"
-    },
-    {
-      "inner": "0x5bcf3785fcecfcbdbbededfe4b8c816f42ba0cffc545b8e44155bb62a74c87ea"
-    },
-    {
-      "inner": "0xf12578c6f7401062e7e87472bd56b8d09c635eba29ea074b0b9462382d02bdf0"
-    },
-    {
-      "inner": "0xec6e9bfd7cc4b4634ba6a127cc122587ae294385e51e074a43fd3d8f072e412b"
-    },
-    {
-      "inner": "0xa14e466ac871f3cda148fd7621d1b312ffcee218173b0bf68175160af223f721"
-    },
-    {
-      "inner": "0x26ec0ab0d408ac512cf300746ea3563d9b103feda231a067db2baf1259b35883"
-    },
-    {
-      "inner": "0x7bdfe2dbfd46141497229b6d81279e130ba3f8252563d59ea9c6cf26cb685405"
-    },
-    {
-      "inner": "0x11139df1af7051a26114d7aeee6dd4ac9eb31ff7785c297fcfc8af218bb48dd6"
-    },
-    {
-      "inner": "0xb89df2c717c8aec4658a38427a4adf9c30ad3e899f0f4259804bad3e59c20051"
-    },
-    {
-      "inner": "0xb389f719b6cd4e2835b5115127209f8dbf80dd8534a94399e81c9cf6f48c6245"
-    },
-    {
-      "inner": "0xa0454b18a94f9d0276176ba3ceba900fb28d179aa9bc276c07b2116b5e6d962a"
-    },
-    {
-      "inner": "0xc24fd6702b84b2524b3c708a48524297294ed022d9c2f30222a743ef3773b2b9"
-    },
-    {
-      "inner": "0xea565dcc843495a292dad0249d4871ce0d39651c65b3442101903fc6dbf7def8"
-    },
-    {
-      "inner": "0x4525e62437f8928b5d68e2a239861e609b229f887c21ad3e4617e13eaa6c67ae"
-    },
-    {
-      "inner": "0x2aa729ca8fdc4ff4150d6dc20061c219506b3c2cefeb96b9516790f09da89a82"
-    },
-    {
-      "inner": "0xec9b7452456d291644384643307c8b883b4398acb448746d006a3bfc41e1b2b4"
-    },
-    {
-      "inner": "0x7c6a5f19c2404d8370535ad9f4d06fe87adf9dfde116355dfcc59d5bf0d81587"
-    },
-    {
       "inner": "0xd8db10dd9de4a5a8c62c4e690428e5acf0c0c31a4bc507f74a26b22529a5739c"
     }
   ]
 ]`
 
 	err := handler.HandleJSONFormatError([]byte(malformedJSON))
-	if err == nil {
-		t.Error("Expected error for malformed JSON, got nil")
-	}
+	require.NoError(t, err)
 }
