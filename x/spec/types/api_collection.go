e9178f0bf26d47de81c552d838c6d0d704b9e18c
Oren <oren@lavanet.xyz>
2025-05-14 13:33:27 +0300

    remove local field from API

--- x/spec/types/api_collection.go
+++ x/spec/types/api_collection.go
@@ -152,7 +152,6 @@ func (apic *ApiCollection) CombineWithOthers(others []*ApiCollection, combineWit
 func (sc SpecCategory) Combine(other SpecCategory) SpecCategory {
 	returnedCategory := SpecCategory{
 		Deterministic: sc.Deterministic && other.Deterministic,
-		Local:         sc.Local || other.Local,
 		Subscription:  sc.Subscription || other.Subscription,
 		Stateful:      sc.Stateful + other.Stateful,
 		HangingApi:    sc.HangingApi || other.HangingApi,
