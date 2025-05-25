c24ba6a80d17357723092e5a8b99399f3fb45abe
agoltzman <106151463+agoltzman@users.noreply.github.com>
2025-05-25 15:42:23 +0300

    feat: :sparkles: added requestId to relay proto

--- utils/requestIdentifier.go
+++ utils/requestIdentifier.go
@@ -10,6 +10,13 @@ func WithRequestId(ctx context.Context, reqId string) context.Context {
 	return context.WithValue(ctx, request_id_ctx_key{}, reqId)
 }
 
+func AppendRequestId(ctx context.Context, reqId string) context.Context {
+	if ctx.Value(request_id_ctx_key{}) != nil || reqId == "" {
+		return ctx
+	}
+	return context.WithValue(ctx, request_id_ctx_key{}, reqId)
+}
+
 func GetRequestId(ctx context.Context) (reqId string, found bool) {
 	reqId, found = ctx.Value(request_id_ctx_key{}).(string)
 	return
