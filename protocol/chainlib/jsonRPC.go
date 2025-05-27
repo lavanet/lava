68b49db6a67c6cf5fef5e12c9d2396cc30074ea7
agoltzman <106151463+agoltzman@users.noreply.github.com>
2025-05-27 13:52:24 +0300

    fix: :loud_sound: update requestId for node message to avoid ambiguous requestId

--- protocol/chainlib/jsonRPC.go
+++ protocol/chainlib/jsonRPC.go
@@ -738,7 +738,7 @@ func (cp *JrpcChainProxy) SendNodeMsg(ctx context.Context, ch chan interface{}, 
 	if err != nil {
 		return nil, "", nil, utils.LavaFormatError("jsonRPC ID mismatch error", err,
 			utils.Attribute{Key: "GUID", Value: ctx},
-			utils.Attribute{Key: "requestId", Value: nodeMessage.ID},
+			utils.Attribute{Key: "nodeMessageRequestId", Value: nodeMessage.ID},
 			utils.Attribute{Key: "responseId", Value: rpcMessage.ID},
 			utils.Attribute{Key: "reply", Value: replyMsg},
 			utils.Attribute{Key: "request", Value: nodeMessage},
