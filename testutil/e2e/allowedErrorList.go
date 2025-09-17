eab13c0571e8c19fa5b2ca2e99c740be6d182bcc
avitenzer <tenzer@clara.co.uk>
2025-09-17 11:44:34 +0300

    Fixed e2e test issue

--- testutil/e2e/allowedErrorList.go
+++ testutil/e2e/allowedErrorList.go
@@ -5,14 +5,17 @@ package e2e
 var allowedErrors = map[string]string{
 	"getSupportedApi":        "This error is allowed because the Tendermint URI tests have a test that checks if the error is caught.",
 	"No pairings available.": "This error is allowed because when the network is just booted up and pairings are not yet done this would happen. If after a few seconds the pairings are still not available the e2e would fail because the initial check if the provider is responsive would time out.",
-	`error connecting to provider error="context deadline exceeded"`: "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
-	"purging provider after all endpoints are disabled provider":     "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
-	"Provider Side Failed Sending Message, Reason: Unavailable":      "This error is allowed because it is caused by the lavad restart to turn on emergency mode",
-	"Maximum cu exceeded PrepareSessionForUsage":                     "This error is allowed because it is caused by switching between providers, continuous failure would be caught by the e2e so we can allowed this error.",
-	"Failed To Connect to cache at address":                          "This error is allowed because it is caused by cache being connected only during the test and not during the bootup",
-	"Not Implemented":                                                "This error is allowed because the /lavanet/lava/pairing/clients API endpoint returns 501 Not Implemented from providers during tests",
-	"tx already exists in cache":                                     "This error is allowed because it can occur when transactions are retried during the test",
-	"failed to create canonical form":                                "This error is allowed because it is caused by the relay processor not being able to create a canonical form",
+	`error connecting to provider error="context deadline exceeded"`:                   "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
+	"purging provider after all endpoints are disabled provider":                       "This error is allowed because it is caused by the initial bootup, continuous failure would be caught by the e2e so we can allowed this error.",
+	"Provider Side Failed Sending Message, Reason: Unavailable":                        "This error is allowed because it is caused by the lavad restart to turn on emergency mode",
+	"Maximum cu exceeded PrepareSessionForUsage":                                       "This error is allowed because it is caused by switching between providers, continuous failure would be caught by the e2e so we can allowed this error.",
+	"Failed To Connect to cache at address":                                            "This error is allowed because it is caused by cache being connected only during the test and not during the bootup",
+	"Not Implemented":                                                                  "This error is allowed because the /lavanet/lava/pairing/clients API endpoint returns 501 Not Implemented from providers during tests",
+	"unsupported method":                                                               "This error is allowed because providers now return unsupported method errors for unimplemented APIs instead of retrying",
+	"unsupported method 'Default-/lavanet/lava/pairing/clients/LAV1': Not Implemented": "This error is allowed because the /lavanet/lava/pairing/clients API endpoint is not implemented in test providers",
+	"unsupported method 'Default-/lavanet/lava/conflict/params': test":                 "This error is allowed because the /lavanet/lava/conflict/params API endpoint is not implemented in test providers",
+	"tx already exists in cache":                                                       "This error is allowed because it can occur when transactions are retried during the test",
+	"failed to create canonical form":                                                  "This error is allowed because it is caused by the relay processor not being able to create a canonical form",
 }
 
 var allowedErrorsDuringEmergencyMode = map[string]string{
