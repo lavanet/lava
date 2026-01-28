3d4396188c7abb53596204297e1a5fdaeceb279d
avitenzer <tenzer@clara.co.uk>
2026-01-28 17:10:37 +0200

    refactor(relay): clarify selection strategy comments in selection.go
    Updated comments for the Stateless and Stateful selection strategies to enhance clarity. The Stateless selection now emphasizes retries and seeking node responses, while the Stateful selection description has been expanded to include waiting for all providers to return a response.

--- protocol/relaycore/selection.go
+++ protocol/relaycore/selection.go
@@ -10,6 +10,6 @@ var RelayCountOnNodeError = 2
 
 // selection Enum, do not add other const
 const (
-	Stateless Selection = iota // retries enabled, seeks majority consensus from node responses
-	Stateful                   // all top providers at once, waits for best result (no retries)
+	Stateless Selection = iota // retries enabled, seeks node responses
+	Stateful                   // all top providers at once, waits for best result (no retries) or for all the providers to return a response
 )
